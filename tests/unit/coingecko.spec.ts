7eedfeac158801d2af8e42f18f269f3df6a18763
bonustrack <fabien@bonustrack.co>
2021-02-02 18:55:56 +0700

    Fix test

--- tests/unit/coingecko.spec.ts
+++ tests/unit/coingecko.spec.ts
@@ -9,10 +9,10 @@ describe('coingecko.getTokensPrice', () => {
     ];
     const prices = await getTokensPrice(addresses);
     expect(
-      prices['0x6b175474e89094c44da98b954eedeac495271d0f']
+      prices['0x6b175474e89094c44da98b954eedeac495271d0f'].price
     ).toBeGreaterThan(0);
     expect(
-      prices['0xba100000625a3754423978a60c9317c58a424e3d']
+      prices['0xba100000625a3754423978a60c9317c58a424e3d'].price
     ).toBeGreaterThan(0);
   });
 });
