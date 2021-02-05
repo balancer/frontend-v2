8190db1bfaff4128744a5af1596a9be1bbe956f8
Greg Taschuk <gregorytaschuk@gmail.com>
2021-02-05 03:12:23 -0800

    set kovan addresses (#6)
    * set kovan addresses
    
    * set dockerParity addresses for core frozen to kovan deploy commit

--- src/utils/balancer/constants.ts
+++ src/utils/balancer/constants.ts
@@ -1,35 +1,38 @@
+// Config for a local node and subgraph
+const dockerParityAddresses = {
+  authorizer: '0x6Bff77238256fFF717e5Cc9A96640dC0D068b14D',
+  vault: '0x293f6758Fe4a4343E367Ac8E6A697eC6315fBEd7',
+  constantProductPoolFactory: '0xE48Aae4178612fdF77af25C83fa1099491A8AD1B',
+  stablecoinPoolFactory: '0xb4F97F452D71C3bba3c30c554305A7250c08b23A',
+  oneToOneSwapValidator: '0x2e7950e645CbA3eD9B376df081F2FA0fE53d30f0',
+  tokenFactory: '0xc9d340Bc37C57e70383b217De2c07FB252E36D55',
+  weth: '0x6c37Fa9557C135dF4323004D3f0549f3e94F112D',
+  MockFlashLoanReceiver: '',
+};
+
+// Config for latest core on Kovan
+const kovanAddresses = {
+  vault: '0x99EceD8Ba43D090CA4283539A31431108FD34438',
+  constantProductPoolFactory: '0xdB49af9b25cFAdbb0118BD38ec86EaE51F210931',
+  stablecoinPoolFactory: '0xd0264838A8278674fa71f135f7Dba07cDDeC20d9',
+  oneToOneSwapValidator: '0x6648473ae4D7a7FdE330846D11ee95FDE2DE9447',
+  tokenFactory: '0xfc532Ef450D9BaB17d5F153B091d271A3ADDcade',
+  weth: '0xd98C8F956714a17a5708A3175B8a3E97d1C5eaCc',
+  MockFlashLoanReceiver: '0xD1C8625C3ECCD11d214A8760093959A8237dD710',
+};
+
 export default {
-  // Config for latest core on Kovan
-  /*
-  authorizer: '0x30Ca53e6e5bEc8B2513ddCfe45eE0F904900F096',
-  vault: '0xBFa16D136bAFEa5a54f581C491be040BA44AF98F',
-  constantProductPoolFactory: '0x26aC322aDa606bA5F3B3E23dB2955fA55d542115',
-  stablecoinPoolFactory: '0x9942784AEb3ECcF6013280A86f0F24dCd785D202',
-  oneToOneSwapValidator: '0x7bf43891B10ce66429d1a101FF0516e1Ab628C3B',
-  tokenFactory: '0xd7ce67992277d157abaB24cd610E1A4AD5292438',
-  weth: '0xB297057e60476438a280686415CBee9a4D949c6B',
-  MockFlashLoanReceiver: '0xD1C8625C3ECCD11d214A8760093959A8237dD710',
-  */
-
-  // Config for a local node and subgraph
-  vault: '0x47753454C4270DAE017499952b973c24Aaa5768D',
-  constantProductPoolFactory: '0x3D8c98c3Edb8fb401B654877516B68b95806DFc1',
-  stablecoinPoolFactory: '0xA840ddccf6a06692B312f54D63F7487b2806A3a1',
-  oneToOneSwapValidator: '0x52402A8bAf78a6d7c5804D4308B31a856BAa52a2',
-  tokenFactory: '0x0B5b6CA7c40C3f4a7b59653B8a2b140BbD8c7D6e',
-  //weth: '0x4CDDb3505Cf09ee0Fa0877061eB654839959B9cd',
-  weth: '0xdA7D8D96bc15CB6dE1B950E53101Ac20452b37d4',
-  MockFlashLoanReceiver: '0xD1C8625C3ECCD11d214A8760093959A8237dD710',
-
+  //...dockerParityAddresses,
+  ...kovanAddresses,
   // Strategy names are i18n translation keys
   strategies: {
     0: {
       type: '0',
-      name: 'weightedPool'
+      name: 'weightedPool',
     },
     1: {
       type: '1',
-      name: 'stablePool'
-    }
-  }
+      name: 'stablePool',
+    },
+  },
 };
