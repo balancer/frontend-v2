ed572baba048b086cc955a4dd2a9f238d5f10684
bonustrack <fabien@bonustrack.co>
2021-02-16 00:22:44 +0700

    Use Balancer org subgraph

--- src/utils/balancer/subgraph.ts
+++ src/utils/balancer/subgraph.ts
@@ -3,7 +3,7 @@ import { getCurrentTs, tsToBlockNumber } from '@/utils';
 
 const BALANCER_SUBGRAPH_URL = {
   '1': 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-beta',
-  '42': 'https://api.thegraph.com/subgraphs/name/bonustrack/balancer-kovan-v2'
+  '42': 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-kovan-v2'
 };
 
 export async function getBPTMarketChart(
