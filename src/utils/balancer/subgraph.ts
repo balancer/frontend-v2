c65149e00ff74e755088fc968a40e35b148ec2a7
Greg Taschuk <gregorytaschuk@gmail.com>
2021-02-16 23:32:26 -0800

    added docker-parity subgraph (#16)

--- src/utils/balancer/subgraph.ts
+++ src/utils/balancer/subgraph.ts
@@ -3,6 +3,7 @@ import { getCurrentTs, tsToBlockNumber } from '@/utils';
 
 const BALANCER_SUBGRAPH_URL = {
   '1': 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-beta',
+  '17': 'http://localhost:8000/subgraphs/name/balancer-labs/balancer-v2',
   '42': 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-kovan-v2'
 };
 
