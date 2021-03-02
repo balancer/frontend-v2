b2bb81f2a540b03764f75216e4775125f83835ce
Timur Badretdinov <destinerx@gmail.com>
2021-03-02 15:24:49 +0300

    Include svg imports in shims

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,8 @@ declare module '*.vue' {
   const component: ReturnType<typeof defineComponent>;
   export default component;
 }
+
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
