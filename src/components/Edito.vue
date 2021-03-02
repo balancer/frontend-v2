0b01b824fc9ca0e464d214c599272b5b417ba152
Gareth Fuller <gareth.a.f@gmail.com>
2021-03-02 16:08:43 +0000

    Minor style fixes

--- src/components/Edito.vue
+++ src/components/Edito.vue
@@ -3,11 +3,11 @@
     <Container class="relative">
       <div class="grid grid-cols-12 gap-8 flex items-center">
         <div class="col-span-12 md:col-span-6">
-          <h1 v-text="$t('editoTitle')" class="text-3xl md:text-4xl mb-4" />
-          <p v-text="$t('editoAbout')" class="mb-10 link-color" />
+          <h1 v-text="$t('editoTitle')" class="mb-4" />
+          <p v-text="$t('editoAbout')" class="mb-10" />
         </div>
         <div class="hidden md:block md:col-span-6">
-          <img src="~@/assets/empty.png" class="border bg-color float-right" />
+          <img src="~@/assets/empty.png" class="bg-gray-100 border border-gray-100 float-right" />
         </div>
       </div>
       <div class="hidden absolute -bottom-4 text-2xl border-color">
