a40b4e8f93e84530b10296f4db08b6022c26f3bc
Gareth Fuller <gareth.a.f@gmail.com>
2021-03-03 11:47:58 +0000

    Lint fixes

--- src/components/Edito.vue
+++ src/components/Edito.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="block-bg py-12 border-b dark:border-gray-700 bg-white dark:bg-gray-900">
+  <div
+    class="block-bg py-12 border-b dark:border-gray-700 bg-white dark:bg-gray-900"
+  >
     <Container class="relative">
       <div class="grid grid-cols-12 gap-8 flex items-center">
         <div class="col-span-12 md:col-span-6">
@@ -7,7 +9,9 @@
           <p v-text="$t('editoAbout')" class="mb-10" />
         </div>
         <div class="hidden md:block md:col-span-6">
-          <div class="w-2/3 h-64 bg-gray-100 dark:bg-gray-800 border border-gray-100 dark:border-gray-700 float-right" />
+          <div
+            class="w-2/3 h-64 bg-gray-100 dark:bg-gray-800 border border-gray-100 dark:border-gray-700 float-right"
+          />
         </div>
       </div>
       <div class="hidden absolute -bottom-4 text-2xl border-color">
