5583b2aa427edcaf1ad26eaaf6d1516acc24ba0f
Gareth Fuller <gareth.a.f@gmail.com>
2021-03-03 15:52:51 +0000

    Add BalCard component

---
+++ src/components/Bal/Card.vue
@@ -0,0 +1,77 @@
+<template>
+  <div :class="['bal-card', cardClasses]">
+    <div :class="['header', headerClasses]">
+      <component :is="titleTag" v-if="!!title" v-text="title" />
+      <div v-if="$slots.header" class="flex-1">
+        <slot name="header" />
+      </div>
+    </div>
+    <div :class="['content', contentClasses]">
+      <slot />
+    </div>
+    <div v-if="$slots.footer" :class="['footer', footerClasses]">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BalCard',
+
+  props: {
+    title: { type: String, default: '' },
+    titleTag: { type: String, default: 'h3' },
+    shadowSize: { type: String, default: '' },
+    square: { type: Boolean, default: false },
+    noPad: { type: Boolean, default: false }
+  },
+
+  computed: {
+    cardClasses(): Record<string, any> {
+      return {
+        rounded: !this.square,
+        [`shadow${this.shadowSize}`]: true
+      };
+    },
+
+    contentClasses(): Record<string, any> {
+      return {
+        'p-4': !this.noPad
+      };
+    },
+
+    headerClasses(): Record<string, any> {
+      return {
+        'p-4': !this.noPad
+      };
+    },
+
+    footerClasses(): Record<string, any> {
+      return {
+        'p-4': !this.noPad,
+        'rounded-b': !this.square
+      };
+    }
+  }
+});
+</script>
+
+<style scoped>
+.bal-card {
+  @apply bg-white dark:bg-gray-900;
+  @apply border-0 dark:border dark:border-gray-700;
+}
+
+.header {
+  @apply flex items-center;
+}
+
+.footer {
+  @apply flex items-center;
+  @apply bg-gray-50 dark:bg-gray-800;
+  @apply border-t border-gray-100 dark:border-gray-700;
+}
+</style>
