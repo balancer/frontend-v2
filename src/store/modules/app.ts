f38769ffd8560b9889154895d73f58ea5850e107
Gareth Fuller <gareth.a.f@gmail.com>
2021-03-03 11:10:54 +0000

    Refactor app store I think it helps to be more explicit here. Also, we should use mutations if all we are doing is mutating the state. Actions should only be used to do something prior to mutating state, e.g. fetching data from an external source.

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -18,57 +18,63 @@ const state: AppState = {
   authLoading: false,
   modalOpen: false,
   darkMode: false,
-  locale: lsGet('locale', defaultLocale),
-  slippage: lsGet('slippage', '0.01')
+  locale: defaultLocale,
+  slippage: '0.01'
 };
 
 const actions = {
   init: async ({ commit, dispatch }) => {
-    commit('SET', { init: true });
+    commit('setInit', true);
+
+    // Fetch init data
     dispatch('loadRegistry');
     dispatch('getBlockNumber');
+
+    // Setup web3
     const auth = getInstance();
     auth.getConnector().then(connector => {
       if (connector) dispatch('login', connector);
     });
-    i18n.global.locale = state.locale;
-  },
 
-  loading: ({ commit }, payload) => {
-    commit('SET', { loading: payload });
-  },
-
-  toggleModal: ({ commit }) => {
-    commit('SET', { modalOpen: !state.modalOpen });
-  },
-
-  setLocale: async ({ commit }, locale) => {
-    lsSet('locale', locale);
-    i18n.global.locale = locale;
-    commit('SET', { locale });
-  },
-  setSlippage: async ({ commit }, slippage) => {
-    lsSet('slippage', slippage);
-    commit('SET', { slippage });
+    // Set defaults from localStorage
+    commit('setLocale', lsGet('locale', defaultLocale));
+    commit('setSlippage', lsGet('slippage', '0.01'));
+    commit('setDarkMode', lsGet('darkMode', false));
   }
 };
 
 const mutations = {
-  SET(_state, payload) {
-    Object.keys(payload).forEach(key => {
-      _state[key] = payload[key];
-    });
+  setInit(state: AppState, val: boolean): void {
+    state.init = val;
   },
 
-  setDarkMode(state: AppState, val: boolean) {
+  setLoading(state: AppState, val: boolean): void {
+    state.loading = val;
+  },
+
+  toggleModal(state: AppState): void {
+    state.modalOpen = !state.modalOpen;
+  },
+
+  setLocale(state: AppState, locale: string): void {
+    state.locale = locale;
+    lsSet('locale', locale);
+    i18n.global.locale = locale;
+  },
+
+  setSlippage(state: AppState, slippage: string): void {
+    state.slippage = slippage;
+    lsSet('slippage', slippage);
+  },
+
+  setDarkMode(state: AppState, val: boolean): void {
     state.darkMode = val;
     lsSet('darkMode', state.darkMode);
-  },
-
-  toggleDarkMode(state: AppState) {
-    console.log('toggle');
-    state.darkMode = !state.darkMode;
-    lsSet('darkMode', state.darkMode);
+    if (state.darkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
   }
 };
 
