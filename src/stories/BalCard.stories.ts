7942a68bf490bcb3484475951cd636aab68f8181
Gareth Fuller <gareth.a.f@gmail.com>
2021-03-03 17:01:22 +0000

    BalCard story

---
+++ src/stories/BalCard.stories.ts
@@ -0,0 +1,53 @@
+import '../assets/css/tailwind.css';
+import '../assets/css/index.css';
+
+import BalCard from '../components/Bal/Card.vue';
+
+export default {
+  component: BalCard,
+  title: 'Components/BalCard',
+  args: {
+    title: 'A title',
+    titleTag: 'h3',
+    square: false,
+    noPad: false,
+    shadowSize: ''
+  },
+  argTypes: {
+    titleTag: { control: { type: 'select', options: ['h1', 'h2', 'h3'] } },
+    shadowSize: {
+      control: {
+        type: 'select',
+        options: ['', '-none', '-sm', '-md', '-lg', '-xl', '-2xl']
+      }
+    }
+  }
+};
+
+interface Props {
+  title: string;
+  titleTag: string;
+  square: boolean;
+  noPad: boolean;
+  shadowSize: string;
+}
+
+//👇 We create a “template” of how args map to rendering
+const Template = (args: Props) => ({
+  components: { BalCard },
+  setup() {
+    //👇 The args will now be passed down to the template
+    return { args };
+  },
+  template: `
+<BalCard v-bind="args">
+  Content
+  <template v-slot:footer>
+    Action
+  </template>
+</BalCard>
+`
+});
+
+//👇 Each story then reuses that template
+export const Primary = Template.bind({});
