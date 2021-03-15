6324627743641dd04b5265963807d07a65a979a3
Timur Badretdinov <destinerx@gmail.com>
2021-03-15 16:33:16 +0300

    Remove fee value from swap table

--- src/components/Table/Swaps.vue
+++ src/components/Table/Swaps.vue
@@ -6,7 +6,6 @@
         <th class="sticky top-0 p-2 py-6 text-right">In</th>
         <th class="sticky top-0 p-2 py-6 text-right">Out</th>
         <th class="sticky top-0 p-2 py-6 text-right">Value</th>
-        <th class="sticky top-0 p-2 py-6 text-right">Fee</th>
         <th class="sticky top-0 p-2 pr-4 py-6 text-right">Transaction</th>
       </tr>
       <tr class="row hover:bg-gray-50" v-for="swap in swaps" :key="swap.id">
@@ -22,9 +21,6 @@
         <th class="sticky top-0 p-2 py-6 text-right">
           {{ _num(getValue(swap), '$0,0.00') }}
         </th>
-        <th class="sticky top-0 p-2 py-6 text-right">
-          {{ _num(getFeeValue(swap), '$0,0.00') }}
-        </th>
         <th class="sticky top-0 p-2 pr-4 py-6 text-right">
           <a :href="_explorer(web3.config.key, swap.id, 'tx')" target="_blank">
             {{ swap.id.substr(0, 10) }}…
@@ -37,7 +33,6 @@
 
 <script lang="ts">
 import { getAddress } from '@ethersproject/address';
-import { formatUnits } from '@ethersproject/units';
 import { useStore } from 'vuex';
 
 export default {
@@ -75,16 +70,9 @@ export default {
       return 0;
     }
 
-    function getFeeValue(swap) {
-      const value = getValue(swap);
-      const fee = parseFloat(formatUnits(swap.poolId.swapFee, 18));
-      return fee * value;
-    }
-
     return {
       getDate,
-      getValue,
-      getFeeValue
+      getValue
     };
   }
 };
