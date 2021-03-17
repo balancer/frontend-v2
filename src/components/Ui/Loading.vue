54bf05dde44626e8af021dec7eb96f5142d613aa
Gareth Fuller <gareth.a.f@gmail.com>
2021-03-17 14:07:09 +0000

    Remove conditional

--- src/components/Ui/Loading.vue
+++ src/components/Ui/Loading.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    v-if="app.loading || !app.init"
-    class="h-screen w-full flex items-center justify-center"
-  >
+  <div class="h-screen w-full flex items-center justify-center">
     <BalLoadingIcon />
   </div>
 </template>
