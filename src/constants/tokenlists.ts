8f75b658a53a0a9c1072e6a56737dee3cb1e4119
Gareth Fuller <gareth.a.f@gmail.com>
2021-03-17 13:53:16 +0000

    Comment out slow loading token lists

--- src/constants/tokenlists.ts
+++ src/constants/tokenlists.ts
@@ -2,16 +2,16 @@ export const TOKEN_LIST_DEFAULT = 'textcoin.eth';
 
 export const TOKEN_LISTS: string[] = [
   TOKEN_LIST_DEFAULT,
-  'tokens.uniswap.eth',
+  'tokens.uniswap.eth'
   // 'tokens.1inch.eth', Fail to load
-  'synths.snx.eth',
-  'tokenlist.dharma.eth',
-  'defi.cmc.eth',
-  'erc20.cmc.eth',
-  'stablecoin.cmc.eth',
-  'tokenlist.zerion.eth',
-  'tokenlist.aave.eth',
-  't2crtokens.eth'
+  // 'synths.snx.eth',
+  // 'tokenlist.dharma.eth',
+  // 'defi.cmc.eth',
+  // 'erc20.cmc.eth',
+  // 'stablecoin.cmc.eth',
+  // 'tokenlist.zerion.eth',
+  // 'tokenlist.aave.eth',
+  // 't2crtokens.eth'
 ];
 
 export const ETHER = {
