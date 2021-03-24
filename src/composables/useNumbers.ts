0d3dfd68ea0d82a9515c01d6e2db16bfe49bfed9
Gareth Fuller <gareth.a.f@gmail.com>
2021-03-24 20:20:00 +0000

    Update useNumber composable

--- src/composables/useNumbers.ts
+++ src/composables/useNumbers.ts
@@ -3,7 +3,6 @@ import numeral from 'numeral';
 export default function useNumbers() {
   function format(number: number, _format = '(0.[0]a)') {
     if (number < 1) _format = '0.[000]';
-    if (number == 0) return '-';
     return numeral(number).format(_format);
   }
 
