4e1f383ed785e8fe5d4f6f7fae035e8201234e75
Gareth Fuller <gareth.a.f@gmail.com>
2021-03-25 09:15:00 +0000

    Refactor menu comp

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,26 +1,20 @@
 <template>
-  <Block
-    :slim="true"
-    class="mainmenu bg-white sliding px-2 md:px-4 pb-1 text-base md:text-lg"
-  >
+  <BalCard class="menu text-gray-700 p-4" no-pad>
     <router-link
       :to="{ name: 'portfolio' }"
       v-text="$t('myPortfolio')"
-      class="md:mx-2 px-2 py-5 inline-block"
+      class="mr-4"
     />
     <router-link
       :to="{ name: 'home' }"
       v-text="$t('investmentPools')"
-      class="md:mx-2 px-2 py-5 inline-block"
+      class=""
     />
-  </Block>
+  </BalCard>
 </template>
 
 <style lang="css" scoped>
-.mainmenu > a {
-  @apply text-gray-700;
-}
-.mainmenu a.router-link-exact-active {
+.menu a.router-link-exact-active {
   @apply text-black dark:text-white font-medium;
 }
 </style>
