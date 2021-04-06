856e91917f31c6cf98d54d76c440dd4af3e318f4
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-06 10:48:30 +0100

    Possible implementation of plugin

--- src/composables/useBlocknative.ts
+++ src/composables/useBlocknative.ts
@@ -1,11 +1,18 @@
 import Notify from 'bnc-notify';
+// import { inject } from 'vue';
 import { useStore } from 'vuex';
+// import { blocknativeSymbol } from '@/plugins/blocknative';
 
 export default function useBlocknative() {
   const store = useStore();
 
-  return Notify({
+  // const notify = inject(blocknativeSymbol) as ReturnType<typeof Notify>;
+  // if (!notify) throw new Error('No notify provided!');
+
+  const notify = Notify({
     dappId: process.env.VUE_APP_BLOCKNATIVE_DAPP_ID || '',
     networkId: Number(store.state.web3.config.key) || 1
   });
+
+  return notify;
 }
