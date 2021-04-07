f47a84a175c0e80ac0217dca45cb5be0e4a8b6e2
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-07 11:17:33 +0100

    Refactor blocknative composable

--- src/composables/useBlocknative.ts
+++ src/composables/useBlocknative.ts
@@ -1,18 +1,10 @@
 import Notify from 'bnc-notify';
-// import { inject } from 'vue';
-import { useStore } from 'vuex';
-// import { blocknativeSymbol } from '@/plugins/blocknative';
+import { inject } from 'vue';
+import { blocknativeSymbol } from '@/plugins/blocknative';
 
 export default function useBlocknative() {
-  const store = useStore();
-
-  // const notify = inject(blocknativeSymbol) as ReturnType<typeof Notify>;
-  // if (!notify) throw new Error('No notify provided!');
-
-  const notify = Notify({
-    dappId: process.env.VUE_APP_BLOCKNATIVE_DAPP_ID || '',
-    networkId: Number(store.state.web3.config.key) || 1
-  });
+  const notify = inject(blocknativeSymbol) as ReturnType<typeof Notify>;
+  if (!notify) throw new Error('Blocknative Notify missing!');
 
   return notify;
 }
