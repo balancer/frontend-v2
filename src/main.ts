a034c850498f663a468b53a8d30b6dcdddd1e470
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-07 11:15:21 +0100

    Refactor and use blocknative plugin

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import store from '@/store';
 import router from '@/plugins/router';
 import mixins from '@/plugins/mixins';
 import i18n from '@/plugins/i18n';
+import blocknative from '@/plugins/blocknative';
 import authOptions from '@/plugins/authOptions';
 import { LockPlugin } from '@snapshot-labs/lock/plugins/vue3';
 import VueApexCharts from 'vue3-apexcharts';
@@ -16,6 +17,7 @@ const app = createApp(App)
   .use(i18n)
   .use(router)
   .use(store)
+  .use(blocknative)
   .use(LockPlugin, authOptions)
   .use(VueApexCharts)
   .mixin(mixins);
--- src/plugins/blocknative.ts
+++ src/plugins/blocknative.ts
@@ -6,11 +6,11 @@ export default {
   install: app => {
     const notifyInstance = Notify({
       dappId: process.env.VUE_APP_BLOCKNATIVE_DAPP_ID || '',
-      networkId: 1
+      networkId: Number(process.env.VUE_APP_BLOCKNATIVE_DAPP_ID) || 1
     });
 
     // Make plugin available in options API
-    app.config.globalProperties.$notify = notifyInstance || {};
+    app.config.globalProperties.$bnNotify = notifyInstance || {};
 
     // Make plugin available in composition API
     app.provide(blocknativeSymbol, notifyInstance);
