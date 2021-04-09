3cb2600d724c80a69f790e8ac92feba79c255900
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-09 13:54:18 +0100

    Add getAllPools action and use

--- src/store/modules/pools.ts
+++ src/store/modules/pools.ts
@@ -1,28 +1,42 @@
 import { Pool } from '@/utils/balancer/types';
 import { getPool } from '@/utils/balancer/pools';
+import { getPools, Pool as SubgraphPool } from '@/api/subgraph';
 import getProvider from '@/utils/provider';
 
 export interface PoolsState {
   current: Pool | null;
+  all: SubgraphPool[];
 }
 
 const state: PoolsState = {
-  current: null
+  current: null,
+  all: []
 };
 
 const mutations = {
-  setCurrent(state, pool: Pool): void {
-    state.current = pool;
+  setCurrent(_state: PoolsState, pool: Pool): void {
+    _state.current = pool;
+  },
+
+  setAll(_state: PoolsState, pools: SubgraphPool[]): void {
+    _state.all = pools;
   }
 };
 
 const actions = {
-  async loadPool({ commit, rootState }, id: string): Promise<Pool> {
+  async getPool({ commit, rootState }, id: string): Promise<Pool> {
     const network = rootState.web3.config.key;
     const provider = getProvider(network);
     const pool = await getPool(network, provider, id);
     commit('setCurrent', pool);
     return pool;
+  },
+
+  async getAllPools({ commit, rootState }): Promise<SubgraphPool[]> {
+    const network = rootState.web3.config.key;
+    const pools = await getPools(network);
+    commit('setAll', pools);
+    return pools;
   }
 };
 
