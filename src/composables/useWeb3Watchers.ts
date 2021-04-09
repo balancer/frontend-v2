cf167dc720cb104045059d0493bf86d8a47e630d
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-09 13:54:41 +0100

    Refactor on network change watcher

--- src/composables/useWeb3Watchers.ts
+++ src/composables/useWeb3Watchers.ts
@@ -1,17 +1,53 @@
-import { watch } from 'vue';
-import useBlocknative from './useBlocknative';
+import { watch, ref } from 'vue';
 import { useStore } from 'vuex';
+import useBlocknative from './useBlocknative';
+import InfuraService from '@/services/infura/service';
+import { getAddress } from '@ethersproject/address';
 
-export default function useWeb3Watchers(): void {
+export default function useWeb3Watchers() {
+  // COMPOSABLES
   const store = useStore();
   const { notify } = useBlocknative();
 
+  // DATA
+  const loading = ref(false);
+
+  // SERVICES
+  const infuraService = new InfuraService(store.state.web3.config.chainId);
+
+  // METHODS
+  const setBlockNumber = (blockNumber: number) =>
+    store.commit('setBlockNumber', blockNumber);
+
+  async function updatePools(chainId: number): Promise<void> {
+    await store.dispatch('getAllPools', chainId);
+  }
+
+  async function updateTokens(): Promise<void> {
+    const pools = store.state.pools.all;
+    const tokens = pools
+      .map(pool => pool.tokens.map(token => getAddress(token.address)))
+      .reduce((a, b) => [...a, ...b], []);
+    await store.dispatch('injectTokens', tokens);
+  }
+
+  // WATCHERS
+
   // Watch for network change:
   // -> Update Blocknative networkId
+  // -> Re-init the block number listener
+  // -> Update the global pools list
+  // -> Update the global token list based on new pools
   watch(
-    () => store.state.web3.config.key,
-    newNetwork => {
-      notify.config({ networkId: Number(newNetwork) });
+    () => store.state.web3.config.chainId,
+    async newChainId => {
+      loading.value = true;
+      notify.config({ networkId: newChainId });
+      infuraService.switchNetwork(newChainId);
+      infuraService.initBlockListener(setBlockNumber);
+      await updatePools(newChainId);
+      await updateTokens();
+      loading.value = false;
     },
     { immediate: true }
   );
@@ -32,4 +68,6 @@ export default function useWeb3Watchers(): void {
       });
     }
   );
+
+  return { loading };
 }
