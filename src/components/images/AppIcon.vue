d4c91636aead5eaf1ecfdab3a3e061770e24316f
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-11 16:36:13 +0100

    Add app logo component

---
+++ src/components/images/AppIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <img v-if="darkMode" src="~@/assets/logo-dark.svg" width="30" class="mr-2" />
+  <img v-else src="~@/assets/logo-light.svg" width="30" class="mr-2" />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import { useStore } from 'vuex';
+
+export default defineComponent({
+  name: 'AppIcon',
+
+  setup() {
+    // COMPOSABLES
+    const store = useStore();
+
+    // COMPUTED
+    const darkMode = computed(() => store.state.app.darkMode);
+
+    return {
+      darkMode
+    };
+  }
+});
+</script>
