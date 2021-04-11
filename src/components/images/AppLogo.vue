60e6b0a085bc24b95e0e8a317de06f3fb45edad9
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-11 16:36:13 +0100

    Add app logo component

---
+++ src/components/images/AppLogo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center">
+    <AppIcon />
+    <span class="mr-1 font-secondary text-xl font-semibold">
+      Balancer
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppIcon from './AppIcon.vue';
+
+export default defineComponent({
+  name: 'AppLogo',
+
+  components: {
+    AppIcon
+  }
+});
+</script>
