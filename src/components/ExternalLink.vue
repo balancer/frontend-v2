078f4815bb00eeca80cb9c9b6739c33127024ee8
Evgeny Boxer <evgeny.boxer@gmail.com>
2021-04-13 21:15:23 +1000

    feat: ExternalLink component

---
+++ src/components/ExternalLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <a :href="href" target="_blank" rel="noopener noreferrer">
+    <slot />
+  </a>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    href: {
+      type: String,
+      required: true
+    }
+  }
+});
+</script>
