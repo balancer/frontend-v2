005f386f5efa7237b2d91bda9719b5d183efa0d4
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-14 17:47:12 +0100

    Update useWeb3 composable

--- src/composables/useWeb3.ts
+++ src/composables/useWeb3.ts
@@ -6,13 +6,24 @@ import configs from '@/config';
 export default function useWeb3() {
   const store = useStore();
 
+  const appNetworkKey = process.env.VUE_APP_NETWORK;
+  const appNetworkId = Number(appNetworkKey);
+  const appNetworkName = configs[appNetworkId].shortName;
   const networkId = computed(() => store.state.web3.config.chainId);
   const networkKey = computed(() => store.state.web3.config.key);
+  const networkName = computed(() => store.state.web3.config.shortName);
   const account = computed(() => store.state.web3.account);
   const blockNumber = computed(() => store.state.web3.blockNumber);
 
   const unsupportedNetwork = computed(() => {
     return store.state.web3.config.unknown;
+  });
+
+  const networkMismatch = computed(() => {
+    return (
+      !unsupportedNetwork.value &&
+      networkKey.value !== process.env.VUE_APP_NETWORK
+    );
   });
 
   const explorerBaseURL = configs[networkId.value].explorer;
@@ -26,10 +37,15 @@ export default function useWeb3() {
 
   return {
     explorer,
+    appNetworkKey,
+    appNetworkId,
+    appNetworkName,
     networkId,
     networkKey,
+    networkName,
     account,
     blockNumber,
-    unsupportedNetwork
+    unsupportedNetwork,
+    networkMismatch
   };
 }
