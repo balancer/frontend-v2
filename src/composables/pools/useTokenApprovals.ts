f79576ebe4a535a093635bd8289a3bc19dac62ad
Ansaf Ahmed <me@ansaf.net>
2021-04-19 00:28:38 +1000

    update: remove unused import

--- src/composables/pools/useTokenApprovals.ts
+++ src/composables/pools/useTokenApprovals.ts
@@ -1,4 +1,4 @@
-import { ref, computed, watch } from 'vue';
+import { ref, computed } from 'vue';
 import { useStore } from 'vuex';
 import { approveTokens } from '@/utils/balancer/tokens';
 import { parseUnits } from '@ethersproject/units';
