6f92f13633a8368192cb31731503f14163f8f614
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-20 09:43:07 +0100

    Refactor app nav

--- src/components/navs/AppNav/AppNav.vue
+++ src/components/navs/AppNav/AppNav.vue
@@ -1,161 +1,90 @@
 <template>
-  <nav id="topnav">
-    <AppNavAlert v-if="alert" :alert="alert" />
+  <AppNavAlert v-if="alert" :alert="alert" />
+  <nav id="app-nav" ref="appNav" class="h-20 px-4 lg:px-6 bg-white sticky top-0 z-50">
+    <div class="h-full flex items-center justify-between">
+      <div class="w-1/3">
+        <router-link :to="{ name: 'home' }">
+          <AppIcon v-if="['sm', 'md'].includes(bp)" />
+          <AppLogo v-else />
+        </router-link>
+      </div>
 
-    <div class="px-6">
-      <div class="flex items-center justify-between h-20">
-        <div class="w-1/3">
-          <router-link :to="{ name: 'home' }">
-            <AppIcon v-if="['sm', 'md'].includes(bp)" />
-            <AppLogo v-else />
-          </router-link>
-        </div>
+      <div class="flex-1 md:w-1/3 flex justify-center">
+        <AppNavToggle />
+      </div>
 
-        <div class="w-1/3 flex justify-center">
-          <AppNavToggle />
-        </div>
-
-        <div class="w-1/3 flex justify-end">
-          <div v-if="$auth.isAuthenticated.value" class="flex items-center">
-            <BalBtn
-              v-if="totalPending"
-              tag="a"
-              :href="`https://claim.balancer.finance/#/${account}`"
-              target="_blank"
-              rel="noreferrer"
-              color="gradient"
-              rounded
-              class="mr-2 text-base"
-              size="sm"
-            >
-              ✨ {{ _num(totalPending) }} BAL
-            </BalBtn>
-            <div class="relative">
-              <BalBtn
-                class="auth-btn"
-                :loading="web3Loading"
-                :loading-label="
-                  ['sm', 'md'].includes(bp) ? '' : 'Connecting...'
-                "
-                color="gray"
-                outline
-                rounded
-                :size="['sm', 'md'].includes(bp) ? 'md' : 'sm'"
-                :circle="['sm', 'md'].includes(bp)"
-              >
-                <Avatar :address="account" :profile="profile" size="20" />
-                <span
-                  v-if="profile.name || profile.ens"
-                  v-text="profile.name || profile.ens"
-                  class="pl-2 hidden md:inline-block"
-                />
-                <span
-                  v-else
-                  v-text="_shorten(account)"
-                  class="pl-2 hidden md:inline-block"
-                />
-              </BalBtn>
-              <SettingsPopover v-if="!web3Loading" class="popover" />
-            </div>
-          </div>
-          <BalBtn
-            v-else
-            class="text-base"
-            color="gray"
-            outline
-            rounded
-            :size="['sm', 'md'].includes(bp) ? 'md' : 'sm'"
-            :circle="['sm', 'md'].includes(bp)"
-            @click="setAccountModal(true)"
-          >
-            <span class="hidden md:inline-block" v-text="$t('connectWallet')" />
-            <BalIcon name="log-out" size="sm" class="md:hidden" />
-          </BalBtn>
-        </div>
+      <div class="w-1/3 flex justify-end">
+        <AppNavActions />
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-import { computed, defineComponent, reactive, toRefs, watch } from 'vue';
+import { computed, defineComponent, toRefs, onMounted, onUnmounted, ref } from 'vue';
 import { useStore } from 'vuex';
-import getProvider from '@/utils/provider';
-import { getPendingClaims } from '@/utils/balancer/claim';
 import useBreakpoints from '@/composables/useBreakpoints';
 import AppLogo from '@/components/images/AppLogo.vue';
 import AppIcon from '@/components/images/AppIcon.vue';
 import AppNavAlert from './AppNavAlert.vue';
 import AppNavToggle from './AppNavToggle.vue';
-import useWeb3 from '@/composables/useWeb3';
+import AppNavActions from './AppNavActions.vue'
 
 export default defineComponent({
   components: {
     AppLogo,
     AppIcon,
     AppNavAlert,
-    AppNavToggle
+    AppNavToggle,
+    AppNavActions
   },
 
   setup() {
     // COMPOSABLES
     const store = useStore();
     const { bp } = useBreakpoints();
-    const { account, profile, loading: web3Loading, userNetwork } = useWeb3();
 
     // DATA
-    const data = reactive({
-      pendingClaims: false,
-      totalPending: false
-    });
+    const appNav = ref(null);
 
     // COMPUTED
     const alert = computed(() => store.state.alerts.current);
 
     // METHODS
-    const setAccountModal = val => store.commit('web3/setAccountModal', val);
-
-    async function getClaimsData() {
-      const provider = getProvider(userNetwork.value.key);
-      const pendingClaims = await getPendingClaims(
-        userNetwork.value.key,
-        provider,
-        account.value
-      );
-      data.pendingClaims = pendingClaims;
-      data.totalPending = pendingClaims
-        .map(claim => parseFloat(claim.amount))
-        .reduce((a, b) => a + b, 0);
+    function handleScroll() {
+      if (window.scrollY === 0) {
+        appNav.value.classList.remove('shadow-lg')
+      } else {
+        appNav.value.classList.add('shadow-lg')
+      }
     }
 
-    // WATCHERS
-    watch(account, newAccount => {
-      data.pendingClaims = false;
-      data.totalPending = false;
-      if (newAccount) getClaimsData();
+    // CALLBACKS
+    onMounted(() => {
+      window.addEventListener('scroll', handleScroll);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener('scroll', handleScroll);
     });
 
     return {
       // data
-      ...toRefs(data),
+      appNav,
       // computed
-      account,
-      profile,
-      web3Loading,
       bp,
-      alert,
-      // methods
-      setAccountModal
+      alert
     };
   }
 });
 </script>
 
 <style scoped>
-#topnav {
-  @apply w-full shadow;
+#app-nav {
+  @apply w-full;
   @apply bg-white dark:bg-gray-900;
   @apply border-b border-transparent dark:border-gray-700;
+  transition: all .2s ease-in-out;
 }
 
 .auth-btn:hover ~ .popover {
