8059928137d25760b8727c0b05cd22499c5b9df8
Evgeny Boxer <evgeny.boxer@gmail.com>
2021-04-21 15:07:58 +1000

    feat: add support to pass multiple txs for txListener

--- src/composables/useNotify.ts
+++ src/composables/useNotify.ts
@@ -1,4 +1,5 @@
 import { TransactionEventCode, TransactionData } from 'bnc-notify';
+import castArray from 'lodash/castArray';
 
 import useBlocknative from './useBlocknative';
 import useWeb3 from './useWeb3';
@@ -10,7 +11,7 @@ export default function useNotify() {
   const { explorer } = useWeb3();
 
   function txListener(
-    txHash: string,
+    txs: string | string[],
     {
       onTxConfirmed,
       onTxCancel,
@@ -21,41 +22,43 @@ export default function useNotify() {
       onTxFailed?: TxCallback;
     }
   ) {
-    const { emitter } = notify.hash(txHash);
+    castArray(txs).forEach(txHash => {
+      const { emitter } = notify.hash(txHash);
 
-    const defaultNotificationParams = {
-      link: explorer.txLink(txHash)
-    };
+      const defaultNotificationParams = {
+        link: explorer.txLink(txHash)
+      };
 
-    // apply notification defaults to all types
-    emitter.on('all', () => {
-      return defaultNotificationParams;
-    });
-
-    const eventsMap: Partial<Record<
-      TransactionEventCode,
-      TxCallback | undefined
-    >> = {
-      txConfirmed: onTxConfirmed,
-      txCancel: onTxCancel,
-      txFailed: onTxFailed
-    };
-
-    // register to events that have a callback
-    Object.entries(eventsMap)
-      .filter(([, txCallback]) => txCallback != null)
-      .forEach(([eventName, txCallback]) => {
-        emitter.on(
-          eventName as TransactionEventCode,
-          (txData: TransactionData) => {
-            if (txCallback != null) {
-              txCallback(txData);
-            }
-
-            return defaultNotificationParams;
-          }
-        );
+      // apply notification defaults to all types
+      emitter.on('all', () => {
+        return defaultNotificationParams;
       });
+
+      const eventsMap: Partial<Record<
+        TransactionEventCode,
+        TxCallback | undefined
+      >> = {
+        txConfirmed: onTxConfirmed,
+        txCancel: onTxCancel,
+        txFailed: onTxFailed
+      };
+
+      // register to events that have a callback
+      Object.entries(eventsMap)
+        .filter(([, txCallback]) => txCallback != null)
+        .forEach(([eventName, txCallback]) => {
+          emitter.on(
+            eventName as TransactionEventCode,
+            (txData: TransactionData) => {
+              if (txCallback != null) {
+                txCallback(txData);
+              }
+
+              return defaultNotificationParams;
+            }
+          );
+        });
+    });
   }
 
   return { txListener };
