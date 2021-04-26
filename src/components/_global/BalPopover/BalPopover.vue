8beac52a6ea0381a3606210578cc6c8a8f129b12
Evgeny Boxer <evgeny.boxer@gmail.com>
2021-04-26 20:46:47 +1000

    Add "click" trigger to BalPopover and set it as default (#201)
    * feat: add "click" trigger and set it as default
    
    * fix: use `size` attr instead of `:size`

--- src/components/_global/BalPopover/BalPopover.vue
+++ src/components/_global/BalPopover/BalPopover.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="relative">
-    <div class="bal-popover-activator">
+  <div class="relative" v-click-outside="handleClickOutside">
+    <div
+      class="bal-popover-activator"
+      @click="trigger === 'click' && togglePopover()"
+      @mouseenter="trigger === 'hover' && showPopover()"
+      @mouseleave="trigger === 'hover' && hidePopover()"
+    >
       <slot name="activator" />
     </div>
-    <div class="bal-popover-wrapper">
+    <div :class="popoverWrapperClasses">
       <BalCard shadow="lg" v-bind="$attrs">
         <slot />
       </BalCard>
@@ -12,26 +17,70 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, computed, PropType } from 'vue';
+
+type PopoverTrigger = 'click' | 'hover';
 
 export default defineComponent({
   name: 'BalPopover',
 
   props: {
-    show: { type: Boolean, default: false },
-    title: { type: String, default: '' }
+    trigger: {
+      type: String as PropType<PopoverTrigger>,
+      default: 'click'
+    }
+  },
+  setup(props) {
+    // DATA
+    const popoverOpened = ref(false);
+
+    // METHODS
+    function togglePopover() {
+      popoverOpened.value = !popoverOpened.value;
+    }
+
+    function showPopover() {
+      popoverOpened.value = true;
+    }
+
+    function hidePopover() {
+      popoverOpened.value = false;
+    }
+
+    function handleClickOutside() {
+      if (props.trigger === 'click') {
+        hidePopover();
+      }
+    }
+
+    // COMPUTED
+    const popoverWrapperClasses = computed(() => ({
+      'bal-popover-wrapper': true,
+      'bal-popover-wrapper-visible': popoverOpened.value
+    }));
+
+    return {
+      // methods
+      togglePopover,
+      showPopover,
+      hidePopover,
+      handleClickOutside,
+
+      // computed
+      popoverWrapperClasses
+    };
   }
 });
 </script>
 
 <style>
-.bal-popover-activator:hover ~ .bal-popover-wrapper {
-  @apply visible opacity-100;
-}
-
 .bal-popover-wrapper {
   @apply top-full invisible opacity-0 absolute z-10 pt-3 right-0;
   transition: all 0.2s ease-in-out;
+}
+
+.bal-popover-wrapper-visible {
+  @apply visible opacity-100;
 }
 
 .bal-popover-wrapper:hover {
