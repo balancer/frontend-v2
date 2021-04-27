6cdc7ad11ba5136fe645d9df537dd3e4ce29db99
EndymionJkb <EndymionJkb@gmail.com>
2021-04-27 12:50:35 -0300

    Update src/components/tables/PoolsTable.vue
    Co-authored-by: Gareth Fuller <gareth.a.f@gmail.com>

--- src/components/tables/PoolsTable.vue
+++ src/components/tables/PoolsTable.vue
@@ -39,7 +39,7 @@
             class="inline-block mr-1"
           >
             <span class="dot">•</span>
-            {{ _num(token.weight * 100, 'percent') }}
+            {{ fNum(token.weight * 100, 'percent') }}
             {{ allTokens[getAddress(token.address)].symbol }}
           </span>
         </div>
