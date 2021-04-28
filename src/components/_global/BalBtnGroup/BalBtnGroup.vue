fef69e261dc702629bbfad6406249ec5d635ee7c
Evgeny Boxer <evgeny.boxer@gmail.com>
2021-04-28 12:04:18 +1000

    feat: new BalBtnGroup component based on feedback

---
+++ src/components/_global/BalBtnGroup/BalBtnGroup.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flex">
+    <BalBtn
+      v-for="option in options"
+      :key="option.value"
+      @click="onSelect(option)"
+      :active="modelValue === option.value"
+      v-bind="$attrs"
+    >
+      {{ option.label }}
+    </BalBtn>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from 'vue';
+import omit from 'lodash/omit';
+
+interface Option {
+  value: string | number;
+  label: string;
+}
+
+export default defineComponent({
+  name: 'BalBtnGroup',
+
+  inheritAttrs: false,
+
+  emits: ['update:modelValue'],
+
+  props: {
+    modelValue: { type: [String, Number], required: true },
+    options: { type: Array as PropType<Option[]>, required: true }
+  },
+
+  setup(props, { emit, attrs }) {
+    // COMPUTED
+    const attrs_ = computed(() => omit(attrs, 'options'));
+
+    // METHODS
+    function onSelect(option: Option) {
+      emit('update:modelValue', option.value);
+    }
+
+    return {
+      // computed
+      attrs_,
+
+      // methods
+      onSelect
+    };
+  }
+});
+</script>
