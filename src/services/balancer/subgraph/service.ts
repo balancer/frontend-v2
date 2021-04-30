0d7efe779d0ebad0d5e0323369369cc0a48ab0fb
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-30 11:56:10 +0100

    Class refactor

--- src/services/balancer/subgraph/service.ts
+++ src/services/balancer/subgraph/service.ts
@@ -6,23 +6,15 @@ import PoolShares from './entities/poolShares';
 export default class Service {
   client: Client;
   infuraService: InfuraService;
-  poolsInstance: Pools;
-  poolSharesInstance: PoolShares;
+  pools: Pools;
+  poolShares: PoolShares;
 
   constructor(client = new Client(), infuraService = new InfuraService()) {
     this.client = client;
     this.infuraService = infuraService;
 
     // Init entities
-    this.poolsInstance = new Pools(this);
-    this.poolSharesInstance = new PoolShares(this);
-  }
-
-  public get pools(): Pools {
-    return this.poolsInstance;
-  }
-
-  public get poolShares(): PoolShares {
-    return this.poolSharesInstance;
+    this.pools = new Pools(this);
+    this.poolShares = new PoolShares(this);
   }
 }
