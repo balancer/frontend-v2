25e0787e944cc0e98cbe5a700d36eec9c874e294
Gareth Fuller <gareth.a.f@gmail.com>
2021-04-30 11:53:37 +0100

    Only init entity classes once

--- src/services/balancer/subgraph/service.ts
+++ src/services/balancer/subgraph/service.ts
@@ -6,17 +6,23 @@ import PoolShares from './entities/poolShares';
 export default class Service {
   client: Client;
   infuraService: InfuraService;
+  poolsInstance: Pools;
+  poolSharesInstance: PoolShares;
 
   constructor(client = new Client(), infuraService = new InfuraService()) {
     this.client = client;
     this.infuraService = infuraService;
+
+    // Init entities
+    this.poolsInstance = new Pools(this);
+    this.poolSharesInstance = new PoolShares(this);
   }
 
   public get pools(): Pools {
-    return new Pools(this);
+    return this.poolsInstance;
   }
 
   public get poolShares(): PoolShares {
-    return new PoolShares(this);
+    return this.poolSharesInstance;
   }
 }
