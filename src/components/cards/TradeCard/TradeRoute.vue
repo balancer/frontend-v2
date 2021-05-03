1555a23fb30be2ad62104c7e959dcefcff58682a
Timur Badretdinov <destinerx@gmail.com>
2021-05-03 14:51:31 +0300

    Add TradeRoute component

---
+++ src/components/cards/TradeCard/TradeRoute.vue
@@ -0,0 +1,393 @@
+<template>
+  <div>
+    <div
+      class="flex text-gray-500 items-center cursor-pointer"
+      @click="toggleVisibility"
+    >
+      <div class="mr-2">
+        {{ label }}
+      </div>
+      <BalIcon v-if="visible" name="chevron-up" size="sm" />
+      <BalIcon v-else name="chevron-down" size="sm" />
+    </div>
+    <div v-if="visible" class="mt-5">
+      <div v-if="routes.length === 0" class="mt-5 text-sm text-gray-500">
+        No data available
+      </div>
+      <div v-else>
+        <div>
+          <div class="flex justify-between text-xs">
+            <div>
+              <div class="font-bold">
+                {{ input.amount }}
+              </div>
+              <div>
+                {{ input.symbol }}
+              </div>
+            </div>
+            <div class="flex flex-col items-end">
+              <div class="font-bold">
+                {{ output.amount }}
+              </div>
+              <div>
+                {{ output.symbol }}
+              </div>
+            </div>
+          </div>
+          <div class="relative mt-2">
+            <div
+              class="pair-line absolute h-1/2 mx-9 border-b border-dashed border-gray-500"
+            />
+            <div class="relative z-10 flex justify-between">
+              <BalAsset :address="input.address" :size="36" />
+              <BalAsset :address="output.address" :size="36" />
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex justify-between"
+          :style="{ margin: `8px ${12 + routes.length}px` }"
+        >
+          <BalIcon
+            name="triangle"
+            size="xxs"
+            :filled="true"
+            class="transform rotate-180 text-gray-500"
+          />
+          <BalIcon
+            name="triangle"
+            size="xxs"
+            :filled="true"
+            class="text-gray-500"
+          />
+        </div>
+        <div class="relative my-1.5 mx-4">
+          <div
+            v-for="(route, index) in routes"
+            :key="index"
+            :style="{
+              height: `${19 + 72 * index}px`,
+              width: `calc(100% - ${4 * (routes.length - index - 1)}px + 1px)`,
+              margin: `0 ${2 * (routes.length - index - 1) - 1}px`
+            }"
+            class="absolute border-l border-r border-b border-gray-500 rounded-b-md"
+          />
+          <div class="relative z-10">
+            <div
+              v-for="route in routes"
+              :key="route.hops[0].pool.address"
+              class="mt-9 first:mt-0 flex justify-between"
+            >
+              <div class="w-4 ml-4 flex items-center">
+                <BalIcon
+                  name="triangle"
+                  size="xxs"
+                  :filled="true"
+                  class="transform rotate-90 text-gray-500"
+                />
+              </div>
+              <div class="flex">
+                <div
+                  v-for="hop in route.hops"
+                  :key="hop.pool.address"
+                  class="p-1.5 ml-4 first:ml-0 flex bg-white rounded shadow"
+                >
+                  <a :href="getPoolLink(hop.pool.address)" target="_blank">
+                    <BalAsset
+                      class="ml-1.5 first:ml-0"
+                      v-for="token in hop.pool.tokens"
+                      :key="token.address"
+                      :address="token.address"
+                      :size="20"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div class="w-10 mr-4 text-xs text-right text-gray-500">
+                {{ formatShare(route.share) }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import BigNumber from 'bignumber.js';
+import { PropType, defineComponent, ref, computed } from 'vue';
+import { useStore } from 'vuex';
+import { getAddress } from '@ethersproject/address';
+import { AddressZero } from '@ethersproject/constants';
+import { Pool, Swap } from '@balancer-labs/sor/dist/types';
+import { SwapV2, SubgraphPoolBase } from '@balancer-labs/sor2';
+
+import useNumbers from '@/composables/useNumbers';
+import { SorReturn } from '@/utils/balancer/helpers/sor/sorManager';
+
+interface Route {
+  share: number;
+  hops: Hop[];
+}
+
+interface Hop {
+  pool: {
+    address: string;
+    tokens: Asset[];
+  };
+  tokenIn: string;
+  tokenOut: string;
+}
+
+interface Asset {
+  address: string;
+  share: number;
+}
+
+export default defineComponent({
+  props: {
+    addressIn: {
+      type: String,
+      required: true
+    },
+    amountIn: {
+      type: String,
+      required: true
+    },
+    addressOut: {
+      type: String,
+      required: true
+    },
+    amountOut: {
+      type: String,
+      required: true
+    },
+    pools: {
+      type: Array as PropType<(Pool | SubgraphPoolBase)[]>,
+      required: true
+    },
+    sorReturn: {
+      type: Object as PropType<SorReturn>,
+      required: true
+    }
+  },
+  setup(props) {
+    const store = useStore();
+    const { fNum } = useNumbers();
+
+    const getConfig = () => store.getters['web3/getConfig']();
+    const getTokens = (params = {}) =>
+      store.getters['registry/getTokens'](params);
+    const tokens = computed(() => getTokens({ includeEther: true }));
+
+    const visible = ref(false);
+
+    function toggleVisibility(): void {
+      visible.value = !visible.value;
+    }
+
+    const label = computed(() => {
+      const version = props.sorReturn.isV1swap ? 'V1' : 'V2';
+      return `Using ${version} liquidity`;
+    });
+
+    const input = computed(() => {
+      const symbol = tokens.value[props.addressIn].symbol;
+      return {
+        amount: props.amountIn,
+        address: props.addressIn,
+        symbol
+      };
+    });
+
+    const output = computed(() => {
+      const symbol = tokens.value[props.addressOut].symbol;
+      return {
+        amount: props.amountOut,
+        address: props.addressOut,
+        symbol
+      };
+    });
+
+    const routes = computed(() => {
+      const { sorReturn } = props;
+
+      if (sorReturn.isV1swap) {
+        const pools = props.pools as Pool[];
+        const swaps = sorReturn.v1result[0];
+
+        return getV1Routes(pools, swaps);
+      } else {
+        const pools = props.pools as SubgraphPoolBase[];
+        const swaps = sorReturn.v2result.swaps;
+        const addresses = sorReturn.v2result.tokenAddresses;
+
+        return getV2Routes(pools, swaps, addresses);
+      }
+    });
+
+    function getV1Routes(pools: Pool[], swaps: Swap[][]) {
+      if (!pools.length || !swaps.length) {
+        return [];
+      }
+
+      const totalSwapAmount = swaps.reduce((total, rawHops) => {
+        return total.plus(rawHops[0].swapAmount || '0');
+      }, new BigNumber(0));
+      const routes = swaps.map(rawHops => {
+        const swapAmount = new BigNumber(rawHops[0].swapAmount || '0');
+        const share = swapAmount.div(totalSwapAmount).toNumber();
+        const hops = rawHops.map(rawHop => {
+          const tokenIn = getAddress(rawHop.tokenIn);
+          const tokenOut = getAddress(rawHop.tokenOut);
+          const rawPool = pools.find(pool => pool.id === rawHop.pool);
+          if (!rawPool) {
+            return {};
+          }
+          const totalWeight = new BigNumber(rawPool.totalWeight);
+          const pool = {
+            address: rawPool.id,
+            tokens: rawPool.tokens
+              .map(token => {
+                const address = getAddress(token.address);
+                const weight = new BigNumber(token.denormWeight);
+                const share = weight.div(totalWeight).toNumber();
+                return {
+                  address,
+                  share
+                };
+              })
+              .sort((a, b) => {
+                if (a.address === tokenIn || b.address === tokenOut) {
+                  return -1;
+                }
+                if (a.address === tokenOut || b.address === tokenIn) {
+                  return 1;
+                }
+                return a.share - b.share;
+              })
+              .filter((_token, index, tokens) => {
+                // Show first 2 and last 2 tokens
+                return index < 2 || index > tokens.length - 3;
+              })
+          };
+          return {
+            pool,
+            tokenIn,
+            tokenOut
+          };
+        });
+        return {
+          share,
+          hops
+        };
+      }) as Route[];
+
+      return routes;
+    }
+
+    function getV2Routes(
+      pools: SubgraphPoolBase[],
+      swaps: SwapV2[],
+      addresses: string[]
+    ) {
+      if (!pools.length || !swaps.length || !addresses.length) {
+        return [];
+      }
+
+      const hops = swaps.map((swap, i) => {
+        const rawPool = pools.find(pool => pool.id === swap.poolId);
+
+        if (!rawPool) {
+          return {};
+        }
+
+        const { addresses: constants } = getConfig();
+        const tokenIn =
+          addresses[i] === AddressZero
+            ? constants.weth
+            : getAddress(addresses[i]);
+        const tokenOut =
+          addresses[i + 1] === AddressZero
+            ? constants.weth
+            : getAddress(addresses[i + 1]);
+
+        const pool = {
+          address: rawPool.address,
+          tokens: rawPool.tokens
+            .map(token => {
+              return {
+                address: getAddress(token.address),
+                share:
+                  parseFloat(token.weight || '') || 1 / rawPool.tokens.length
+              };
+            })
+            .sort((a, b) => {
+              if (a.address === tokenIn || b.address === tokenOut) {
+                return -1;
+              }
+              if (a.address === tokenOut || b.address === tokenIn) {
+                return 1;
+              }
+              return a.share - b.share;
+            })
+            .filter((_token, index, tokens) => {
+              // Show first 2 and last 2 tokens
+              return index < 2 || index > tokens.length - 3;
+            })
+        };
+
+        const hop = {
+          pool,
+          tokenIn,
+          tokenOut
+        };
+
+        return hop;
+      });
+      const route = {
+        share: 1,
+        hops
+      } as Route;
+
+      return [route];
+    }
+
+    function formatShare(share: number): string {
+      return fNum(share, 'percent');
+    }
+
+    function getPoolLink(address: string): string {
+      const { chainId } = getConfig();
+      const prefixMap = {
+        1: '',
+        42: 'kovan.'
+      };
+      const prefix = prefixMap[chainId] || '';
+      return props.sorReturn.isV1swap
+        ? `https://${prefix}pools.balancer.exchange/#/pool/${address}`
+        : `https://${prefix}app.balancer.fi/#pool/${address}`;
+    }
+
+    return {
+      visible,
+      toggleVisibility,
+
+      label,
+      input,
+      output,
+      routes,
+
+      formatShare,
+      getPoolLink
+    };
+  }
+});
+</script>
+
+<style scoped>
+.pair-line {
+  width: calc(100% - 72px);
+}
+</style>
