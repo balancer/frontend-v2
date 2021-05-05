00e74bc6d60be5a04b6c76419910dd6e76706885
Jeffrey Bennett <endymionjkb@gmail.com>
2021-05-05 19:39:14 -0400

    Reduce mixin usage

--- src/plugins/mixins.ts
+++ src/plugins/mixins.ts
@@ -1,26 +1,7 @@
-import numeral from 'numeral';
-import prettyMs from 'pretty-ms';
-import configs from '@/config';
 import { shorten } from '@/utils';
-import { formatUnits } from '@ethersproject/units';
 
 export default {
   methods: {
-    _ms(number) {
-      const diff = number * 1e3 - new Date().getTime();
-      return prettyMs(diff);
-    },
-    _num(number, format) {
-      if (!format) {
-        format = '(0.[0]a)';
-        if (number < 1) format = '0.[000]';
-        if (number == 0) return '-';
-      }
-      return numeral(number).format(format);
-    },
-    _units(bn, decimals) {
-      return formatUnits(bn, decimals);
-    },
     _shorten(str: string, key: string): string {
       if (!str) return str;
       let limit;
@@ -39,9 +20,6 @@ export default {
       return url
         .replace('ipfs://', `https://${process.env.VUE_APP_IPFS_NODE}/ipfs/`)
         .replace('ipns://', `https://${process.env.VUE_APP_IPFS_NODE}/ipns/`);
-    },
-    _explorer(network, str: string, type = 'address'): string {
-      return `${configs[network].explorer}/${type}/${str}`;
     }
   }
 };
