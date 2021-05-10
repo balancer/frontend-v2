bfdab76ad537e6d0eb9fa57930949fe3a3eacc7e
Gareth Fuller <gareth.a.f@gmail.com>
2021-05-11 00:09:36 +0100

    Add wrap eth link to invest form (#318)

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,5 +1,9 @@
 export const TOKENS = {
   Popular: {
     Symbols: ['WBTC', 'DAI', 'USDC', 'BAL', 'AAVE', 'WETH']
+  },
+  AddressMap: {
+    ETH: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+    WETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
   }
 };
