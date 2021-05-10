162a7c4874529c2b9f56bfc507eb4e14fa1b2175
Gareth Fuller <gareth.a.f@gmail.com>
2021-05-10 23:22:07 +0100

    Add constants

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -5,5 +5,8 @@ export const EXTERNAL_LINKS = {
       'https://docs.balancer.finance/core-concepts/bal-balancer-governance-token/bal-for-gas',
     Claim: (account: string) => `https://claim.balancer.finance/#/${account}`,
     PoolsV1Dashboard: 'https://pools.balancer.exchange/#/dashboard'
+  },
+  Gauntlet: {
+    Home: 'https://gauntlet.network'
   }
 };
