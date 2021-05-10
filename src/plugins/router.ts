fc78566a9a246d4f53c0fd41eadd502f4a7d97b5
Gareth Fuller <gareth.a.f@gmail.com>
2021-05-10 21:08:15 +0100

    Lint fix

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -6,9 +6,12 @@ import Trade from '@/views/Trade.vue';
 const routes: any[] = [
   { path: '/', name: 'home', component: Home },
   { path: '/trade/:assetIn?/:assetOut?', name: 'trade', component: Trade },
-  { path: '/swap/:assetIn?/:assetOut?', redirect: to => {
-    return `/trade${to.path.split('/swap')[1]}`
-  }},
+  {
+    path: '/swap/:assetIn?/:assetOut?',
+    redirect: to => {
+      return `/trade${to.path.split('/swap')[1]}`;
+    }
+  },
   { path: '/pool/:id', name: 'pool', component: Pool },
   { path: '/:pathMatch(.*)*', name: 'not-found', redirect: '/' }
 ];
