2bad91c198f40b020656424ea2a7cfea9049187a
Gareth Fuller <gareth.a.f@gmail.com>
2021-05-11 10:53:22 +0100

    Let BalTable handle missing results

--- src/components/tables/PoolsTable.vue
+++ src/components/tables/PoolsTable.vue
@@ -41,12 +41,6 @@
         </div>
       </template>
     </BalTable>
-    <div
-      v-if="!isLoading && data && data.length === 0"
-      class="h-40 border-t -mt-px flex items-center justify-center text-gray-500 font-medium"
-    >
-      {{ noPoolsLabel }}
-    </div>
   </BalCard>
 </template>
 
