1f8cda206427366a8d6269f570e6261c731686fc
Gareth Fuller <gareth.a.f@gmail.com>
2021-05-12 20:52:08 +0100

    Remove legacy global component import

--- src/plugins/components.ts
+++ src/plugins/components.ts
@@ -1,7 +1,5 @@
 import { App } from 'vue';
 import parsePath from 'parse-filepath';
-import upperFirst from 'lodash/upperFirst';
-import camelCase from 'lodash/camelCase';
 
 export function registerGlobalComponents(app: App): void {
   // Load global components from @/components/_global
@@ -16,20 +14,4 @@ export function registerGlobalComponents(app: App): void {
     const componentConfig = req(filePath);
     app.component(componentName, componentConfig.default || componentConfig);
   }
-
-  // Legacy import of all components with naming based on directory structure
-  // This should be phased out by adding non-global folders to the excluded regex
-  // Once all auto imported components are removed we should remove this
-  const requireComponent = require.context(
-    '@/components',
-    true,
-    /^((?![\\/]cards|[\\/]dialogs|[\\/]forms|[\\/]modals|[\\/]images|[\\/]navs|[\\/]tables|[\\/]heros).)*\.vue$/
-  );
-  requireComponent.keys().forEach(fileName => {
-    const componentConfig = requireComponent(fileName);
-    const componentName = upperFirst(
-      camelCase(fileName.replace(/^\.\//, '').replace(/\.\w+$/, ''))
-    );
-    app.component(componentName, componentConfig.default || componentConfig);
-  });
 }
