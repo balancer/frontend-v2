bdb312db80a842f35a6678db3e550e7c7fae2d0f
Ansaf Ahmed <me@ansaf.net>
2021-06-02 21:25:24 +1000

    lint

--- src/composables/useOnboard.ts
+++ src/composables/useOnboard.ts
@@ -14,11 +14,10 @@ export default function useOnboard() {
   const web3State = inject<Web3State>(Web3ProviderSymbol);
 
   const ding = async () => {
-      await web3State?.onboardInstance.value?.walletSelect();
-      console.log('boing');
+    await web3State?.onboardInstance.value?.walletSelect();
+    console.log('boing');
     await web3State?.onboardInstance.value?.walletCheck();
-      console.log('doing');
-
+    console.log('doing');
   };
 
   return {
