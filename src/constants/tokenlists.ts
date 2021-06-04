08fb0cdd82c9c4b02ba05ca4660f6f106bbab30d
Gareth Fuller <gareth.a.f@gmail.com>
2021-06-04 17:40:11 +0100

    Update token lists constants

--- src/constants/tokenlists.ts
+++ src/constants/tokenlists.ts
@@ -1,6 +1,8 @@
+// Compliant list for exchange
 export const TOKEN_LIST_DEFAULT =
   'https://storageapi.fleek.co/balancer-team-bucket/assets/listed.tokenlist.json';
 
+// Extended list to include LBP tokens
 export const ELIGIBLE_TOKEN_LIST =
   'https://storageapi.fleek.co/balancer-team-bucket/assets/vetted.tokenlist.json';
 
@@ -22,4 +24,17 @@ export const ETHER = {
   decimals: 18,
   logoURI:
     'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png'
+};
+
+export default {
+  All: [
+    TOKEN_LIST_DEFAULT,
+    ELIGIBLE_TOKEN_LIST,
+    'ipns://tokens.uniswap.org',
+    'tokenlist.zerion.eth',
+    'tokens.1inch.eth',
+    'tokenlist.aave.eth',
+    'https://tokens.coingecko.com/uniswap/all.json',
+    'https://umaproject.org/uma.tokenlist.json'
+  ]
 };
