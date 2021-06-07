debe72e6e0e5f8d5f90a747a9395fe459ad6660f
Gareth Fuller <gareth.a.f@gmail.com>
2021-06-07 15:27:30 +0100

    Add list toggle functionality

--- src/composables/useTokenLists2.ts
+++ src/composables/useTokenLists2.ts
@@ -3,12 +3,16 @@ import TokenListService from '@/services/token-list/token-list.service';
 import { TokenListGroup } from '@/types/TokenList';
 import { pick } from 'lodash';
 import TOKEN_LISTS from '@/constants/tokenlists';
+import { lsGet, lsSet } from '@/lib/utils';
+import LS_KEYS from '@/constants/local-storage.keys';
 
 // SERVICES
 const tokenListService = new TokenListService();
 // State
 const tokenLists = ref<TokenListGroup>({});
-// const toggledLists = ref<string[]>([]);
+const toggledLists = ref<string[]>(
+  lsGet(LS_KEYS.TokenLists.Toggled, [TOKEN_LISTS.Balancer.Default])
+);
 const loading = ref(true);
 const failed = ref(false);
 
@@ -39,11 +43,26 @@ export default function useTokenLists2() {
     pick(tokenLists.value, TOKEN_LISTS.Exchange)
   );
 
+  function toggleList(uri: string): void {
+    if (!TOKEN_LISTS.Exchange.includes(uri)) return;
+
+    if (toggledLists.value.includes(uri)) {
+      toggledLists.value.splice(toggledLists.value.indexOf(uri), 1);
+    } else {
+      toggledLists.value.push(uri)
+    }
+
+    lsSet(LS_KEYS.TokenLists.Toggled, toggledLists.value);
+  }
+
   return {
     loading,
+    failed,
     all: readonly(tokenLists),
     balancer: balancerLists,
     exchange: exchangeLists,
-    vettedList
+    vettedList,
+    toggledLists,
+    toggleList
   };
 }
