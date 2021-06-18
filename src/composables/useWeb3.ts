bce8b9682977cdb80772805f00822af015d662e7
Tom French <15848336+TomAFrench@users.noreply.github.com>
2021-06-18 09:29:57 +0100

    Hide trade liquidity UI on networks where V1 isn't deployed (#436)
    * feat: hide Trade Liquidity UI on networks where V1 isn't deployed
    
    * refactor: use ConfigService in useWeb3

--- src/composables/useWeb3.ts
+++ src/composables/useWeb3.ts
@@ -1,13 +1,11 @@
 import { computed } from 'vue';
 import { useStore } from 'vuex';
 
-import configs, { Config } from '@/lib/config';
 import getProvider from '@/lib/utils/provider';
 import useAuth from '@/composables/useAuth';
 import { NetworkId } from '@/constants/network';
-
-const appConfig: Config | undefined =
-  configs[Number(process.env.VUE_APP_NETWORK)];
+import { isAddress } from '@ethersproject/address';
+import ConfigService from '@/services/config/config.service';
 
 export default function useWeb3() {
   const store = useStore();
@@ -19,13 +17,16 @@ export default function useWeb3() {
   const loading = computed(() => store.state.web3.loading);
   const isConnected = computed(() => isAuthenticated.value && !loading.value);
 
+  const configService = new ConfigService();
+
   // App Network vars (static)
   const appNetwork = {
-    key: process.env.VUE_APP_NETWORK || '1',
-    id: (Number(process.env.VUE_APP_NETWORK) || 1) as NetworkId,
-    name: appConfig?.shortName || 'Mainnet',
-    networkName: appConfig?.network || 'homestead',
-    nativeAsset: appConfig?.nativeAsset || 'ETH'
+    key: configService.env.NETWORK,
+    id: Number(configService.env.NETWORK) as NetworkId,
+    name: configService.network.shortName,
+    networkName: configService.network.network,
+    nativeAsset: configService.network.nativeAsset,
+    supportsV1: isAddress(configService.network.addresses.exchangeProxy)
   };
 
   // User network vars (dynamic)
@@ -56,7 +57,7 @@ export default function useWeb3() {
     );
   });
 
-  const explorerBaseURL = configs[appNetwork.id].explorer;
+  const explorerBaseURL = configService.network.explorer;
 
   // assumes etherscan.io
   const explorer = {
