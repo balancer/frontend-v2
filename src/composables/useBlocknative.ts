6afaaa1e8c1e0b1a99705678fe215c497555202c
Gareth Fuller <gareth.a.f@gmail.com>
2021-06-22 11:04:03 +0100

    Update blocknative notify & remove unused sdk pkg

--- src/composables/useBlocknative.ts
+++ src/composables/useBlocknative.ts
@@ -1,18 +1,12 @@
-import BlocknativeSdk from 'bnc-sdk';
 import Notify from 'bnc-notify';
 import { inject } from 'vue';
-import { bnNotifySymbol, defaultOptions } from '@/plugins/blocknative';
+import { bnNotifySymbol } from '@/plugins/blocknative';
 
 export default function useBlocknative() {
   const notify = inject(bnNotifySymbol) as ReturnType<typeof Notify>;
   if (!notify) throw new Error('Blocknative Notify missing!');
 
-  function blocknative(networkId: number): BlocknativeSdk {
-    return new BlocknativeSdk(Object.assign({}, defaultOptions, { networkId }));
-  }
-
   return {
-    blocknative,
     notify
   };
 }
