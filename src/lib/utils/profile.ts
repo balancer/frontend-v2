87d6c6137ae271f9ba3b515b03a05231961a15f1
Ansaf Ahmed <me@ansaf.net>
2021-06-24 18:20:15 +1000

    update: cleanup by removing the 3boxprofile fetcher method

--- src/lib/utils/profile.ts
+++ src/lib/utils/profile.ts
@@ -1,26 +1,4 @@
-import { subgraphRequest } from '@/lib/utils/subgraph';
 import getProvider from '@/lib/utils/provider';
-
-async function get3BoxProfile(address: string): Promise<Record<any, any>> {
-  try {
-    const {
-      profiles: [profile]
-    } = await subgraphRequest('https://api.3box.io/graph', {
-      profiles: {
-        __args: {
-          ids: [address]
-        },
-        name: true,
-        eth_address: true,
-        image: true
-      }
-    });
-    return profile;
-  } catch (error) {
-    console.error(error);
-    return {};
-  }
-}
 
 async function getEnsName(
   address: string,
