7362af0e20409ee8716042bb3357b6d06b394fd8
Gareth Fuller <gareth.a.f@gmail.com>
2021-07-04 13:57:14 +0100

    Make dark mode toggle responsive

--- src/components/btns/DarkModeToggle.vue
+++ src/components/btns/DarkModeToggle.vue
@@ -1,11 +1,19 @@
 <template>
-  <BalBtn color="gray" size="sm" outline circle @click="toggleDarkMode">
+  <BalBtn
+    color="gray"
+    :size="upToLargeBreakpoint ? 'md' : 'sm'"
+    outline
+    circle
+    @click="toggleDarkMode"
+    class="hidden xs:inline-block"
+  >
     <BalIcon v-if="darkMode" name="moon" />
     <BalIcon v-else name="sun" />
   </BalBtn>
 </template>
 
 <script lang="ts">
+import useBreakpoints from '@/composables/useBreakpoints';
 import useDarkMode from '@/composables/useDarkMode';
 import { defineComponent } from 'vue';
 
@@ -13,9 +21,11 @@ export default defineComponent({
   name: 'DarkModeToggle',
 
   setup() {
+    // COMPOSABLES
+    const { upToLargeBreakpoint } = useBreakpoints();
     const { darkMode, toggleDarkMode } = useDarkMode();
 
-    return { darkMode, toggleDarkMode };
+    return { darkMode, toggleDarkMode, upToLargeBreakpoint };
   }
 });
 </script>
