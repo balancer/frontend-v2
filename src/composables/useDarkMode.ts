c66f9f4f3a68d06a11843f977fa99cb23aceb374
Gareth Fuller <gareth.a.f@gmail.com>
2021-07-09 11:21:09 +0100

    Respect OS dark mode on initial load

--- src/composables/useDarkMode.ts
+++ src/composables/useDarkMode.ts
@@ -2,8 +2,10 @@ import { ref } from 'vue';
 import { lsGet, lsSet } from '@/lib/utils';
 import LS_KEYS from '@/constants/local-storage.keys';
 
+const osDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
 // STATE
-const darkMode = ref<boolean>(lsGet(LS_KEYS.App.DarkMode, false));
+const darkMode = ref<boolean>(lsGet(LS_KEYS.App.DarkMode, osDarkMode));
 
 // MUTATIONS
 function setDarkMode(val: boolean): void {
