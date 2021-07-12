0f8c857399d69799085615abc392105de38258df
Gareth Fuller <gareth.a.f@gmail.com>
2021-07-12 16:52:51 +0100

    Revert web3 to jsonRpc provider change

--- src/lib/utils/balancer/tokens.ts
+++ src/lib/utils/balancer/tokens.ts
@@ -1,4 +1,4 @@
-import { JsonRpcProvider, TransactionResponse } from '@ethersproject/providers';
+import { Web3Provider, TransactionResponse } from '@ethersproject/providers';
 import { BigNumber } from '@ethersproject/bignumber';
 import { MaxUint256 } from '@ethersproject/constants';
 import { multicall, Multicaller } from '@/lib/utils/balancer/contract';
@@ -115,7 +115,7 @@ export async function getTokensMeta(
 }
 
 export async function approveTokens(
-  web3: JsonRpcProvider,
+  web3: Web3Provider,
   spender: string,
   tokens: string[]
 ): Promise<TransactionResponse[]> {
--- src/services/claim.ts
+++ src/services/claim.ts
@@ -1,5 +1,5 @@
 import { parseUnits } from '@ethersproject/units';
-import { TransactionResponse, JsonRpcProvider } from '@ethersproject/providers';
+import { TransactionResponse, Web3Provider } from '@ethersproject/providers';
 import { toWei, soliditySha3 } from 'web3-utils';
 import axios from 'axios';
 
@@ -45,7 +45,7 @@ type ClaimStatus = boolean;
 
 export async function getClaimStatus(
   network: NetworkId,
-  provider: JsonRpcProvider,
+  provider: Web3Provider,
   ids: number,
   account: string
 ): Promise<ClaimStatus[]> {
@@ -67,7 +67,7 @@ export async function getReports(snapshot: Snapshot, weeks: number[]) {
 
 export async function getPendingClaims(
   network: NetworkId,
-  provider: JsonRpcProvider,
+  provider: Web3Provider,
   account: string
 ): Promise<{ claims: Claim[]; reports: Report }> {
   if (!constants[network]) {
@@ -171,7 +171,7 @@ export async function getCurrentRewardsEstimate(
 
 export async function claimRewards(
   network: NetworkId,
-  provider: JsonRpcProvider,
+  provider: Web3Provider,
   account: string,
   pendingClaims: Claim[],
   reports: Report
