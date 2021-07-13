fec9cc0f3443fe2b128510b7624b903b33ffd4a8
Evgeny Boxer <evgeny.boxer@gmail.com>
2021-07-13 11:15:15 +1000

    refactor(useNotifications): move functions out of the composable

--- src/composables/useNotifications.ts
+++ src/composables/useNotifications.ts
@@ -28,84 +28,82 @@ type NotificationsMap = Record<string, Notification>;
 
 const notificationsState = ref<NotificationsMap>({});
 
+// METHODS
+function addNotification(newNotification: NewNotification) {
+  const notificationsMap = getNotifications();
+  const notificationId = uniqueId('notification');
+
+  attemptToRemoveExistingNotification(newNotification);
+
+  notificationsMap[notificationId] = {
+    ...newNotification,
+    id: notificationId,
+    type: newNotification.type ?? 'info',
+    addedTime: Date.now(),
+    autoCloseAfterMs:
+      newNotification.autoCloseAfterMs ?? DEFAULT_NOTIFICATION_TIMEOUT
+  };
+
+  setNotifications(notificationsMap);
+
+  return notificationId;
+}
+
+function attemptToRemoveExistingNotification(newNotification: NewNotification) {
+  if (newNotification.transactionMetadata?.id) {
+    const previousNotificationFound = notifications.value.find(
+      previousNotification =>
+        previousNotification.transactionMetadata?.id ===
+        newNotification.transactionMetadata?.id
+    );
+    if (previousNotificationFound != null) {
+      removeNotification(previousNotificationFound.id);
+    }
+  }
+}
+
+function getNotification(id: string) {
+  const notificationsMap = getNotifications();
+
+  return notificationsMap[id];
+}
+
+function clearAllNotifications() {
+  setNotifications({});
+}
+
+function updateNotification(id: string, updates: Partial<Notification>) {
+  const notificationsMap = getNotifications();
+  const notification = getNotification(id);
+
+  if (notification != null) {
+    notificationsMap[id] = merge(notification, updates);
+
+    setNotifications(notificationsMap);
+  }
+}
+
+function removeNotification(id: string) {
+  const notificationsMap = getNotifications();
+
+  delete notificationsMap[id];
+
+  setNotifications(notificationsMap);
+}
+
+function getNotifications() {
+  return notificationsState.value;
+}
+
+function setNotifications(notifications: NotificationsMap) {
+  notificationsState.value = notifications;
+}
+
+const notifications = computed(() =>
+  orderBy(Object.values(getNotifications()), 'addedTime', 'desc')
+);
+
 export default function useNotifications() {
-  // METHODS
-  function addNotification(newNotification: NewNotification) {
-    const notificationsMap = getNotifications();
-    const notificationId = uniqueId('notification');
-
-    attemptToRemoveExistingNotification(newNotification);
-
-    notificationsMap[notificationId] = {
-      ...newNotification,
-      id: notificationId,
-      type: newNotification.type ?? 'info',
-      addedTime: Date.now(),
-      autoCloseAfterMs:
-        newNotification.autoCloseAfterMs ?? DEFAULT_NOTIFICATION_TIMEOUT
-    };
-
-    setNotifications(notificationsMap);
-
-    return notificationId;
-  }
-
-  function attemptToRemoveExistingNotification(
-    newNotification: NewNotification
-  ) {
-    if (newNotification.transactionMetadata?.id) {
-      const previousNotificationFound = notifications.value.find(
-        previousNotification =>
-          previousNotification.transactionMetadata?.id ===
-          newNotification.transactionMetadata?.id
-      );
-      if (previousNotificationFound != null) {
-        removeNotification(previousNotificationFound.id);
-      }
-    }
-  }
-
-  function getNotification(id: string) {
-    const notificationsMap = getNotifications();
-
-    return notificationsMap[id];
-  }
-
-  function clearAllNotifications() {
-    setNotifications({});
-  }
-
-  function updateNotification(id: string, updates: Partial<Notification>) {
-    const notificationsMap = getNotifications();
-    const notification = getNotification(id);
-
-    if (notification != null) {
-      notificationsMap[id] = merge(notification, updates);
-
-      setNotifications(notificationsMap);
-    }
-  }
-
-  function removeNotification(id: string) {
-    const notificationsMap = getNotifications();
-
-    delete notificationsMap[id];
-
-    setNotifications(notificationsMap);
-  }
-
-  function getNotifications() {
-    return notificationsState.value;
-  }
-
-  function setNotifications(notifications: NotificationsMap) {
-    notificationsState.value = notifications;
-  }
-
-  const notifications = computed(() =>
-    orderBy(Object.values(getNotifications()), 'addedTime', 'desc')
-  );
-
   return {
     // methods
     addNotification,
