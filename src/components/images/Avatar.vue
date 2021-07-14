03133e55adab55eb2d3f91dd3c8550005abe5126
Gareth Fuller <gareth.a.f@gmail.com>
2021-07-14 16:06:28 +0100

    Refactor profile fetching

--- src/components/images/Avatar.vue
+++ src/components/images/Avatar.vue
@@ -1,18 +1,6 @@
 <template>
   <span>
-    <img
-      v-if="profile?.image"
-      :src="_ipfsUrl(profile.image)"
-      :style="{
-        width: `${parseInt(size) || 22}px`,
-        height: `${parseInt(size) || 22}px`,
-        verticalAlign: 'middle'
-      }"
-      @error="error = true"
-      class="rounded-full leading-none"
-    />
     <jazzicon
-      v-else
       :address="address"
       :diameter="parseInt(size) || 22"
       class="leading-none inline-block align-middle"
