d940d05c4c74991148eb5ab4ced41b6dc5022f60
Ansaf Ahmed <me@ansaf.net>
2021-07-14 16:50:28 +1000

    fix: useBlocknative using old web3 composable still

--- src/composables/useBlocknative.ts
+++ src/composables/useBlocknative.ts
@@ -1,18 +1,18 @@
 import { computed, inject } from 'vue';
 import Notify from 'bnc-notify';
 import { bnNotifySymbol } from '@/plugins/blocknative';
-import useWeb3 from './useWeb3';
+import useWeb3 from '@/services/web3/useWeb3';
 
 const SUPPORTED_NETWORKS = [1, 42];
 
 export default function useBlocknative() {
-  const { appNetwork } = useWeb3();
+  const { appNetworkConfig } = useWeb3();
 
   const notify = inject(bnNotifySymbol) as ReturnType<typeof Notify>;
   if (!notify) throw new Error('Blocknative Notify missing!');
 
   const supportsBlocknative = computed(() => {
-    return SUPPORTED_NETWORKS.includes(appNetwork.id);
+    return SUPPORTED_NETWORKS.includes(appNetworkConfig.chainId);
   });
 
   function updateNotifyConfig(opts): void {
