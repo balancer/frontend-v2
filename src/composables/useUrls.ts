d4c368749266626ef69983c5bb518a296c308fb8
Gareth Fuller <gareth.a.f@gmail.com>
2021-08-03 10:48:10 +0100

    Move function outside composable

--- src/composables/useUrls.ts
+++ src/composables/useUrls.ts
@@ -1,12 +1,12 @@
 import { configService } from '@/services/config/config.service';
 
-export default function useUrls() {
-  function resolve(uri: string): string {
-    if (!uri) return '';
-    return uri
-      .replace('ipfs://', `https://${configService.env.IPFS_NODE}/ipfs/`)
-      .replace('ipns://', `https://${configService.env.IPFS_NODE}/ipns/`);
-  }
+function resolve(uri: string): string {
+  if (!uri) return '';
+  return uri
+    .replace('ipfs://', `https://${configService.env.IPFS_NODE}/ipfs/`)
+    .replace('ipns://', `https://${configService.env.IPFS_NODE}/ipns/`);
+}
 
+export default function useUrls() {
   return { resolve };
 }
