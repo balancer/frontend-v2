c07cd20780eb4977cd730ac1e0aaad5546b7a536
Evgeny Boxer <evgeny.boxer@gmail.com>
2021-08-05 21:33:55 +1000

    Add support for transaction speed ups / cancellations (#631)
    * feat: support speed ups and cancellations
    
    * add comment

--- src/composables/useTransactions.ts
+++ src/composables/useTransactions.ts
@@ -21,7 +21,8 @@ import useNumbers from './useNumbers';
 import { GnosisTransactionDetails } from './trade/useGnosis';
 
 const WEEK_MS = 86_400_000 * 7;
-const TRANSACTIONS_SCHEMA_VERSION = '1.0';
+// Please update the schema version when making changes to the transaction structure.
+const TRANSACTIONS_SCHEMA_VERSION = '1.1';
 
 export type TransactionStatus =
   | 'pending'
@@ -53,8 +54,12 @@ export type TxReceipt = Pick<
 
 export type OrderReceipt = OrderMetaData;
 
+export type ReplacementReason = 'txSpeedUp' | 'txCancel';
+
 export type Transaction = {
   id: string;
+  originalId?: string;
+  replacementReason?: ReplacementReason;
   action: TransactionAction;
   type: TransactionType;
   receipt?: OrderReceipt | TxReceipt;
@@ -150,7 +155,7 @@ function getTransaction(id: string, type: TransactionType) {
   const transactionsMap = getTransactions();
   const txId = getId(id, type);
 
-  return transactionsMap[txId];
+  return transactionsMap[txId] ?? null;
 }
 
 function updateTransaction(
@@ -163,7 +168,17 @@ function updateTransaction(
   const transaction = transactionsMap[txId];
 
   if (transaction != null) {
-    transactionsMap[txId] = merge(transaction, updates);
+    // id change requires a replacement of the transaction
+    if (updates.id != null) {
+      const newTxId = getId(updates.id, type);
+
+      transactionsMap[newTxId] = merge({}, transaction, updates, {
+        originalId: id
+      });
+      delete transactionsMap[txId];
+    } else {
+      transactionsMap[txId] = merge({}, transaction, updates);
+    }
 
     setTransactions(transactionsMap);
 
@@ -174,7 +189,10 @@ function updateTransaction(
 }
 
 function isSuccessfulTransaction(transaction: Transaction) {
-  if (transaction.status === 'confirmed') {
+  if (
+    transaction.status === 'confirmed' &&
+    transaction.replacementReason !== 'txCancel'
+  ) {
     if (transaction.type === 'order') {
       return (transaction.receipt as OrderReceipt)?.status === 'fulfilled';
     } else {
@@ -287,21 +305,23 @@ export default function useTransactions() {
     if (receipt != null) {
       const transaction = getTransaction(id, type);
 
-      const updateSuccessful = updateTransaction(id, type, {
-        receipt:
-          type === 'tx'
-            ? normalizeTxReceipt(receipt as TransactionReceipt)
-            : receipt,
-        summary:
-          type === 'order'
-            ? getSettledOrderSummary(transaction, receipt as OrderReceipt)
-            : transaction.summary,
-        status: 'confirmed',
-        confirmedTime: Date.now()
-      });
-      if (updateSuccessful) {
-        addNotificationForTransaction(id, type);
-        return true;
+      if (transaction != null) {
+        const updateSuccessful = updateTransaction(id, type, {
+          receipt:
+            type === 'tx'
+              ? normalizeTxReceipt(receipt as TransactionReceipt)
+              : receipt,
+          summary:
+            type === 'order'
+              ? getSettledOrderSummary(transaction, receipt as OrderReceipt)
+              : transaction.summary,
+          status: 'confirmed',
+          confirmedTime: Date.now()
+        });
+        if (updateSuccessful) {
+          addNotificationForTransaction(id, type);
+          return true;
+        }
       }
     }
 
@@ -311,18 +331,25 @@ export default function useTransactions() {
   function addNotificationForTransaction(id: string, type: TransactionType) {
     const transaction = getTransaction(id, type);
 
-    addNotification({
-      title: `${t(`transactionAction.${transaction.action}`)} ${t(
-        `transactionStatus.${transaction.status}`
-      )}`,
-      message: transaction.summary,
-      transactionMetadata: {
-        id: transaction.id,
-        status: transaction.status,
-        isSuccess: isSuccessfulTransaction(transaction),
-        explorerLink: getExplorerLink(transaction.id, transaction.type)
-      }
-    });
+    if (transaction != null) {
+      const transactionStatus: TransactionStatus =
+        transaction.replacementReason === 'txCancel'
+          ? 'cancelled'
+          : transaction.status;
+
+      addNotification({
+        title: `${t(`transactionAction.${transaction.action}`)} ${t(
+          `transactionStatus.${transactionStatus}`
+        )}`,
+        message: transaction.summary,
+        transactionMetadata: {
+          id: transaction.id,
+          status: transaction.status,
+          isSuccess: isSuccessfulTransaction(transaction),
+          explorerLink: getExplorerLink(transaction.id, transaction.type)
+        }
+      });
+    }
   }
 
   function checkOrderActivity(transaction: Transaction) {
@@ -397,6 +424,7 @@ export default function useTransactions() {
     finalizeTransaction,
     getExplorerLink,
     isSuccessfulTransaction,
+    updateTransaction,
 
     // computed
     pendingTransactions,
--- src/composables/useWeb3Watchers.ts
+++ src/composables/useWeb3Watchers.ts
@@ -1,11 +1,12 @@
 import useWeb3 from '@/services/web3/useWeb3';
+import { EthereumTransactionData } from 'bnc-sdk/dist/types/src/interfaces';
 import { watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useStore } from 'vuex';
 
 import useBlocknative from './useBlocknative';
 import useTokens from './useTokens';
-import useTransactions from './useTransactions';
+import useTransactions, { ReplacementReason } from './useTransactions';
 
 export default function useWeb3Watchers() {
   // COMPOSABLES
@@ -21,7 +22,22 @@ export default function useWeb3Watchers() {
     blockNumber
   } = useWeb3();
   const { refetchBalances, refetchAllowances } = useTokens();
-  const { handlePendingTransactions } = useTransactions();
+  const { handlePendingTransactions, updateTransaction } = useTransactions();
+
+  function handleTransactionReplacement(
+    tx: EthereumTransactionData,
+    replacementReason: ReplacementReason
+  ) {
+    const originalHash = tx.replaceHash;
+
+    if (originalHash != null) {
+      updateTransaction(originalHash, 'tx', {
+        // new id
+        id: tx.hash,
+        replacementReason
+      });
+    }
+  }
 
   // Watch for user account change:
   // -> Unsubscribe Blocknative from old account if exits
@@ -38,6 +54,20 @@ export default function useWeb3Watchers() {
           refetchBalances.value();
           refetchAllowances.value();
         });
+
+        emitter.on('txSpeedUp', tx =>
+          handleTransactionReplacement(
+            tx as EthereumTransactionData,
+            'txSpeedUp'
+          )
+        );
+
+        emitter.on('txCancel', tx =>
+          handleTransactionReplacement(
+            tx as EthereumTransactionData,
+            'txCancel'
+          )
+        );
       }
     }
   );
