834ab66d3a1bfae615d16d94b5da792de05d97af
Gareth Fuller <gareth.a.f@gmail.com>
2021-08-06 16:04:50 +0100

    Add ipfs service test

--- src/tests/unit/services/ipfs/ipfs.service.spec.ts
+++ src/tests/unit/services/ipfs/ipfs.service.spec.ts
@@ -3,11 +3,19 @@ import nock from 'nock';
 
 describe('IPFS service', () => {
   describe('Get data given IPFS hash', () => {
-    it('Constructs correct ipfs url', async () => {
+    it('Returns data via IPFS protocol', async () => {
       nock('https://ipfs.fleek.co')
         .get('/ipfs/xyz')
         .reply(200, 'some data');
       const data = await ipfsService.get('xyz');
+      expect(data).toBe('some data');
+    });
+
+    it('Returns data via IPNS protocol', async () => {
+      nock('https://ipfs.fleek.co')
+        .get('/ipns/xyz')
+        .reply(200, 'some data');
+      const data = await ipfsService.get('xyz', 'ipns');
       expect(data).toBe('some data');
     });
   });
