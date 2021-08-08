71b9f6faaeed7392b82e4da2b2bf60fe46b62e09
Gareth Fuller <gareth.a.f@gmail.com>
2021-08-08 19:53:37 +0100

    Correctly test rpc provider class dependencies

--- src/tests/unit/services/rpc-provider/rpc-provider.service.spec.ts
+++ src/tests/unit/services/rpc-provider/rpc-provider.service.spec.ts
@@ -6,8 +6,6 @@ import {
 } from '@ethersproject/providers';
 import { mocked } from 'ts-jest/utils';
 
-let rpcProviderService;
-
 jest.mock('@ethersproject/providers', () => {
   return {
     JsonRpcProvider: jest.fn().mockImplementation(() => {
@@ -27,58 +25,29 @@ describe('RPC provider service', () => {
   const MockedWebSocketProvider = mocked(WebSocketProvider, true);
   const MockedAlchemyProvider = mocked(AlchemyProvider, true);
 
-  beforeAll(() => {
-    rpcProviderService = new RpcProviderService();
-  });
-
   beforeEach(() => {
     MockedJsonRpcProvider.mockClear();
     MockedWebSocketProvider.mockClear();
     MockedAlchemyProvider.mockClear();
   });
 
-  describe('JSON providers', () => {
-    describe('Get default app provider', () => {
-      it('Default provider instantiated', () => {
-        expect(MockedJsonRpcProvider).toHaveBeenCalledTimes(1);
-      });
-    });
-
-    // describe('Get JSON provider for network key', () => {
-    //   it('Returns instance of JsonRpcProvider for valid network', () => {
-    //     const provider = rpcProviderService.getJsonProvider('1');
-    //     expect(provider).toBeInstanceOf(JsonRpcProvider);
-    //   });
-
-    //   it('Throws error if network not supported', () => {
-    //     expect(() => {
-    //       rpcProviderService.getJsonProvider('99');
-    //     }).toThrow('No config for network key: 99');
-    //   });
-    // });
+  it('Instantiates the provider service', () => {
+    const rpcProviderService = new RpcProviderService();
+    expect(rpcProviderService).toBeTruthy();
   });
 
-  // describe('Websocket providers', () => {
-  //   describe('Get default app ws provider', () => {
-  //     it('Returns mainnet provider by default', () => {
-  //       expect(rpcProviderService.wsProvider).toBeInstanceOf(WebSocketProvider);
-  //       expect(rpcProviderService.wsProvider.connection.url).toContain(
-  //         'balancer.fi'
-  //       );
-  //     });
-  //   });
-  // });
+  it('Calls the JsonProvider constructor', () => {
+    new RpcProviderService();
+    expect(JsonRpcProvider).toHaveBeenCalledTimes(1);
+  });
 
-  // describe('Alchemy providers', () => {
-  //   describe('Get default app Alchemy provider', () => {
-  //     it('Returns mainnet provider by default', () => {
-  //       expect(rpcProviderService.alchemyProvider).toBeInstanceOf(
-  //         AlchemyProvider
-  //       );
-  //       expect(rpcProviderService.alchemyProvider.connection.url).toContain(
-  //         'mainnet'
-  //       );
-  //     });
-  //   });
-  // });
+  it('Calls the WebSocketProvider constructor', () => {
+    new RpcProviderService();
+    expect(WebSocketProvider).toHaveBeenCalledTimes(1);
+  });
+
+  it('Calls the AlchemyProvider constructor', () => {
+    new RpcProviderService();
+    expect(AlchemyProvider).toHaveBeenCalledTimes(1);
+  });
 });
