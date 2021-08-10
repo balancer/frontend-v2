5edbc8c526c088ac300a98bcae9dffc89cb2e77b
Evgeny Boxer <evgeny.boxer@gmail.com>
2021-08-10 20:52:43 +1000

    persist trade interface + use local-storage.keys

--- src/constants/local-storage.keys.ts
+++ src/constants/local-storage.keys.ts
@@ -1,6 +1,11 @@
 export default {
   App: {
-    DarkMode: 'app.darkMode'
+    DarkMode: 'app.darkMode',
+    Locale: 'app.locale',
+    TradeSlippage: 'app.tradeSlippage',
+    TradeLiquidity: 'app.tradeLiquidity',
+    TradeInterface: 'app.tradeInterface',
+    TradeDeadline: 'app.tradeDeadline'
   },
   UserSettings: {
     Currency: 'userSettings.currency'
