4d2907f15dbdf339cc592d7a29028dd2cd708af5
Tom French <15848336+TomAFrench@users.noreply.github.com>
2021-08-17 01:58:18 +0100

    feat: switch to using Gnosis's GPv2VaultRelayer for gasless trading (#658)

--- src/services/gnosis/constants.ts
+++ src/services/gnosis/constants.ts
@@ -2,7 +2,7 @@ import { IS_DEV } from '@/constants/env';
 import { APP_NETWORK_ID, Network } from '@/constants/network';
 import {
   GPv2Settlement,
-  GPv2AllowanceManager
+  GPv2VaultRelayer
 } from '@gnosis.pm/gp-v2-contracts/networks.json';
 
 export const OPERATOR_URLS = {
@@ -21,6 +21,6 @@ export const GP_SETTLEMENT_CONTRACT_ADDRESS: string = (
   GPv2Settlement[APP_NETWORK_ID] ?? GPv2Settlement[Network.MAINNET]
 ).address;
 
-export const GP_ALLOWANCE_MANAGER_CONTRACT_ADDRESS: string = (
-  GPv2AllowanceManager[APP_NETWORK_ID] ?? GPv2AllowanceManager[Network.MAINNET]
+export const GP_RELAYER_CONTRACT_ADDRESS: string = (
+  GPv2VaultRelayer[APP_NETWORK_ID] ?? GPv2VaultRelayer[Network.MAINNET]
 ).address;
