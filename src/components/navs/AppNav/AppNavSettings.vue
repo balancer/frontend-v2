0fe86780576b17a17b763a60f62a60774e8b9308
Evgeny Boxer <evgeny.boxer@gmail.com>
2021-08-19 16:09:25 +1000

    hide trade interface on Polygon

--- src/components/navs/AppNav/AppNavSettings.vue
+++ src/components/navs/AppNav/AppNavSettings.vue
@@ -139,7 +139,7 @@
         @update:modelValue="setEthereumTxType"
       />
     </div>
-    <div class="px-4 mt-6">
+    <div v-if="!isPolygon" class="px-4 mt-6">
       <div class="flex items-baseline">
         <span v-text="$t('tradeInterface')" class="font-medium mb-2" />
         <BalTooltip>
