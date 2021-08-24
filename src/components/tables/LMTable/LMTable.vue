beba8d67b616dab5cce5d8a3f1598df9a2ad628d
1saf <me@ansaf.net>
2021-08-25 03:22:05 +1000

    update: tick version

--- src/components/tables/LMTable/LMTable.vue
+++ src/components/tables/LMTable/LMTable.vue
@@ -31,7 +31,9 @@
       >
         <div class="text-right flex flex-col">
           <span>{{ getWeekName(week.week) }}</span>
-          <span class="text-xs">Starts {{ getWeekStart(weeks.length - i - 1) }}</span>
+          <span class="text-xs"
+            >Starts {{ getWeekStart(weeks.length - i - 1) }}</span
+          >
         </div>
       </template>
       <template v-slot:iconColumnCell="pool">
@@ -222,7 +224,7 @@ export default defineComponent({
     }
 
     function getWeekStart(howManyWeeksToSubtract: number) {
-      console.log('lmao', howManyWeeksToSubtract)
+      console.log('lmao', howManyWeeksToSubtract);
       return format(
         startOfWeek(subWeeks(new Date(), howManyWeeksToSubtract)),
         'dd/MM/yyyy'
