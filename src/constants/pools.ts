3de2bb52b1da6c26015ea764630c7a892863650b
Jeffrey Bennett <endymionjkb@gmail.com>
2021-09-23 00:36:12 -0400

    lint

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -63,7 +63,7 @@ export const POOLS = {
   Investment: {
     AllowList: [
       '0x4fd63966879300cafafbb35d157dc5229278ed23000100000000000000000169', // kovan
-      '0xe7b1d394f3b40abeaa0b64a545dbcf89da1ecb3f00010000000000000000009a'  // mainnet
+      '0xe7b1d394f3b40abeaa0b64a545dbcf89da1ecb3f00010000000000000000009a' // mainnet
     ]
   },
   Factories: {
