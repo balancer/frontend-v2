2aa9dcaca04b67bbbcb7ff678dee759c703502fe
Gareth Fuller <gareth.a.f@gmail.com>
2021-09-25 19:30:30 +0100

    Catch meta stable pools in encode fn selection

--- src/services/pool/exchange/serializers/JoinParams.ts
+++ src/services/pool/exchange/serializers/JoinParams.ts
@@ -3,11 +3,11 @@ import { encodeJoinStablePool } from '@/lib/utils/balancer/stablePoolEncoding';
 import { encodeJoinWeightedPool } from '@/lib/utils/balancer/weightedPoolEncoding';
 import { parseUnits } from '@ethersproject/units';
 import { BigNumberish } from '@ethersproject/bignumber';
-import { isInvestment, isStable } from '@/composables/usePool';
+import { isInvestment, isStableLike } from '@/composables/usePool';
 
 export default class JoinParams {
   private exchange: PoolExchange;
-  private isStablePool: boolean;
+  private isStableLikePool: boolean;
   private isInvestmentPool: boolean;
   private isSwapEnabled: boolean;
   private dataEncodeFn: (data: any) => string;
@@ -15,12 +15,12 @@ export default class JoinParams {
 
   constructor(exchange: PoolExchange) {
     this.exchange = exchange;
-    this.isStablePool = isStable(exchange.pool.poolType);
+    this.isStableLikePool = isStableLike(exchange.pool.poolType);
     this.isInvestmentPool = isInvestment(exchange.pool.poolType);
     this.isSwapEnabled = !!(
       this.isInvestmentPool && exchange.pool.onchain.swapEnabled
     );
-    this.dataEncodeFn = this.isStablePool
+    this.dataEncodeFn = this.isStableLikePool
       ? encodeJoinStablePool
       : encodeJoinWeightedPool;
   }
