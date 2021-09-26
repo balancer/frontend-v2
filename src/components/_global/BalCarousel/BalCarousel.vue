290cf0ac1e24c232065bcde06c36c3fd540685ad
1saf <me@ansaf.net>
2021-09-26 22:26:10 +1000

    feature: add balcarousel

---
+++ src/components/_global/BalCarousel/BalCarousel.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { useSlots, h, renderSlot, onMounted } from 'vue';
+import Swiper from 'swiper';
+import SwiperCore, { Pagination, Navigation } from 'swiper/core';
+import 'swiper/swiper-bundle.css';
+
+// modules styles
+import 'swiper/components/pagination/pagination.min.css';
+SwiperCore.use([Pagination]);
+
+onMounted(() => {
+  const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: -5,
+
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+      renderBullet: (index, className) => {
+        return `<span class="${className} bal-bullet"></span>`;
+      }
+    }
+  });
+});
+</script>
+
+<template>
+  <div class="p-4">
+    <div class="swiper-container">
+      <div class="swiper-wrapper h-full">
+        <div
+          class="swiper-slide h-full pr-4"
+          v-for="(child, i) in $slots.default()"
+          :key="`slide-${i}`"
+        >
+          <component :is="child" />
+        </div>
+      </div>
+    </div>
+    <div class="swiper-pagination" />
+  </div>
+</template>
+
+<style scoped>
+.swiper-container {
+  @apply w-full;
+  height: 250px;
+}
+.swiper-pagination {
+  @apply relative;
+  @apply mt-3;
+}
+</style>
+<style>
+.bal-bullet {
+  @apply bg-gradient-to-tr;
+  @apply from-yellow-400;
+  @apply to-pink-500;
+  @apply mr-3;
+  width: 10px;
+  height: 10px;
+}
+</style>
