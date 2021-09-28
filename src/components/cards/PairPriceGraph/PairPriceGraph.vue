97838b24e8e7a22a75c8aba1107aa604972c3b6a
1saf <me@ansaf.net>
2021-09-28 18:42:33 +1000

    update: convert pairpricegraph widget to script setup

--- src/components/cards/PairPriceGraph/PairPriceGraph.vue
+++ src/components/cards/PairPriceGraph/PairPriceGraph.vue
@@ -1,3 +1,228 @@
+<script setup lang="ts">
+import { computed, defineComponent, reactive, ref } from 'vue';
+import anime from 'animejs';
+import { useStore } from 'vuex';
+import useTokens from '@/composables/useTokens';
+import { coingeckoService } from '@/services/coingecko/coingecko.service';
+import { useQuery } from 'vue-query';
+import {
+  Dictionary,
+  mapKeys,
+  mapValues,
+  maxBy,
+  minBy,
+  pickBy,
+  toPairs
+} from 'lodash';
+import { fromUnixTime, format } from 'date-fns';
+import useTailwind from '@/composables/useTailwind';
+import useBreakpoints from '@/composables/useBreakpoints';
+import { useTradeState } from '@/composables/trade/useTradeState';
+import { getAddress } from '@ethersproject/address';
+const easing = 'spring(1, 150, 18, 0)';
+
+async function getPairPriceData(
+  inputAsset: string,
+  outputAsset: string,
+  days: number
+) {
+  const aggregateBy = days === 1 ? 'hour' : 'day';
+  const inputAssetData = await coingeckoService.prices.getTokensHistorical(
+    [inputAsset],
+    days,
+    1,
+    aggregateBy
+  );
+
+  const outputAssetData = await coingeckoService.prices.getTokensHistorical(
+    [outputAsset],
+    days,
+    1,
+    aggregateBy
+  );
+
+  const calculatedPricing = mapValues(inputAssetData, (value, timestamp) => {
+    if (!outputAssetData[timestamp]) return null;
+    return (1 / value[0]) * outputAssetData[timestamp][0];
+  });
+
+  const calculatedPricingNoNulls = pickBy(calculatedPricing) as Dictionary<
+    number
+  >;
+
+  const formatTimestamps = mapKeys(
+    calculatedPricingNoNulls,
+    (_, timestamp: any) =>
+      format(fromUnixTime(timestamp / 1000), 'yyyy/MM/dd HH:mm')
+  );
+  return toPairs(formatTimestamps);
+}
+
+const chartTimespans = [
+  {
+    option: '1d',
+    value: 1
+  },
+  {
+    option: '1w',
+    value: 7
+  },
+  {
+    option: '1m',
+    value: 30
+  },
+  {
+    option: '1y',
+    value: 365
+  },
+  {
+    option: 'All',
+    value: -1
+  }
+];
+
+const { upToLargeBreakpoint } = useBreakpoints();
+const store = useStore();
+const { tokens } = useTokens();
+const { tokenInAddress, tokenOutAddress } = useTradeState();
+const tailwind = useTailwind();
+
+const animateInstance = ref();
+const elementToAnimate = ref<HTMLElement>();
+const chartHeight = ref(upToLargeBreakpoint ? 75 : 100);
+const activeTimespan = ref(chartTimespans[0]);
+const isExpanded = ref(false);
+const resizeTick = ref(0);
+
+const appLoading = computed(() => store.state.app.loading);
+
+const inputSym = computed(
+  () => tokens.value[getAddress(tokenInAddress.value)]?.symbol
+);
+const outputSym = computed(
+  () => tokens.value[getAddress(tokenOutAddress.value)]?.symbol
+);
+
+const shouldLoadPriceData = computed(
+  () => tokenInAddress.value !== '' && tokenOutAddress.value !== ''
+);
+
+const dataMin = computed(() => {
+  return (minBy(priceData.value || [], v => v[1]) || [])[1] || 0;
+});
+
+const dataMax = computed(() => {
+  return (maxBy(priceData.value || [], v => v[1]) || [])[1] || 0;
+});
+
+const {
+  isLoading: isLoadingPriceData,
+  data: priceData,
+  error: failedToLoadPriceData
+} = useQuery(
+  reactive([
+    'pairPriceData',
+    { tokenInAddress, tokenOutAddress, activeTimespan }
+  ]),
+  () =>
+    getPairPriceData(
+      tokenInAddress.value,
+      tokenOutAddress.value,
+      activeTimespan.value.value
+    ),
+  reactive({
+    retry: false,
+    shouldLoadPriceData,
+    // when refetch on window focus in enabled, it causes a flash
+    // in the loading state of the card which is jarring. disabling it
+    refetchOnWindowFocus: false
+  })
+);
+
+const maximise = () => {
+  if (elementToAnimate.value) {
+    anime.set(elementToAnimate.value, {
+      transformOrigin: 'center'
+    });
+  }
+  window.requestAnimationFrame(() => {
+    animateInstance.value = anime({
+      targets: elementToAnimate.value,
+      // delay needed to avoid major wonkiness
+      width: { value: '750px', delay: 20 },
+      // h-96
+      height: '384px',
+      // delay needed to avoid major wonkiness
+      translateX: { value: '-85%', delay: 10 },
+      easing,
+      update: () => {
+        resizeTick.value = resizeTick.value + 1;
+        chartHeight.value = (elementToAnimate.value?.offsetHeight || 0) * 0.6;
+      }
+    });
+    isExpanded.value = true;
+  });
+};
+
+const minimise = () => {
+  requestAnimationFrame(() => {
+    anime({
+      targets: elementToAnimate.value,
+      width: '250px',
+      height: '225px',
+      // delay needed to avoid major wonkiness
+      translateX: { value: '0%', delay: 135 },
+      easing,
+      update: () => {
+        resizeTick.value = resizeTick.value + 1;
+        chartHeight.value = upToLargeBreakpoint ? 75 : 100;
+      }
+    });
+    isExpanded.value = false;
+  });
+};
+
+const toggle = () => {
+  if (isExpanded.value) {
+    minimise();
+  } else {
+    maximise();
+  }
+};
+
+const chartData = computed(() => [
+  {
+    name: `${inputSym.value}/${outputSym.value}`,
+    values: priceData.value || []
+  }
+]);
+
+const chartColors = computed(() => {
+  let color = tailwind.theme.colors.green['400'];
+  const _priceData = priceData.value || [];
+  if (_priceData.length > 2) {
+    if (
+      _priceData[_priceData.length - 1][1] <
+      _priceData[_priceData.length - 2][1]
+    ) {
+      color = tailwind.theme.colors.red['400'];
+    }
+  }
+  return [color];
+});
+
+const chartGrid = computed(() => {
+  const bottom = isExpanded.value ? '7.5%' : '15%';
+  return {
+    left: '2.5%',
+    right: '0',
+    top: '10%',
+    bottom,
+    containLabel: false
+  };
+});
+</script>
+
 <template>
   <div ref="elementToAnimate" class="priceGraphCard">
     <BalLoadingBlock
@@ -84,255 +309,6 @@
     </BalCard>
   </div>
 </template>
-
-<script lang="ts">
-import { computed, defineComponent, reactive, ref } from 'vue';
-import anime from 'animejs';
-import { useStore } from 'vuex';
-import useTokens from '@/composables/useTokens';
-import { coingeckoService } from '@/services/coingecko/coingecko.service';
-import { useQuery } from 'vue-query';
-import {
-  Dictionary,
-  mapKeys,
-  mapValues,
-  maxBy,
-  minBy,
-  pickBy,
-  toPairs
-} from 'lodash';
-import { fromUnixTime, format } from 'date-fns';
-import useTailwind from '@/composables/useTailwind';
-import useBreakpoints from '@/composables/useBreakpoints';
-import { useTradeState } from '@/composables/trade/useTradeState';
-import { getAddress } from '@ethersproject/address';
-const easing = 'spring(1, 150, 18, 0)';
-
-async function getPairPriceData(
-  inputAsset: string,
-  outputAsset: string,
-  days: number
-) {
-  const aggregateBy = days === 1 ? 'hour' : 'day';
-  const inputAssetData = await coingeckoService.prices.getTokensHistorical(
-    [inputAsset],
-    days,
-    1,
-    aggregateBy
-  );
-
-  const outputAssetData = await coingeckoService.prices.getTokensHistorical(
-    [outputAsset],
-    days,
-    1,
-    aggregateBy
-  );
-
-  const calculatedPricing = mapValues(inputAssetData, (value, timestamp) => {
-    if (!outputAssetData[timestamp]) return null;
-    return (1 / value[0]) * outputAssetData[timestamp][0];
-  });
-
-  const calculatedPricingNoNulls = pickBy(calculatedPricing) as Dictionary<
-    number
-  >;
-
-  const formatTimestamps = mapKeys(
-    calculatedPricingNoNulls,
-    (_, timestamp: any) =>
-      format(fromUnixTime(timestamp / 1000), 'yyyy/MM/dd HH:mm')
-  );
-  return toPairs(formatTimestamps);
-}
-
-const chartTimespans = [
-  {
-    option: '1d',
-    value: 1
-  },
-  {
-    option: '1w',
-    value: 7
-  },
-  {
-    option: '1m',
-    value: 30
-  },
-  {
-    option: '1y',
-    value: 365
-  },
-  {
-    option: 'All',
-    value: -1
-  }
-];
-
-export default defineComponent({
-  setup() {
-    const elementToAnimate = ref<HTMLElement>();
-    const { upToLargeBreakpoint } = useBreakpoints();
-    const chartHeight = ref(upToLargeBreakpoint ? 75 : 100);
-    const activeTimespan = ref(chartTimespans[0]);
-    const isExpanded = ref(false);
-    const animateInstance = ref();
-    const store = useStore();
-    const { tokens } = useTokens();
-    const tailwind = useTailwind();
-    const resizeTick = ref(0);
-    const { tokenInAddress, tokenOutAddress } = useTradeState();
-    const appLoading = computed(() => store.state.app.loading);
-
-    const inputSym = computed(
-      () => tokens.value[getAddress(tokenInAddress.value)]?.symbol
-    );
-    const outputSym = computed(
-      () => tokens.value[getAddress(tokenOutAddress.value)]?.symbol
-    );
-
-    const shouldLoadPriceData = computed(
-      () => tokenInAddress.value !== '' && tokenOutAddress.value !== ''
-    );
-
-    const {
-      isLoading: isLoadingPriceData,
-      data: priceData,
-      error: failedToLoadPriceData
-    } = useQuery(
-      reactive([
-        'pairPriceData',
-        { tokenInAddress, tokenOutAddress, activeTimespan }
-      ]),
-      () =>
-        getPairPriceData(
-          tokenInAddress.value,
-          tokenOutAddress.value,
-          activeTimespan.value.value
-        ),
-      reactive({
-        retry: false,
-        shouldLoadPriceData,
-        // when refetch on window focus in enabled, it causes a flash
-        // in the loading state of the card which is jarring. disabling it
-        refetchOnWindowFocus: false
-      })
-    );
-
-    const dataMin = computed(() => {
-      return (minBy(priceData.value || [], v => v[1]) || [])[1] || 0;
-    });
-
-    const dataMax = computed(() => {
-      return (maxBy(priceData.value || [], v => v[1]) || [])[1] || 0;
-    });
-
-    const maximise = () => {
-      if (elementToAnimate.value) {
-        anime.set(elementToAnimate.value, {
-          transformOrigin: 'center'
-        });
-      }
-      window.requestAnimationFrame(() => {
-        animateInstance.value = anime({
-          targets: elementToAnimate.value,
-          // delay needed to avoid major wonkiness
-          width: { value: '750px', delay: 20 },
-          // h-96
-          height: '384px',
-          // delay needed to avoid major wonkiness
-          translateX: { value: '-85%', delay: 10 },
-          easing,
-          update: () => {
-            resizeTick.value = resizeTick.value + 1;
-            chartHeight.value =
-              (elementToAnimate.value?.offsetHeight || 0) * 0.6;
-          }
-        });
-        isExpanded.value = true;
-      });
-    };
-
-    const minimise = () => {
-      requestAnimationFrame(() => {
-        anime({
-          targets: elementToAnimate.value,
-          width: '250px',
-          height: '225px',
-          // delay needed to avoid major wonkiness
-          translateX: { value: '0%', delay: 135 },
-          easing,
-          update: () => {
-            resizeTick.value = resizeTick.value + 1;
-            chartHeight.value = upToLargeBreakpoint ? 75 : 100;
-          }
-        });
-        isExpanded.value = false;
-      });
-    };
-
-    const toggle = () => {
-      if (isExpanded.value) {
-        minimise();
-      } else {
-        maximise();
-      }
-    };
-
-    const chartData = computed(() => [
-      {
-        name: `${inputSym.value}/${outputSym.value}`,
-        values: priceData.value || []
-      }
-    ]);
-
-    const chartColors = computed(() => {
-      let color = tailwind.theme.colors.green['400'];
-      const _priceData = priceData.value || [];
-      if (_priceData.length > 2) {
-        if (
-          _priceData[_priceData.length - 1][1] <
-          _priceData[_priceData.length - 2][1]
-        ) {
-          color = tailwind.theme.colors.red['400'];
-        }
-      }
-      return [color];
-    });
-
-    const chartGrid = computed(() => {
-      const bottom = isExpanded.value ? '7.5%' : '15%';
-      return {
-        left: '2.5%',
-        right: '0',
-        top: '10%',
-        bottom,
-        containLabel: false
-      };
-    });
-
-    return {
-      elementToAnimate,
-      toggle,
-      inputSym,
-      outputSym,
-      isLoadingPriceData,
-      chartData,
-      chartColors,
-      appLoading,
-      resizeTick,
-      chartHeight,
-      chartGrid,
-      upToLargeBreakpoint,
-      failedToLoadPriceData,
-      chartTimespans,
-      activeTimespan,
-      isExpanded,
-      dataMin,
-      dataMax
-    };
-  }
-});
-</script>
 
 <style scoped>
 .maximise {
