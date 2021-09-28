c9599d14e84b1161079d6a73de32ce486ffdc5e5
1saf <me@ansaf.net>
2021-09-28 18:45:01 +1000

    update: convert mywallet widget to script setup

--- src/components/cards/MyWallet/MyWallet.vue
+++ src/components/cards/MyWallet/MyWallet.vue
@@ -1,3 +1,33 @@
+<script setup lang="ts">
+import { useTradeState } from '@/composables/trade/useTradeState';
+import useBreakpoints from '@/composables/useBreakpoints';
+import useTokens from '@/composables/useTokens';
+import useWeb3 from '@/services/web3/useWeb3';
+import { computed } from 'vue';
+
+const { appNetworkConfig, isWalletReady, toggleWalletSelectModal } = useWeb3();
+const { upToLargeBreakpoint } = useBreakpoints();
+const { setTokenInAddress } = useTradeState();
+const {
+  balanceFor,
+  balances,
+  dynamicDataLoading: isLoadingBalances
+} = useTokens();
+
+const etherBalance = computed(() => {
+  if (!isWalletReady.value) return '-';
+  return Number(balanceFor(appNetworkConfig.nativeAsset.address)).toFixed(4);
+});
+
+const tokensWithBalance = computed(() => {
+  return Object.keys(balances.value).filter(
+    tokenAddress =>
+      Number(balances.value[tokenAddress]) > 0 &&
+      tokenAddress !== appNetworkConfig.nativeAsset.address
+  );
+});
+</script>
+
 <template>
   <BalCard noPad :hFull="upToLargeBreakpoint" :shadow="false">
     <div class="flex flex-col bg-white w-full">
@@ -25,52 +55,3 @@
     </div>
   </BalCard>
 </template>
-
-<script lang="ts">
-import { useTradeState } from '@/composables/trade/useTradeState';
-import useBreakpoints from '@/composables/useBreakpoints';
-import useTokens from '@/composables/useTokens';
-import useWeb3 from '@/services/web3/useWeb3';
-import { computed, defineComponent } from 'vue';
-
-export default defineComponent({
-  setup() {
-    const {
-      balanceFor,
-      balances,
-      dynamicDataLoading: isLoadingBalances
-    } = useTokens();
-    const {
-      appNetworkConfig,
-      isWalletReady,
-      toggleWalletSelectModal
-    } = useWeb3();
-    const { upToLargeBreakpoint } = useBreakpoints();
-    const { setTokenInAddress } = useTradeState();
-    const etherBalance = computed(() => {
-      if (!isWalletReady.value) return '-';
-      return Number(balanceFor(appNetworkConfig.nativeAsset.address)).toFixed(
-        4
-      );
-    });
-
-    const tokensWithBalance = computed(() => {
-      return Object.keys(balances.value).filter(
-        tokenAddress =>
-          Number(balances.value[tokenAddress]) > 0 &&
-          tokenAddress !== appNetworkConfig.nativeAsset.address
-      );
-    });
-
-    return {
-      etherBalance,
-      tokensWithBalance,
-      upToLargeBreakpoint,
-      isWalletReady,
-      isLoadingBalances,
-      setTokenInAddress,
-      toggleWalletSelectModal
-    };
-  }
-});
-</script>
