7e1f1626a25fe973b5122f71f0733fffc5bb0d49
1saf <me@ansaf.net>
2021-09-29 16:29:30 +1000

    update: add explicit types

--- src/components/cards/TrendingPairs/TrendingPairs.vue
+++ src/components/cards/TrendingPairs/TrendingPairs.vue
@@ -1,3 +1,52 @@
+<script setup lang="ts">
+import { useTradeState } from '@/composables/trade/useTradeState';
+import { balancerSubgraphService } from '@/services/balancer/subgraph/balancer-subgraph.service';
+import { getAddress } from '@ethersproject/address';
+import { startOfDay } from 'date-fns';
+import { computed, reactive } from 'vue';
+import { useQuery } from 'vue-query';
+
+type TrendingPair = {
+  symbol: string;
+  address: string;
+}
+
+const { setTokenOutAddress, setTokenInAddress } = useTradeState();
+
+const getTrendingTradePairs = async () => {
+  return await balancerSubgraphService.tradePairSnapshots.get({
+    orderBy: 'swapVolume',
+    orderDirection: 'desc',
+    where: {
+      timestamp_gte: startOfDay(new Date()).getTime() / 1000
+    },
+    first: 5
+  });
+};
+
+const { data: tradePairSnapshots } = useQuery(
+  reactive(['trendingTradePairs']),
+  () => getTrendingTradePairs()
+);
+const trendingPairs = computed(() => {
+  return (tradePairSnapshots.value || []).map(pairSnapshot => [
+    {
+      symbol: pairSnapshot.pair.token0.symbol,
+      address: getAddress(pairSnapshot.pair.token0.address)
+    },
+    {
+      symbol: pairSnapshot.pair.token1.symbol,
+      address: getAddress(pairSnapshot.pair.token1.address)
+    }
+  ]);
+});
+
+const setTradePair = (pair: TrendingPair[]) => {
+  setTokenInAddress(pair[0].address);
+  setTokenOutAddress(pair[1].address);
+};
+</script>
+
 <template>
   <BalCard noPad hFull :shadow="false">
     <div class="flex flex-col bg-white widget-card">
@@ -17,55 +66,3 @@
     </div>
   </BalCard>
 </template>
-
-<script lang="ts">
-import { useTradeState } from '@/composables/trade/useTradeState';
-import { balancerSubgraphService } from '@/services/balancer/subgraph/balancer-subgraph.service';
-import { getAddress } from '@ethersproject/address';
-import { startOfDay } from 'date-fns';
-import { computed, defineComponent, reactive } from 'vue';
-import { useQuery } from 'vue-query';
-
-const getTrendingTradePairs = async () => {
-  return await balancerSubgraphService.tradePairSnapshots.get({
-    orderBy: 'swapVolume',
-    orderDirection: 'desc',
-    where: {
-      timestamp_gte: startOfDay(new Date()).getTime() / 1000
-    },
-    first: 5
-  });
-};
-
-export default defineComponent({
-  setup() {
-    const { setTokenOutAddress, setTokenInAddress } = useTradeState();
-
-    const { data: tradePairSnapshots } = useQuery(
-      reactive(['trendingTradePairs']),
-      () => getTrendingTradePairs()
-    );
-    const trendingPairs = computed(() => {
-      return (tradePairSnapshots.value || []).map(pairSnapshot => [
-        {
-          symbol: pairSnapshot.pair.token0.symbol,
-          address: getAddress(pairSnapshot.pair.token0.address)
-        },
-        {
-          symbol: pairSnapshot.pair.token1.symbol,
-          address: getAddress(pairSnapshot.pair.token1.address)
-        }
-      ]);
-    });
-
-    const setTradePair = pair => {
-      setTokenInAddress(pair[0].address);
-      setTokenOutAddress(pair[1].address);
-    };
-    return {
-      trendingPairs,
-      setTradePair
-    };
-  }
-});
-</script>
