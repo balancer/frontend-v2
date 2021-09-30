0b760752498c56803dddbf7f76e0b848fe4e1121
Gareth Fuller <gareth.a.f@gmail.com>
2021-09-30 10:39:16 +0100

    Fix prop usage

--- src/components/_global/BalCard/BalCard.vue
+++ src/components/_global/BalCard/BalCard.vue
@@ -55,7 +55,7 @@ export default defineComponent({
     const cardClasses = computed(() => {
       return {
         'rounded-lg': !props.square,
-        'overflow-hidden': props.exposeOverflow,
+        'overflow-hidden': !props.exposeOverflow,
         [`bg-white dark:bg-gray-${props.darkBgColor}`]: true,
         [`shadow${props.shadow ? '-' : ''}${props.shadow}`]: true,
         [borderClasses.value]: !props.noBorder,
