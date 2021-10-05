ea898008c34d368dbdee19a3c93c232d6aa85596
1saf <me@ansaf.net>
2021-10-05 19:14:08 +1100

    temp: test transform

--- src/components/_global/BalAccordion/BalAccordion.vue
+++ src/components/_global/BalAccordion/BalAccordion.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { ref, nextTick, onMounted } from 'vue';
+import { ref, nextTick, onMounted, watch } from 'vue';
 import anime from 'animejs';
 import AnimatePresence from '@/components/animate/AnimatePresence.vue';
+import { findIndex, takeRight } from 'lodash';
 
 type Section = {
   title: string;
@@ -17,8 +18,15 @@ const props = defineProps<Props>();
 const activeSection = ref('');
 const activeSectionElement = ref<HTMLElement>();
 const accordionHeightSetterElement = ref<HTMLElement>();
+const wrapperElement = ref<HTMLElement>();
+const handleBarElement = ref<HTMLElement>();
+const handleBarElements = ref<HTMLElement[]>([]);
+
+const minimisedWrapperHeight = ref(0);
 const isContentVisible = ref(false);
 const height = ref();
+const handleBarHeight = ref(0);
+const totalHeight = ref(0);
 
 async function toggleSection(section: string) {
   activeSection.value = section;
@@ -27,28 +35,75 @@ async function toggleSection(section: string) {
   if (activeSectionElement.value && accordionHeightSetterElement.value) {
     height.value = activeSectionElement.value.clientHeight;
     isContentVisible.value = false;
-    await nextTick();
-    anime.set(accordionHeightSetterElement.value, {
-      maxHeight: '0px',
-      opacity: 0
-    });
+  }
 
-    isContentVisible.value = true;
+
+  const activeSectionIndex = props.sections.findIndex(s => s.id === activeSection.value);
+  const handleBarsToTransform = takeRight(handleBarElements.value, handleBarElements.value.length - (activeSectionIndex + 1))
+
+  handleBarsToTransform.forEach((handleBar, i) => {
     anime({
-      targets: accordionHeightSetterElement.value,
-      maxHeight: `${height.value}px`,
-      easing: 'spring(.25, 100, 18, 0)',
-      opacity: 1
+      targets: handleBar,
+      translateY: `${height.value}px`
+    })
+  })
+
+  console.log('bars', handleBarsToTransform)
+}
+
+// all of this happens without the user seeing any feedback
+onMounted(async () => {
+  // set to true so we can actually measure the content height
+  isContentVisible.value = true;
+
+  // set the height of the minimised accordion
+  minimisedWrapperHeight.value = wrapperElement.value?.offsetHeight || 0;
+
+  handleBarHeight.value = handleBarElement.value?.offsetHeight || 0;
+
+  // the total expanded height starts with tracking the minimised height first
+  totalHeight.value = wrapperElement.value?.offsetHeight || 0;
+
+  // calculating the height of the completely expanded accordion
+  // by summing the heights of each section onto the minimised
+  // height of the accordion
+  for (const section of props.sections) {
+    activeSection.value = section.id;
+    await nextTick();
+    totalHeight.value =
+      totalHeight.value + (activeSectionElement.value?.offsetHeight || 0);
+  }
+
+  // need to set this back to false so its like the accordion
+  // was never active
+  activeSection.value = '';
+  isContentVisible.value = false;
+
+  if (wrapperElement.value) {
+    anime.set(wrapperElement.value, {
+      height: totalHeight.value
     });
+  }
+});
+
+function setHandleBars(el: HTMLElement) {
+  if (!handleBarElements.value?.includes(el)) {
+    handleBarElements.value.push(el);
   }
 }
 </script>
 
 <template>
   <div ref="wrapperElement">
-    <BalCard no-pad>
-      <div class="flex flex-col" v-for="section in sections" :key="section.id">
+    <BalCard hFull no-pad>
+      <div
+        class="flex flex-col"
+        v-for="section in sections"
+        :key="section.id"
+        :ref="setHandleBars"
+      >
         <button
+          ref="handleBarElement"
           @click="toggleSection(section.id)"
           class="w-full flex justify-between p-3"
         >
@@ -61,9 +116,10 @@ async function toggleSection(section: string) {
           v-if="activeSection === section.id"
         >
           <!-- content -->
-          <div ref="activeSectionElement">
+          <div ref="activeSectionElement" v-if="isContentVisible">
             <slot :name="section.id" />
           </div>
+
         </div>
       </div>
     </BalCard>
