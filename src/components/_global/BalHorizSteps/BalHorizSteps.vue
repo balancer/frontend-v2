435944edbb2ab37484ef741b75b5fe0e4aa9637a
Gareth Fuller <gareth.a.f@gmail.com>
2021-10-08 14:27:22 +0100

    Dark mode fixes

--- src/components/_global/BalHorizSteps/BalHorizSteps.vue
+++ src/components/_global/BalHorizSteps/BalHorizSteps.vue
@@ -58,15 +58,15 @@ const walletLogo = computed((): string => getConnectorLogo(connector.value.id));
 function stateClasses(state: StepState): string {
   switch (state) {
     case StepState.Success:
-      return 'border-green-500 text-green-500';
+      return 'border-green-500 dark:border-green-500 text-green-500';
     case StepState.Pending:
-      return 'border-none text-yellow-500';
+      return 'border-none dark:border-none text-yellow-500';
     case StepState.Active:
-      return 'border-purple-500 text-gradient';
+      return 'border-purple-500 dark:border-purple-500 text-gradient';
     case StepState.WalletOpen:
-      return 'border-purple-500 text-gradient';
+      return 'border-purple-500 dark:border-purple-500 text-gradient';
     default:
-      return '';
+      return 'dark:border-gray-700';
   }
 }
 </script>
@@ -74,7 +74,7 @@ function stateClasses(state: StepState): string {
 <template>
   <div class="flex items-center">
     <div v-for="(step, i) in steps" :key="i" class="flex items-center">
-      <div v-if="i !== 0" class="h-px bg-gray-100 w-16" />
+      <div v-if="i !== 0" class="h-px bg-gray-100 dark:bg-gray-700 w-16" />
       <BalTooltip :text="step.tooltip" width="44" textCenter>
         <template v-slot:activator>
           <div :class="['step', stateClasses(step.state)]">
--- src/components/forms/pool_actions/InvestForm/components/InvestPreviewModal/components/InvestSummary.vue
+++ src/components/forms/pool_actions/InvestForm/components/InvestPreviewModal/components/InvestSummary.vue
@@ -115,7 +115,9 @@ function weeklyYieldForAPR(apr: string): string {
         <div class="summary-table-number">
           {{ fNum(totalWeeklyYield, currency) }}
           <BalTooltip icon-size="sm" width="72" class="ml-2" noPad>
-            <div class="p-2 bg-gray-50 border-b">
+            <div
+              class="p-2 bg-gray-50 dark:bg-gray-700 rounded-t-lg border-b dark:border-gray-700"
+            >
               <span class="text-sm" v-text="$t('yieldEarnings')" />
               <span class="ml-1 text-gray-500">
                 ({{ $t('basedOnLast24h') }})
