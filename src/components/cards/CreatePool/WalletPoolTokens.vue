c4e80d543a5aa21e4f7eb37fc7e121fb9d7eabeb
1saf <me@ansaf.net>
2021-10-19 20:17:55 +1100

    update: pie graph with tokens weight distribution and colors

---
+++ src/components/cards/CreatePool/WalletPoolTokens.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import Vue from 'vue';
+</script>
+
+<template>
+  <BalCard noPad shadow="false">
+    <div class="p-2 px-3 border-b">
+      <h6>Max initial liquidity in wallet</h6>
+    </div>
+    <div class="p-2">
+      <div class="grid grid-cols-12 w-full">
+        <div class="col-span-5">
+          <span class="text-sm font-semibold">Token</span>
+        </div>
+        <div class="col-span-4 text-right">
+          <span class="text-sm font-semibold">USD Value</span>
+        </div>
+        <div class="col-span-3 text-right">
+          <span class="text-sm font-semibold">%</span>
+        </div>
+      </div>
+    </div>
+  </BalCard>
+</template>
