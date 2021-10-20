85aa581f3e212f56f9287147286b3c79bf18b67b
Gareth Fuller <gareth.a.f@gmail.com>
2021-10-20 19:45:58 +0100

    Build optimization (#953)
    * Update supported browsers
    
    * Update gitignore
    
    * Update vue-loader and add dev pkgs
    
    * Use babel plugin
    
    * Update webpack config + measue build time
    
    * Revert parallel true config
    
    * Remove speed analysis
    
    * Remove install husky on prepare
    
    * Cleanup vue config
    
    * fix: Re-introduce prepare command but prevent running husky install in builds
    
    * Try turning parallel option back on after updating vue-loader
    
    * fix: Looks like parallel = false is no longer required
    
    * Move tailwind deps to dev deps
    
    * Remove storybook references
    
    * npm install forced

---
+++ src/plugins/webpack.js
@@ -0,0 +1,26 @@
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+const { version } = require('../../package.json');
+
+const plugins = [];
+
+if (process.env.VUE_APP_SENTRY_AUTH_TOKEN) {
+  const release = `frontend-v2@${version}`;
+  const ENV = process.env.VUE_APP_ENV || 'development';
+
+  const sentryWebpack = new SentryWebpackPlugin({
+    // sentry-cli configuration
+    authToken: process.env.VUE_APP_SENTRY_AUTH_TOKEN,
+    org: 'balancer-labs',
+    project: 'app',
+    release: release,
+    // webpack specific configuration
+    include: './dist',
+    ignore: ['node_modules', 'webpack.config.js']
+  });
+
+  if (['production', 'staging'].includes(ENV)) {
+    plugins.push(sentryWebpack);
+  }
+}
+
+module.exports = plugins;
