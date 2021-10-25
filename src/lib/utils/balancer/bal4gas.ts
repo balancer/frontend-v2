fed6b85e67ed4cfe2bd2959ec24ef787f62b5671
Markus Buhatem Koch <markusbkoch@gmail.com>
2021-10-25 10:36:46 -0300

    add safety margin to isBudgetLeft()

--- src/lib/utils/balancer/bal4gas.ts
+++ src/lib/utils/balancer/bal4gas.ts
@@ -1,10 +1,13 @@
 const ENDPOINT_BUDGET_SPENT =
   'https://api.balancer.finance/liquidity-mining/v1/gas/';
-const BUDGET = 80000;
+// The program's budget is 80k BAL. To avoid someone seeing
+// the message but ultimately not getting the reimbursement
+// we add a safety margin and stop showing the message @ 79.5k
+const BUDGET_THRESHOLD = 79500;
 
 export async function isBudgetLeft(): Promise<boolean> {
   const response = await fetch(ENDPOINT_BUDGET_SPENT);
   const data = await response.json();
-  const budgetLeft = data.success ? data.result < BUDGET : false;
+  const budgetLeft = data.success ? data.result < BUDGET_THRESHOLD : false;
   return budgetLeft;
 }
