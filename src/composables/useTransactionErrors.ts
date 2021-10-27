68c3528ad849ee5a8edcb0d3222f6f68e8be0b9a
Gareth Fuller <gareth.a.f@gmail.com>
2021-10-27 14:06:04 +0100

    feat: Display tx errors in preview modal

---
+++ src/composables/useTransactionErrors.ts
@@ -0,0 +1,52 @@
+import { useI18n } from 'vue-i18n';
+
+export type TransactionError = {
+  title: string;
+  description: string;
+};
+
+export default function useTranasactionErrors() {
+  /**
+   * COMPOSABLES
+   */
+  const { t } = useI18n();
+
+  /**
+   * ERRORS
+   */
+  const gasTooLowError: TransactionError = {
+    title: t('transactionErrors.gasTooLow.title'),
+    description: t('transactionErrors.gasTooLow.description')
+  };
+
+  const slippageError: TransactionError = {
+    title: t('transactionErrors.slippage.title'),
+    description: t('transactionErrors.slippage.description')
+  };
+
+  function defaultError(message = ''): TransactionError {
+    return {
+      title: t('transactionErrors.default.title'),
+      description: `${message} ${t(
+        'transactionErrors.default.description'
+      )}.`.trim()
+    };
+  }
+
+  /**
+   * METHODS
+   */
+  function parseError(error): TransactionError {
+    if (error?.message) {
+      if (error.message.includes('-32010')) return gasTooLowError;
+      if (error.message.includes('BAL#507')) return slippageError;
+
+      return defaultError(error.message);
+    }
+    return defaultError();
+  }
+
+  return {
+    parseError
+  };
+}
