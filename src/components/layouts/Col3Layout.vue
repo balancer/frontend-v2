edb6d13a6104783f1521b43ab812c717ea22082f
Gareth Fuller <gareth.a.f@gmail.com>
2021-10-29 17:08:02 +0100

    Unify tradn & invest/withdraw 3 column layouts (#1016)
    * Move page layouts into pages folder
    
    * Unify trade & invest/withdraw layouts
    
    * Lint fix
    
    * Match trade module header shadow
    
    * Revert back to number input
    
    * Fix layout for large screens

---
+++ src/components/layouts/Col3Layout.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+/**
+ * TYPES
+ */
+type Props = {
+  mobileGuttersFirst?: boolean;
+  mobileGuttersLast?: boolean;
+  mobileHideGutters?: boolean;
+  offsetGutters?: boolean;
+};
+
+/**
+ * PROPS
+ */
+const props = withDefaults(defineProps<Props>(), {
+  mobileGuttersFirst: false,
+  mobileGuttersLast: false,
+  mobileHideGutters: false,
+  offsetGutters: false
+});
+
+/**
+ * COMPUTED
+ */
+const gutterClasses = computed(() => ({
+  'order-1 lg:order-2': props.mobileGuttersFirst,
+  'order-3 lg:order-2': props.mobileGuttersLast,
+  'hidden lg:block': props.mobileHideGutters,
+  'mt-6': props.offsetGutters
+}));
+
+const centerClasses = computed(() => ({
+  'order-2': props.mobileGuttersFirst,
+  'order-1 lg:order-2': props.mobileGuttersLast
+}));
+</script>
+
+<template>
+  <div class="layout-container">
+    <div :class="['gutter-col', gutterClasses]">
+      <slot name="gutterLeft" />
+    </div>
+    <div :class="['center-col', centerClasses]">
+      <slot />
+    </div>
+    <div :class="['gutter-col', gutterClasses]">
+      <slot name="gutterRight" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.layout-container {
+  @apply max-w-6xl mx-auto px-0 sm:px-4;
+  @apply grid grid-cols-1 lg:grid-cols-7 gap-y-8 gap-x-0 lg:gap-x-8;
+}
+
+.center-col {
+  @apply col-span-3;
+}
+
+.gutter-col {
+  @apply col-span-2;
+}
+</style>
