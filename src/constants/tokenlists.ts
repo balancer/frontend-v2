ce6d6ff6b665003682bb406ebd458baa41cfd641
dospore <tetthera@gmail.com>
2021-11-01 14:14:36 +1000

    Add TCR token list

--- src/constants/tokenlists.ts
+++ src/constants/tokenlists.ts
@@ -61,6 +61,8 @@ export const TOKEN_LIST_MAP: TokenListMapByNetwork = {
       Vetted:
         'https://storageapi.fleek.co/tomafrench-team-bucket/arbitrum.vetted.tokenlist.json'
     },
-    External: []
+    External: [
+      'https://raw.githubusercontent.com/dospore/tracer-balancer-token-list/master/tokens.json'
+    ]
   }
 };
