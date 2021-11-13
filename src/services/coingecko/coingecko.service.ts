114b33c4b34e8ea9ba0ba38fd7baa46133d1d56d
Daniel <danielBeets@protonmail.com>
2021-11-13 05:44:54 -1000

    add multicall2

--- src/services/coingecko/coingecko.service.ts
+++ src/services/coingecko/coingecko.service.ts
@@ -7,7 +7,8 @@ export const getNativeAssetId = (chainId: string): string => {
     '1': 'ethereum',
     '42': 'ethereum',
     '137': 'matic-network',
-    '42161': 'ethereum'
+    '42161': 'ethereum',
+    '250': 'fantom'
   };
 
   return mapping[chainId] || 'ethereum';
@@ -18,7 +19,8 @@ export const getPlatformId = (chainId: string): string => {
     '1': 'ethereum',
     '42': 'ethereum',
     '137': 'polygon-pos',
-    '42161': 'arbitrum-one'
+    '42161': 'arbitrum-one',
+    '250': 'fantom'
   };
 
   return mapping[chainId] || 'ethereum';
