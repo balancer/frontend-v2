ffaa3b6b99a6dce2d50b1a2940f5f63619d1cb14
Gareth Fuller <gareth.a.f@gmail.com>
2021-12-11 13:38:27 +0000

    Remove prices missing warning from StablePhantom pool page

--- src/pages/pool/_id.vue
+++ src/pages/pool/_id.vue
@@ -326,8 +326,8 @@ export default defineComponent({
         const tokensWithPrice = Object.keys(prices.value);
 
         const tokens =
-          isStablePhantomPool.value && pool.value.linearPoolTokensMap != null
-            ? Object.keys(pool.value.linearPoolTokensMap)
+          isStablePhantomPool.value && pool.value.mainTokens
+            ? pool.value.mainTokens
             : pool.value.tokenAddresses;
 
         return !tokens.every(token => tokensWithPrice.includes(token));
