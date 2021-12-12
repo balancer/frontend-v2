1da2971faee4008cf8fd878eedbfe69a2350ca5e
Gareth Fuller <gareth.a.f@gmail.com>
2021-12-12 15:55:36 +0000

    Add StablePhantom logic to pools query

--- src/composables/queries/useUserPoolsQuery.ts
+++ src/composables/queries/useUserPoolsQuery.ts
@@ -5,12 +5,18 @@ import { flatten, keyBy } from 'lodash';
 import { bnum, forChange } from '@/lib/utils';
 import QUERY_KEYS from '@/constants/queryKeys';
 import { balancerSubgraphService } from '@/services/balancer/subgraph/balancer-subgraph.service';
-import { DecoratedPoolWithShares } from '@/services/balancer/subgraph/types';
+import {
+  DecoratedPoolWithShares,
+  LinearPool,
+  Pool
+} from '@/services/balancer/subgraph/types';
 import useWeb3 from '@/services/web3/useWeb3';
 import useTokenLists from '../useTokenLists';
 import useTokens from '../useTokens';
 import useUserSettings from '../useUserSettings';
 import useNetwork from '../useNetwork';
+import { isStablePhantom, lpTokensFor } from '../usePool';
+import { getAddress } from '@ethersproject/address';
 
 type UserPoolsQueryResponse = {
   pools: DecoratedPoolWithShares[];
@@ -38,6 +44,64 @@ export default function useUserPoolsQuery(
       isWalletReady.value && account.value != null && !loadingTokenLists.value
   );
 
+  function removePreMintedBPT(pool: Pool): Pool {
+    const poolAddress = balancerSubgraphService.pools.addressFor(pool.id);
+    // Remove pre-minted BPT token if exits
+    pool.tokensList = pool.tokensList.filter(
+      address => address !== poolAddress.toLowerCase()
+    );
+    return pool;
+  }
+
+  /**
+   * fetches StablePhantom linear pools and extracts
+   * required attributes.
+   */
+  async function getLinearPoolAttrs(pool: Pool): Promise<Pool> {
+    // Fetch linear pools from subgraph
+    const linearPools = (await balancerSubgraphService.pools.get(
+      {
+        where: {
+          address_in: pool.tokensList,
+          totalShares_gt: -1 // Avoid the filtering for low liquidity pools
+        }
+      },
+      { mainIndex: true, wrappedIndex: true }
+    )) as LinearPool[];
+
+    const linearPoolTokensMap: Pool['linearPoolTokensMap'] = {};
+
+    // Inject main/wrapped tokens into pool schema
+    linearPools.forEach(linearPool => {
+      if (!pool.mainTokens) pool.mainTokens = [];
+      if (!pool.wrappedTokens) pool.wrappedTokens = [];
+
+      const index = pool.tokensList.indexOf(linearPool.address.toLowerCase());
+
+      pool.mainTokens[index] = getAddress(
+        linearPool.tokensList[linearPool.mainIndex]
+      );
+      pool.wrappedTokens[index] = getAddress(
+        linearPool.tokensList[linearPool.wrappedIndex]
+      );
+
+      linearPool.tokens
+        .filter(token => token.address !== linearPool.address)
+        .forEach(token => {
+          const address = getAddress(token.address);
+
+          linearPoolTokensMap[address] = {
+            ...token,
+            address
+          };
+        });
+    });
+
+    pool.linearPoolTokensMap = linearPoolTokensMap;
+
+    return pool;
+  }
+
   /**
    * QUERY PROPERTIES
    */
@@ -59,7 +123,24 @@ export default function useUserPoolsQuery(
       }
     });
 
-    const tokens = flatten(pools.map(pool => pool.tokensList));
+    for (let i = 0; i < pools.length; i++) {
+      const isStablePhantomPool = isStablePhantom(pools[i].poolType);
+
+      if (isStablePhantomPool) {
+        pools[i] = removePreMintedBPT(pools[i]);
+        pools[i] = await getLinearPoolAttrs(pools[i]);
+      }
+    }
+
+    const tokens = flatten(
+      pools.map(pool => {
+        return [
+          ...pool.tokensList,
+          ...lpTokensFor(pool),
+          balancerSubgraphService.pools.addressFor(pool.id)
+        ];
+      })
+    );
     await injectTokens(tokens);
     await forChange(dynamicDataLoading, false);
     const decoratedPools = await balancerSubgraphService.pools.decorate(
