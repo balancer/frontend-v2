82ee5fa59349db3b7a8882b225e91bc72910c17e
Gareth Fuller <gareth.a.f@gmail.com>
2021-12-14 14:29:38 +0000

    Add feature alert

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { defineComponent, onBeforeMount, watch, ref } from 'vue';
 import { VueQueryDevTools } from 'vue-query/devtools';
 import { useStore } from 'vuex';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import BigNumber from 'bignumber.js';
 import * as Layouts from '@/pages/_layouts';
 import useWeb3Watchers from '@/composables/watchers/useWeb3Watchers';
@@ -13,6 +13,13 @@ import Notifications from '@/components/notifications/Notifications.vue';
 import useGnosisSafeApp from './composables/useGnosisSafeApp';
 import useGlobalQueryWatchers from './composables/watchers/useGlobalQueryWatchers';
 import usePoolCreationWatcher from './composables/watchers/usePoolCreationWatcher';
+import useAlerts, {
+  Alert,
+  AlertPriority,
+  AlertType
+} from './composables/useAlerts';
+import { isMainnet } from './composables/useNetwork';
+import { useI18n } from 'vue-i18n';
 
 BigNumber.config({ DECIMAL_PLACES: DEFAULT_TOKEN_DECIMALS });
 
@@ -40,6 +47,29 @@ export default defineComponent({
     const { isWalletSelectVisible, toggleWalletSelectModal } = useWeb3();
     const route = useRoute();
     const store = useStore();
+    const router = useRouter();
+    const { addAlert } = useAlerts();
+    const { t } = useI18n();
+
+    // Temporary feature alert for Balancer boosted pools.
+    if (isMainnet) {
+      const featureAlert: Alert = {
+        id: 'boosted-pools',
+        priority: AlertPriority.LOW,
+        label: t('alerts.boostedPools'),
+        type: AlertType.FEATURE,
+        actionOnClick: true,
+        action: () =>
+          router.push({
+            name: 'pool',
+            params: {
+              id:
+                '0x7b50775383d3d6f0215a8f290f2c9e2eebbeceb20000000000000000000000fe'
+            }
+          })
+      };
+      addAlert(featureAlert);
+    }
 
     /**
      * CALLBACKS
