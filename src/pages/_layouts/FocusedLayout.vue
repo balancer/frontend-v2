b49a0cd8bcc53e068f3aa421b76ce0623749f84f
1saf <me@ansaf.net>
2021-12-16 20:55:28 +1100

    feature: add a focusedlayout and place pool creation inside the layout

---
+++ src/pages/_layouts/FocusedLayout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="pb-16">
+    <div class="layout-header mb-12">
+      <div></div>
+      <router-link :to="{ name: 'home' }">
+        <BalIcon name="x" size="lg" />
+      </router-link>
+    </div>
+
+    <router-view :key="$route.path" />
+  </div>
+</template>
+
+<style scoped>
+.layout-header {
+  @apply h-16;
+  @apply px-4 lg:px-6;
+  @apply flex items-center justify-between;
+}
+</style>
--- src/pages/_layouts/index.ts
+++ src/pages/_layouts/index.ts
@@ -1,3 +1,4 @@
 export { default as DefaultLayout } from './DefaultLayout.vue';
 export { default as BlankLayout } from './BlankLayout.vue';
 export { default as PoolTransferLayout } from './PoolTransferLayout.vue';
+export { default as FocusedLayout } from './FocusedLayout.vue';
