2d040e14b88cf001013dd7c0336ad7ee021a22a3
pkattera <kattera@gmail.com>
2021-12-21 16:39:47 -0500

    Base structure for new policy pages

--- src/components/web3/WalletSelectModal.vue
+++ src/components/web3/WalletSelectModal.vue
@@ -4,16 +4,31 @@
     @close="$emit('close')"
     title="Connect to a wallet"
   >
+    <p class="pb-3 text-sm">By connecting a wallet, I agree to Balancer Labs’ 
+
+
+      <router-link :to="{ name: 'terms-of-use' }" target="_blank">
+        <span className='text-blue-500 hover:text-blue-800 focus:text-blue-800 transition-all hover:underline focus:underline'>Terms of Use</span>, 
+      </router-link>
+
+      <router-link :to="{ name: 'cookies-policy' }" target="_blank">
+        <span className='text-blue-500 hover:text-blue-800 focus:text-blue-800 transition-all hover:underline focus:underline'>Cookies Policy</span> and 
+      </router-link>
+
+      <router-link :to="{ name: 'privacy-policy' }" target="_blank">
+        <span className='text-blue-500 hover:text-blue-800 focus:text-blue-800 transition-all hover:underline focus:underline'>Privacy Policy</span>.
+      </router-link>
+
+    </p>
     <WalletButton v-for="wallet in wallets" :wallet="wallet" :key="wallet" />
     <div
       class="p-4 rounded-lg bg-gradient-to-b from-gray-50 dark:from-gray-900 to-gray-100 dark:to-gray-850"
     >
       <h6>New to Ethereum?</h6>
       <p class="text-sm">
-        Balancer is a DeFi app on Ethereum. To invest and trade here, you'll
-        first need to set up an Ethereum Wallet.
+        Balancer is a DeFi app on Ethereum. Set up an Ethereum Wallet to Invest and Trade here.
         <BalLink :href="EXTERNAL_LINKS.Ethereum.Wallets" external>
-          Learn More
+          Learn&nbsp;More
           <span class="align-middle"
             ><BalIcon name="arrow-up-right" size="sm"
           /></span>
---
+++ src/pages/_layouts/ContentLayout.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import AppNav from '@/components/navs/AppNav/AppNav.vue';
+
+/**
+ * COMPOSABLES
+ */
+const route = useRoute();
+
+</script>
+
+<template>
+<AppNav />
+  <section class="dark:bg-gray-900 lg:py-8">
+    <div class="content-container">
+       <router-view/>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+h2,
+h3,
+p,
+ul,
+ol,
+em {
+  @apply pb-3;
+}
+.content-container {
+  @apply w-full max-w-4xl px-4 md:p-8 lg:p-12 mx-auto py-8 bg-white dark:bg-gray-900;
+  min-height: calc(100vh - 140px);
+}
+.subsection {
+  @apply mb-8;
+}
+
+h1,
+h2,
+h3 {
+  @apply font-body tracking-tight;
+  font-variation-settings: 'wght' 500;
+}
+
+h3 {
+  @apply font-body tracking-tight;
+  font-variation-settings: 'wght' 600;
+}
+
+li {
+  @apply list-disc ml-8 pb-2;
+}
+
+ol > li {
+  @apply list-decimal;
+}
+
+em {
+  font-style: italic;
+  font-variation-settings: 'ital' 1;
+  font-synthesis: none;
+}
+</style>
+
+
+
+
+
+
