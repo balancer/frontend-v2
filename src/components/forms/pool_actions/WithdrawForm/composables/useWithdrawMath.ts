73c1faf41bb55ef8e9822664afcf17e4411a3c55
Gareth Fuller <gareth.a.f@gmail.com>
2022-01-12 14:50:25 +0000

    Lint fix

--- src/components/forms/pool_actions/WithdrawForm/composables/useWithdrawMath.ts
+++ src/components/forms/pool_actions/WithdrawForm/composables/useWithdrawMath.ts
@@ -170,7 +170,7 @@ export default function useWithdrawMath(
       .toFixed(poolDecimals.value, OldBigNumber.ROUND_DOWN);
     // If the user's bpt balance is greater than the withdrawal limit for
     // weighted pools we need to return the poolMax bpt value.
-    console.log('maxes', bptBalance.value, poolMax)
+    console.log('maxes', bptBalance.value, poolMax);
     return OldBigNumber.min(bptBalance.value, poolMax).toString();
   });
 
@@ -350,7 +350,7 @@ export default function useWithdrawMath(
     if (amountExceedsPoolBalance.value) return 1;
     if (!hasAmounts.value || isProportional.value) return 0;
 
-    console.log('fullBPTIn.value', fullBPTIn.value)
+    console.log('fullBPTIn.value', fullBPTIn.value);
     return poolCalculator
       .priceImpact(fullAmounts.value, {
         exactOut: exactOut.value,
