ec96bd0a5fe7c845ea65a8048858fe1f6ea37083
1saf <me@ansaf.net>
2022-01-17 18:59:44 +1100

    update: convert balstack to a render function component

--- src/components/_global/BalStack/BalStack.vue
+++ src/components/_global/BalStack/BalStack.vue
@@ -1,19 +1,8 @@
-<script setup lang="ts">
-import { uniqueId } from 'lodash';
-import { computed, useSlots } from 'vue';
+<script lang="ts">
+import {  defineComponent, PropType, h } from 'vue';
 
 type Spacing = 'xs' | 'sm' | 'base' | 'lg' | 'xl' | '2xl' | 'none';
-type Props = {
-  vertical?: boolean;
-  horizontal?: boolean;
-  spacing?: Spacing;
-  withBorder?: boolean;
-  ref?: any;
-  align?: 'center' | 'start' | 'end' | 'between';
-  justify?: 'center' | 'start' | 'end' | 'between';
-  isDynamic?: boolean;
-  expandChildren?: boolean;
-};
+type Alignment = 'center' | 'start' | 'end' | 'between';
 
 const SpacingMap: Record<Spacing, number> = {
   xs: 1,
@@ -25,63 +14,96 @@ const SpacingMap: Record<Spacing, number> = {
   none: 0
 };
 
-const props = withDefaults(defineProps<Props>(), {
-  spacing: 'base'
-});
-
-const spacingClass = computed(() => {
-  const spacingType = props.vertical ? 'mb' : 'mr';
-  return `${spacingType}-${SpacingMap[props.spacing]}`;
-});
-
-const stackId = uniqueId();
-const slots = useSlots();
-
-const slotsWithContent = computed(() => {
-  if (props.isDynamic) {
-    if (Array.isArray(slots.default()[0].children)) {
-      return (slots.default()[0].children as any[]).filter(
-        child => child.children !== 'v-if'
-      );
+export default defineComponent({
+  props: {
+    vertical: { type: Boolean, default: () => false },
+    horizontal: { type: Boolean, default: () => false },
+    spacing: {
+      type: String as PropType<Spacing>,
+      default: () => 'base'
+    },
+    withBorder: {
+      type: Boolean,
+      default: () => false
+    },
+    align: {
+      type: String as PropType<Alignment>
+    },
+    justify: {
+      type: String as PropType<Alignment>
+    },
+    isDynamic: {
+      type: Boolean,
+      default: () => false
+    },
+    expandChildren: {
+      type: Boolean,
+      default: () => false
     }
+  },
+  setup(props, { slots, attrs }) {
+    return {
+      slotsWithContent: [],
+      slots,
+      attrs
+    };
+  },
+  render() {
+    const spacingType = this.vertical ? 'mb' : 'mr';
+    const spacingClass = `${spacingType}-${SpacingMap[this.spacing]}`;
+
+    const vNodes = this.$slots.default() || [];
+    const children = vNodes.filter(vNode => vNode.children !== 'v-if');
+    const styledChildren = children.map((child, childIndex) => {
+      let styledNestedChildren = child.children;
+      if (Array.isArray(styledNestedChildren)) {
+        const nonNullishChildren = styledNestedChildren.filter(
+          nestedChild => nestedChild !== undefined || nestedChild !== null
+        );
+        styledNestedChildren = nonNullishChildren.map(
+          (nestedChild, nestedChildIndex) => {
+            return h(nestedChild, {
+              class:
+                nestedChildIndex !== nonNullishChildren.length - 1
+                  ? spacingClass
+                  : null
+            });
+          }
+        );
+        return h(
+          child,
+          {
+            class: childIndex !== children.length - 1 ? spacingClass : null
+          },
+          [styledNestedChildren]
+        );
+      }
+      return h(child, {
+        class: childIndex !== children.length - 1 ? spacingClass : null
+      });
+    });
+    return h(
+      'div',
+      {
+        attrs: this.$attrs,
+        class: [
+          'flex',
+          {
+            'flex-row': this.horizontal,
+            'flex-col': this.vertical,
+            'items-center': this.align === 'center',
+            'items-start': this.align === 'start',
+            'items-end': this.align === 'end',
+            'items-between': this.align === 'between',
+            'justify-center': this.justify === 'center',
+            'justify-start': this.justify === 'start',
+            'justify-end': this.justify === 'end',
+            'justify-between': this.justify === 'between'
+          }
+        ]
+      },
+      [styledChildren]
+    );
   }
-  return slots.default().filter(slot => {
-    if (slot.children !== 'v-if') return true;
-    return false;
-  });
 });
 </script>
-
-<template>
-  <div
-    :class="[
-      'flex',
-      {
-        'flex-row': horizontal,
-        'flex-col': vertical,
-        'items-center': align === 'center',
-        'items-start': align === 'start',
-        'items-end': align === 'end',
-        'items-between': align === 'between',
-        'justify-center': justify === 'center',
-        'justify-start': justify === 'start',
-        'justify-end': justify === 'end',
-        'justify-between': justify === 'between'
-      }
-    ]"
-  >
-    <component
-      v-for="(child, i) in slotsWithContent"
-      :key="`stack-${stackId}-child-${i}-${child?.key || ''}`"
-      :is="child"
-      :class="{
-        [spacingClass]: i !== slotsWithContent.length - 1,
-        'border-b': i !== slotsWithContent.length - 1 && withBorder && vertical,
-        'border-r':
-          i !== slotsWithContent.length - 1 && withBorder && horizontal,
-        'w-full': expandChildren,
-        'dark:border-gray-600': true
-      }"
-    />
-  </div>
-</template>
