41ed142727178d962333ad8049369a52ede1a437
1saf <me@ansaf.net>
2022-01-27 15:24:30 +1100

    update: use a form cache

--- src/components/_global/BalForm/useForm.ts
+++ src/components/_global/BalForm/useForm.ts
@@ -1,134 +1,180 @@
-import { Rules } from '@/types';
-import { onBeforeMount, reactive, toRefs, ToRefs, Ref } from 'vue';
+import { RuleFunction, Rules } from '@/types';
+import { get, isEmpty, isPlainObject, set } from 'lodash';
+import { onBeforeMount, reactive, ref, Ref } from 'vue';
 
-type UseFormRequest<DefaultValues> = {
+/**
+ * TYPES
+ */
+type UseFormRequest<T> = {
   name: string;
-  defaultValues?: Partial<
-    { [Property in keyof DefaultValues]: Ref<any> | any }
-  >;
+  defaultValues?: Partial<T>;
 };
 
-export type FormState = {
+export type FormState<T> = {
   name: string;
-  values: Record<string, any>;
-  errors: Record<string, string[]>;
-  rules: Record<string, Rules | undefined>;
-  defaultValues: Record<string, any>;
+  values: T;
+  errors: { [key in keyof T]: string[] };
+  rules: { [key in keyof T]: RuleFunction[] | undefined };
+  defaultValues: T;
 };
 
-export type FormInstance = ToRefs<FormState> & {
+export type FormInstance<T> = FormState<T> & {
   register: (name: string, rules?: Rules) => any;
   onChange: (name: string, value: any) => void;
   handleSubmit: (submit: () => void) => void;
+  setValue: (name: string, value: any) => void;
 };
+
+export type ReactiveFormInstance<T> = FormInstance<T> & {
+  values: Record<string, Ref>;
+  errors: Record<string, Ref>;
+};
+
+function validateAgainst(value: any, rules?: RuleFunction[]) {
+  if (rules?.length) {
+    const failedRules = rules
+      .map(rule => {
+        const pass = rule(value);
+        if (typeof pass === 'string') {
+          return pass;
+        }
+      })
+      .filter(result => result !== undefined);
+    return failedRules;
+  }
+  return [];
+}
+
+function toRefRecursive(value: Record<string, any>) {
+  if (isEmpty(value)) return {};
+  const refObj = {};
+
+  for (const key of Object.keys(value)) {
+    // not a leaf node
+    if (isPlainObject(value[key])) {
+      refObj[key] = toRefRecursive(value[key]);
+    } else {
+      refObj[key] = ref(value[key]);
+    }
+  }
+  return refObj;
+}
+
+/**
+ * GLOBAL
+ */
+const formCache = reactive<Record<string, FormInstance<any>>>({});
+export function useFormCache(name: string) {
+  return formCache[name];
+}
 
 export const FormContextSymbol = Symbol('FORM_CONTEXT');
 
-export default function useForm<DefaultValues>({
+export default function useForm<T>({
   name: formName,
   defaultValues = {}
-}: UseFormRequest<DefaultValues>): FormInstance {
+}: UseFormRequest<T>) {
+  // this initialisation is not in a lifecycle hook as
+  // the return operation of this function executes before
+  // the beforeMount hook, which means that an undefined
+  // value is returned to consumers.
+  if (!formCache[formName]) {
+    // each useForm call spawns its own instance to support
+    // the usage of multiple forms on the same page
+    formCache[formName] = reactive<FormInstance<T>>({
+      name: formName,
+      values: {} as T,
+      errors: {} as { [key in keyof T]: string[] },
+      rules: {} as { [key in keyof T]: RuleFunction[] | undefined },
+      defaultValues: {} as T,
+      register,
+      onChange,
+      setValue,
+      handleSubmit
+    }) as any;
+    registerDefaultValues();
+  }
+
   onBeforeMount(() => {
     if (!formName) {
       throw new Error(
         `Failed to create a form instance. No form name was provided.`
       );
     }
-    formInstance.name = formName;
-    registerDefaultValues();
   });
 
   /**
    * STATE
    */
-  // each useForm call spawns its own instance to support
-  // the usage of multiple forms on the same page
-  const formInstance = reactive<FormState>({
-    name: '',
-    values: {},
-    errors: {},
-    rules: {},
-    defaultValues: {}
-  });
+  const formState = formCache[formName];
 
   /**
    * METHODS
    */
   function registerDefaultValues() {
-    formInstance.defaultValues = defaultValues;
+    formCache[formName].defaultValues = defaultValues;
     for (const key of Object.keys(defaultValues)) {
-      formInstance.values[key] = defaultValues[key];
+      formCache[formName].values[key] = defaultValues[key];
     }
   }
 
   // this method is used to pass in a form value to a field
   // make sure it is passed to the :value prop or an equivalent
   // as it is bound only one way (read only)
-  function register(name: string, rules?: Rules) {
-    // invalid name
-    if (name === '') {
+  function register(key: string, rules?: Rules) {
+    // invalid key
+    if (key === '') {
       throw new Error(
         `Failed to register input for form [${formName}], name was empty string.`
       );
     }
     // if the field isn't registered yet set it up with an empty value
-    if (!formInstance.values[name]) {
-      formInstance.values[name] = '';
+    if (get(formState.values, key) === undefined) {
+      set(formState.values, key, '');
     }
     // register any rules the field has
-    if (!formInstance.rules[name] && rules?.length) {
-      formInstance.rules[name] = rules;
+    if (!formState.rules[key] && rules?.length) {
+      formState.rules[key] = rules;
     }
+
     // return the value
-    return formInstance.values[name];
+    return get(formState.values, key);
   }
 
   // call this function to update bound form values
-  function onChange(name: string, value: any) {
-    if (formInstance.values[name] === undefined) {
+  function onChange(key: string, value: any) {
+    if (get(formState.values, key) === undefined) {
       throw new Error(
-        `[${name}] is not a registered input for form [${formInstance.name}]`
+        `[${key}] is not a registered input for form [${formState.name}]`
       );
     }
-    formInstance.values[name] = value;
+    set(formState.values, key, value);
 
     // validate against any rules that might be registered
     // for this form field
-    const rules = formInstance.rules[name];
-    if (rules?.length) {
-      const failedRules = rules
-        .map(rule => {
-          const pass = rule(formInstance.values[name]);
-          if (typeof pass === 'string') {
-            return pass;
-          }
-        })
-        .filter(result => result !== undefined);
-      formInstance.errors[name] = failedRules as string[];
-    }
+    const rules = formState.rules[key];
+    const failedRules = validateAgainst(value, rules);
+    formState.errors[key] = failedRules as string[];
+  }
+
+  function setValue(key: string, value: any) {
+    onChange(key, value);
   }
 
   function handleSubmit(submit: () => void) {
     return function() {
-      const hasErrors = Object.values(formInstance).every(
+      const hasErrors = Object.values(formState).every(
         errorListForInput => errorListForInput.length === 0
       );
       if (!hasErrors) {
         submit();
       } else {
         console.log(
-          `Form [${formName}] has errors. ${JSON.stringify(
-            formInstance.errors
-          )}`
+          `Form [${formName}] has errors. ${JSON.stringify(formState.errors)}`
         );
       }
     };
   }
 
-  return {
-    ...toRefs(formInstance),
-    register,
-    onChange,
-    handleSubmit
-  };
+  return formCache[formName];
 }
