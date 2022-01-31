42dde6f86f85e746cc2ed4074815e990c307fdbd
Evgeny Boxer <evgeny.boxer@gmail.com>
2022-01-31 14:35:47 +1100

    refactor: simplify tests

--- src/services/balancer/subgraph/entities/pools/helpers.spec.ts
+++ src/services/balancer/subgraph/entities/pools/helpers.spec.ts
@@ -1,47 +1,70 @@
-import { pickBy } from 'lodash';
+import { parseUnits } from '@ethersproject/units';
 
-import mockExcludedAddresses from './__mocks__/excludedAddresses';
-import mockDecoratedPool from './__mocks__/decoratedPool';
+import decoratedPool from './__mocks__/decoratedPool';
 
-import { removeAddressesFromTotalLiquidity } from './helpers';
+import {
+  ExcludedAddresses,
+  removeAddressesFromTotalLiquidity
+} from './helpers';
 
 describe('helpers', () => {
   describe('removeAddressesFromTotalLiquidity tests', () => {
+    const mockedDecoratedPool = {
+      ...decoratedPool,
+      totalShares: 1000,
+      totalLiquidity: 2000
+    };
+
     it('Should correctly remove excluded addresses', () => {
+      const excludedAddresses: ExcludedAddresses = {
+        [mockedDecoratedPool.address]: {
+          '0x359f4fe841f246a095a82cb26f5819e10a91fe0d': parseUnits('250'),
+          '0x001c249c09090d79dc350a286247479f08c7aad7': parseUnits('250')
+        }
+      };
+
       const miningTotalLiquidity = removeAddressesFromTotalLiquidity(
-        mockExcludedAddresses,
-        mockDecoratedPool,
-        mockDecoratedPool.totalLiquidity
+        excludedAddresses,
+        mockedDecoratedPool,
+        mockedDecoratedPool.totalLiquidity
       );
 
-      expect(miningTotalLiquidity).toEqual(
-        '2390.29774029333387320036489667810835644165'
-      );
+      expect(miningTotalLiquidity).toEqual('1000');
     });
 
     it('Should return unchanged totalLiquidity when excluded addresses belong to different pools', () => {
-      const mockExcludedAddressesWithoutPool = pickBy(
-        mockExcludedAddresses,
-        (_, poolAddress) => poolAddress !== mockDecoratedPool.address
-      );
+      const excludedAddresses: ExcludedAddresses = {
+        '0x8f4205e1604133d1875a3E771AE7e4F2b0865639': {
+          '0x567d220B0169836cBF351DF70A9c517096ec9De7': parseUnits('100'),
+          '0x10A19e7eE7d7F8a52822f6817de8ea18204F2e4f': parseUnits('300')
+        },
+        '0x06Df3b2bbB68adc8B0e302443692037ED9f91b42': {
+          '0x9cff0533972da48ac05a00a375cc1a65e87da7ec': parseUnits('50'),
+          '0x7a32aa9a16a59cb335ffdee3dc94024b7f8a9a47': parseUnits('400')
+        }
+      };
 
       const miningTotalLiquidity = removeAddressesFromTotalLiquidity(
-        mockExcludedAddressesWithoutPool,
-        mockDecoratedPool,
-        mockDecoratedPool.totalLiquidity
+        excludedAddresses,
+        mockedDecoratedPool,
+        mockedDecoratedPool.totalLiquidity
       );
 
-      expect(miningTotalLiquidity).toEqual(mockDecoratedPool.totalLiquidity);
+      expect(miningTotalLiquidity).toEqual(
+        mockedDecoratedPool.totalLiquidity.toString()
+      );
     });
 
     it('Should return unchanged totalLiquidity when excludedAddresses is null', () => {
       const miningTotalLiquidity = removeAddressesFromTotalLiquidity(
         null,
-        mockDecoratedPool,
-        mockDecoratedPool.totalLiquidity
+        mockedDecoratedPool,
+        mockedDecoratedPool.totalLiquidity
       );
 
-      expect(miningTotalLiquidity).toEqual(mockDecoratedPool.totalLiquidity);
+      expect(miningTotalLiquidity).toEqual(
+        mockedDecoratedPool.totalLiquidity.toString()
+      );
     });
   });
 });
