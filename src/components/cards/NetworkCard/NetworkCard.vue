33445902f6dbb0b2c68f7573b799fc847a3fdd79
1saf <me@ansaf.net>
2022-02-01 14:18:01 +1100

    feature: add common network card component

---
+++ src/components/cards/NetworkCard/NetworkCard.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import useWeb3 from '@/services/web3/useWeb3';
+import { computed } from 'vue';
+
+/**
+ * TYPES
+ */
+type Props = {
+  title: string;
+};
+
+withDefaults(defineProps<Props>(), {
+  title: ''
+});
+
+/**
+ * COMPOSABLES
+ */
+const { userNetworkConfig } = useWeb3();
+
+/**
+ * COMPUTED
+ */
+const networkName = computed(() => userNetworkConfig.value?.name);
+</script>
+
+<template>
+  <BalCard shadow="xl" noBorder>
+    <BalStack vertical spacing="sm">
+      <BalStack vertical spacing="xs">
+        <span class="text-xs text-gray-700 dark:text-gray-500">{{
+          networkName
+        }}</span>
+        <h5 class="font-bold dark:text-gray-300">
+          {{ title }}
+        </h5>
+      </BalStack>
+    </BalStack>
+  </BalCard>
+</template>
