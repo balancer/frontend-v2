96daa1887503b4e149c0f49e84faae47387efb1c
Gareth Fuller <gareth.a.f@gmail.com>
2022-02-08 09:39:21 +0000

    Show empty state until we can solve boosted pool chart

--- src/components/contextual/pages/pool/PoolChart.vue
+++ src/components/contextual/pages/pool/PoolChart.vue
@@ -70,6 +70,8 @@ const history = computed(() => {
     return [];
   }
 
+  if (supportsPoolLiquidity.value) return [];
+
   // Prices are required when not using pool liquidity
   if (!supportsPoolLiquidity.value && pricesTimestamps.length === 0) {
     return [];
