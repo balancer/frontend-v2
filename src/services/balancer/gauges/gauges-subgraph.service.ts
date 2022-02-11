62e69d4c34261b67098ad49bb49d1ba7a54d3eed
Gareth Fuller <gareth.a.f@gmail.com>
2022-02-11 15:19:43 +0000

    Remove rpcProvider from service

--- src/services/balancer/gauges/gauges-subgraph.service.ts
+++ src/services/balancer/gauges/gauges-subgraph.service.ts
@@ -1,14 +1,10 @@
 import { gaugesSubgraphClient } from './gauges-subgraph.client';
-import { rpcProviderService as _rpcProviderService } from '@/services/rpc-provider/rpc-provider.service';
 import { GaugesEntity } from './entities/gauges/gauges.entity';
 
 export class GaugesSubgraphService {
   gauges: GaugesEntity;
 
-  constructor(
-    readonly client = gaugesSubgraphClient,
-    readonly rpcProviderService = _rpcProviderService
-  ) {
+  constructor(readonly client = gaugesSubgraphClient) {
     this.gauges = new GaugesEntity(this);
   }
 }
