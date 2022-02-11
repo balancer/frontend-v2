e5b933ef9b164a096ded78c25ae26f637b17d89b
Gareth Fuller <gareth.a.f@gmail.com>
2022-02-11 14:52:21 +0000

    Remove unneeded fix and add node extension where nock used

--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -1,14 +1,5 @@
 import nock from 'nock';
-import axios from 'axios';
 import '@testing-library/jest-dom';
-
-// If you are using jsdom, axios will default to using the XHR adapter which
-// can't be intercepted by nock. So, configure axios to use the node adapter.
-//
-// References:
-// https://github.com/nock/nock/issues/699#issuecomment-272708264
-// https://github.com/axios/axios/issues/305
-axios.defaults.adapter = require('axios/lib/adapters/http');
 
 /**
  * HTTP Requests
