bb85d07908b8975abd99e3fdadeef8a33b9511b2
Markus Buhatem Koch <markusbkoch@gmail.com>
2022-03-04 17:20:41 +0000

    add polygon and arbitrum

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -171,6 +171,91 @@ const POOLS_MAINNET: Pools = {
   }
 };
 
+const POOLS_POLYGON: Pools = {
+  IdsMap: {
+    staBAL:
+      '0x0000000000000000000000000000000000000000000000000000000000000000',
+    bbAaveUSD:
+      '0x0000000000000000000000000000000000000000000000000000000000000000'
+  },
+  Pagination: {
+    PerPage: 10
+  },
+  DelegateOwner: '0xba1ba1ba1ba1ba1ba1ba1ba1ba1ba1ba1ba1ba1b',
+  ZeroAddress: '0x0000000000000000000000000000000000000000',
+  DynamicFees: {
+    Gauntlet: ['']
+  },
+  BlockList: [''],
+  ExcludedPoolTypes: ['Element', 'AaveLinear', 'Linear'],
+  Stable: {
+    AllowList: [
+      '0x06df3b2bbb68adc8b0e302443692037ed9f91b42000000000000000000000012', // polygon MAI/DAI/USDC/USDT
+      '0xfeadd389a5c427952d8fdb8057d6c8ba1156cc5600020000000000000000001e', // polygon WBTC/renBTC
+      '0x9f19a375709baf0e8e35c2c5c65aca676c4c7191000200000000000000000022', // polygon PAR/PAR,
+      '0xf38cf113d2d4f60c36cbd95af2f48a9a0167045a00000000000000000000005b', // polygon,
+      '0x0d34e5dd4d8f043557145598e4e2dc286b35fd4f000000000000000000000068', // tusd polygon
+      '0x5028497af0c9a54ea8c6d42a054c0341b9fc616800020000000000000000007b', // dusd polygon
+      '0xaf5e0b5425de1f5a630a8cb5aa9d97b8141c908d000200000000000000000366' // polygon staked matic
+    ]
+  },
+  Investment: {
+    AllowList: ['']
+  },
+  Factories: {
+    '0xa5bf2ddf098bb0ef6d120c98217dd6b141c74ee0': 'oracleWeightedPool',
+    '0x8e9aa87e45e92bad84d5f8dd1bff34fb92637de9': 'weightedPool',
+    '0xc66ba2b6595d3613ccab350c886ace23866ede24': 'stablePool',
+    '0xdae7e32adc5d490a43ccba1f0c736033f2b4efca': 'stablePool', // Metastable
+    '0x751a0bc0e3f75b38e01cf25bfce7ff36de1c87de': 'liquidityBootstrappingPool', // LBP
+    '0x41b953164995c11c81da73d212ed8af25741b7ac': 'liquidityBootstrappingPool', // LBP (zero protocol fee)
+    '0x0f7bb7ce7b6ed9366f9b6b910adefe72dc538193': 'managedPool', // Polygon Managed
+    '0xc128a9954e6c874ea3d62ce62b468ba073093f25': 'boostedPool' // polygon stablephantom
+  }
+};
+
+const POOLS_ARBITRUM: Pools = {
+  IdsMap: {
+    staBAL:
+      '0x0000000000000000000000000000000000000000000000000000000000000000',
+    bbAaveUSD:
+      '0x0000000000000000000000000000000000000000000000000000000000000000'
+  },
+  Pagination: {
+    PerPage: 10
+  },
+  DelegateOwner: '0xba1ba1ba1ba1ba1ba1ba1ba1ba1ba1ba1ba1ba1b',
+  ZeroAddress: '0x0000000000000000000000000000000000000000',
+  DynamicFees: {
+    Gauntlet: ['']
+  },
+  BlockList: [''],
+  ExcludedPoolTypes: ['Element', 'AaveLinear', 'Linear'],
+  Stable: {
+    AllowList: [
+      '0x06df3b2bbb68adc8b0e302443692037ed9f91b42000000000000000000000063',
+      '0xfeadd389a5c427952d8fdb8057d6c8ba1156cc56000000000000000000000066',
+      '0x9f19a375709baf0e8e35c2c5c65aca676c4c719100000000000000000000006e',
+      '0x9be7de742865d021c0e8fb9d64311b2c040c1ec1000200000000000000000012', // arbitrum
+      '0x1533a3278f3f9141d5f820a184ea4b017fce2382000000000000000000000016', // arbitrum
+      '0x386b5d43ba8b97c43d4afb4cdae7877a1b295e8a000000000000000000000020' // tusd arbitrum
+    ]
+  },
+  Investment: {
+    AllowList: ['']
+  },
+  Factories: {
+    '0x7dfdef5f355096603419239ce743bfaf1120312b': 'weightedPool', // Arbitrum Weighted
+    '0xcf0a32bbef8f064969f21f7e02328fb577382018': 'weightedPool', // Arbitrum WeightedOracle
+    '0x2433477a10fc5d31b9513c638f19ee85caed53fd': 'stablePool', // Arbitrum Stable
+    '0xebfd5681977e38af65a7487dc70b8221d089ccad': 'stablePool', // Arbitrum MetaStable
+    '0x142b9666a0a3a30477b052962dda81547e7029ab': 'liquidityBootstrappingPool', // Arbitrum LBP (old)
+    '0x1802953277fd955f9a254b80aa0582f193cf1d77': 'liquidityBootstrappingPool', // Arbitrum LBP (new)
+    '0xacd615b3705b9c880e4e7293f1030b34e57b4c1c': 'managedPool', // arbitrum managed
+    '0xdae7e32adc5d490a43ccba1f0c736033f2b4efca': 'boostedPool' // arbitrum stablephantom
+  }
+};
+
 const POOLS_GENERIC: Pools = {
   IdsMap: {
     staBAL:
@@ -184,46 +269,7 @@ const POOLS_GENERIC: Pools = {
   DelegateOwner: '0xba1ba1ba1ba1ba1ba1ba1ba1ba1ba1ba1ba1ba1b',
   ZeroAddress: '0x0000000000000000000000000000000000000000',
   DynamicFees: {
-    Gauntlet: [
-      '0x8339e311265a025fd5792db800daa8eda4264e2c000200000000000000000029',
-      '0x45910faff3cbf990fdb204682e93055506682d1700020000000000000000000d',
-      '0xfa22ec1c02f121083bf04fbbcaad019f490d7a3000020000000000000000002a',
-      '0xa660ba113f9aabaeb4bcd28a4a1705f4997d5432000200000000000000000022',
-      '0xea8886a24b6e01fba88a9e98d794e8d1f29ed863000200000000000000000010',
-      '0x0b09dea16768f0799065c475be02919503cb2a3500020000000000000000001a',
-      '0xde148e6cc3f6047eed6e97238d341a2b8589e19e000200000000000000000017',
-      '0x5d563ca1e2daaae3402c36097b934630ab53702c000200000000000000000024',
-      '0x9f1f16b025f703ee985b58ced48daf93dad2f7ef00020000000000000000001e',
-      '0x3ebf48cd7586d7a4521ce59e53d9a907ebf1480f000200000000000000000028',
-      '0xefaa1604e82e1b3af8430b90192c1b9e8197e377000200000000000000000021',
-      '0x9c08c7a7a89cfd671c79eacdc6f07c1996277ed5000200000000000000000025',
-      '0xe99481dc77691d8e2456e5f3f61c1810adfc1503000200000000000000000018',
-      '0xec60a5fef79a92c741cb74fdd6bfc340c0279b01000200000000000000000015',
-      '0x231e687c9961d3a27e6e266ac5c433ce4f8253e4000200000000000000000023',
-      '0x80be0c303d8ad2a280878b50a39b1ee8e54dbd2200020000000000000000000f',
-      '0x5aa90c7362ea46b3cbfbd7f01ea5ca69c98fef1c000200000000000000000020',
-      '0xa6f548df93de924d73be7d25dc02554c6bd66db500020000000000000000000e',
-      '0x96646936b91d6b9d7d0c47c496afbf3d6ec7b6f8000200000000000000000019',
-      '0x186084ff790c65088ba694df11758fae4943ee9e000200000000000000000013',
-      '0x4626d81b3a1711beb79f4cecff2413886d461677000200000000000000000011',
-      '0x072f14b85add63488ddad88f855fda4a99d6ac9b000200000000000000000027',
-      '0xaac98ee71d4f8a156b6abaa6844cdb7789d086ce00020000000000000000001b',
-      '0x7eb878107af0440f9e776f999ce053d277c8aca8000200000000000000000012',
-      '0x16faf9f73748013155b7bc116a3008b57332d1e600020000000000000000001c',
-      '0x6ae82385f76e3742f89cb46343b169f6ee49de1b000200000000000000000016',
-      '0x8a92c3afabab59101b4e2426c82a7ddbb66b545000020000000000000000001f',
-      '0x5c6ee304399dbdb9c8ef030ab642b10820db8f56000200000000000000000014',
-      '0xf4c0dd9b82da36c07605df83c8a416f11724d88b000200000000000000000026',
-      '0x3e5fa9518ea95c3e533eb377c001702a9aacaa32000200000000000000000052',
-      '0xf8a0623ab66f985effc1c69d05f1af4badb01b0000020000000000000000001d',
-      '0x021c343c6180f03ce9e48fae3ff432309b9af19900020000000000000000000b',
-      '0x14462305d211c12a736986f4e8216e28c5ea7ab4000200000000000000000068',
-      '0xf5aaf7ee8c39b651cebf5f1f50c10631e78e0ef9000200000000000000000069',
-      '0xe2469f47ab58cf9cf59f9822e3c5de4950a41c49000200000000000000000089',
-      '0xfeadd389a5c427952d8fdb8057d6c8ba1156cc56000000000000000000000066',
-      '0x87165b659ba7746907a48763063efa3b323c2b0700020000000000000000002d',
-      '0x06df3b2bbb68adc8b0e302443692037ed9f91b42000000000000000000000063'
-    ]
+    Gauntlet: ['']
   },
   BlockList: [''],
   ExcludedPoolTypes: ['Element', 'AaveLinear', 'Linear'],
@@ -287,9 +333,12 @@ const POOLS_GENERIC: Pools = {
   }
 };
 
-export const POOLS: Pools =
-  networkId.value == Network.KOVAN
-    ? POOLS_KOVAN
-    : networkId.value == Network.MAINNET
-    ? POOLS_MAINNET
-    : POOLS_GENERIC;
+const POOLS_MAP = {
+  [Network.KOVAN]: POOLS_KOVAN,
+  [Network.MAINNET]: POOLS_MAINNET,
+  [Network.POLYGON]: POOLS_POLYGON,
+  [Network.ARBITRUM]: POOLS_ARBITRUM
+};
+export const POOLS: Pools = POOLS_MAP[networkId.value]
+  ? POOLS_MAP[networkId.value]
+  : POOLS_GENERIC;
