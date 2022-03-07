95ff7a82392f86df64a5f60e7158f4f3700a1fee
1saf <me@ansaf.net>
2022-03-07 17:12:20 +1100

    update: remove unused code

--- src/services/balancer/gauges/types.ts
+++ src/services/balancer/gauges/types.ts
@@ -1,17 +1,9 @@
 export type Address = string;
 export type QueryArgs = Record<string, any>;
 export type QueryAttrs = Record<string, any>;
-export type AdditionalQueryEntities = Record<
-  string,
-  {
-    __args: QueryArgs;
-    [key: string]: any;
-  }
->;
 export type QueryBuilder = (
   args?: QueryArgs,
-  attrs?: QueryAttrs,
-  additionalEntities?: AdditionalQueryEntities
+  attrs?: QueryAttrs
 ) => Record<string, any>;
 
 export interface SubgraphGauge {
