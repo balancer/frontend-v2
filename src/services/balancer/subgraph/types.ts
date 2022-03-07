555b064d470250a91423234f727f89738d6489d8
1saf <me@ansaf.net>
2022-03-07 16:56:20 +1100

    update: locales and types

--- src/services/balancer/subgraph/types.ts
+++ src/services/balancer/subgraph/types.ts
@@ -170,6 +170,13 @@ export interface PoolShare {
 
 export interface DecoratedPoolWithShares extends DecoratedPool {
   shares: string;
+  bpt: string;
+}
+
+export interface DecoratedPoolWithStakedShares extends DecoratedPoolWithShares {
+  shares: string;
+  bpt: string;
+  stakedPct: string;
 }
 
 export type PoolActivityType = 'Join' | 'Exit';
