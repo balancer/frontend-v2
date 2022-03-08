0be25223cb98064b125d0849dc03d2d1ef9fa1e5
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-08 15:52:02 +0000

    Update claim page to include tables

--- src/pages/claim.vue
+++ src/pages/claim.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
-import { computed, onBeforeMount, watch } from 'vue';
-import StatCard from '@/components/cards/StatCard/StatCard.vue';
+import { computed, watch } from 'vue';
 import { configService } from '@/services/config/config.service';
 import useGaugesQuery from '@/composables/queries/useGaugesQuery';
 import useGaugesDecorationQuery from '@/composables/queries/useGaugesDecorationQuery';
@@ -12,21 +11,28 @@ import { uniq } from 'lodash';
 import { TokenInfo, TokenInfoMap } from '@/types/TokenList';
 import { TOKENS } from '@/constants/tokens';
 import { networkId } from '@/composables/useNetwork';
+import { formatUnits } from 'ethers/lib/utils';
+import useNumbers from '@/composables/useNumbers';
+
+import { RewardRow } from '@/components/tables/TokenClaimsTable/TokenClaimsTable.vue';
+import StatCard from '@/components/cards/StatCard/StatCard.vue';
+import TokenClaimsTable from '@/components/tables/TokenClaimsTable/TokenClaimsTable.vue';
 
 /**
  * COMPOSABLES
  */
 const { injectTokens, getTokens, getToken } = useTokens();
+const { toFiat } = useNumbers();
 
+// Data fetching
 const { data: subgraphGauges } = useGaugesQuery();
-const gauges = useGaugesDecorationQuery(subgraphGauges);
-
-const gaugePoolAddresses = computed(() => {
-  if (!gauges.data.value) return [];
-  return gauges.data.value.map(gauge => gauge.poolId);
+const gaugesQuery = useGaugesDecorationQuery(subgraphGauges);
+const gauges = computed((): Gauge[] => gaugesQuery.data.value || []);
+const gaugePoolIds = computed((): string[] => {
+  return gauges.value.map(gauge => gauge.poolId);
 });
-
-const pools = useSimplePoolsQuery(gaugePoolAddresses);
+const poolsQuery = useSimplePoolsQuery(gaugePoolIds);
+const pools = computed((): Pool[] => poolsQuery.data.value || []);
 
 /**
  * CONSTANTS
@@ -61,20 +67,22 @@ const networkBtns = computed(() => {
 
 const gaugeQueryLoading = computed(
   (): boolean =>
-    gauges.isLoading.value || gauges.isIdle.value || !!gauges.error.value
+    gaugesQuery.isLoading.value ||
+    gaugesQuery.isIdle.value ||
+    !!gaugesQuery.error.value
 );
 
 const poolQueryLoading = computed(
   (): boolean =>
-    pools.isLoading.value || pools.isIdle.value || !!pools.error.value
+    poolsQuery.isLoading.value ||
+    poolsQuery.isIdle.value ||
+    !!poolsQuery.error.value
 );
 
 const rewardTokens = computed(
   (): TokenInfoMap => {
-    if (!gauges.data.value) return {};
-
     const addresses = uniq(
-      gauges.data.value.map(gauge => gauge.rewardTokens).flat()
+      gauges.value.map(gauge => gauge.rewardTokens).flat()
     );
     return getTokens(addresses);
   }
@@ -83,6 +91,23 @@ const rewardTokens = computed(
 const balToken = computed(
   (): TokenInfo => getToken(TOKENS.AddressMap[networkId.value.toString()].BAL)
 );
+
+const balRewardsData = computed((): RewardRow[] => {
+  // Using reduce to filter out gauges we don't have corresponding pools for
+  return gauges.value.reduce<RewardRow[]>((arr, gauge) => {
+    const amount = formatUnits(gauge.claimableTokens, balToken.value.decimals);
+    const pool = pools.value.find(pool => pool.id === gauge.poolId);
+
+    if (pool)
+      arr.push({
+        pool,
+        amount,
+        value: toFiat(amount, balToken.value.address)
+      });
+
+    return arr;
+  }, []);
+});
 
 /**
  * METHODS
@@ -97,15 +122,39 @@ async function injectPoolTokens(pools: Pool[]): Promise<void> {
   return await injectTokens(allPoolTokens);
 }
 
+function rewardDataFor(token: TokenInfo): RewardRow[] {
+  const relevantGauges = gauges.value.filter(gauge =>
+    gauge.rewardTokens.includes(token.address)
+  );
+
+  // Using reduce to filter out gauges we don't have corresponding pools for
+  return relevantGauges.reduce<RewardRow[]>((arr, gauge) => {
+    const amount = formatUnits(
+      gauge.claimableRewards[token.address],
+      token.decimals
+    );
+    const pool = pools.value.find(pool => pool.id === gauge.poolId);
+
+    if (pool)
+      arr.push({
+        pool,
+        amount,
+        value: toFiat(amount, balToken.value.address)
+      });
+
+    return arr;
+  }, []);
+}
+
 /**
  * WATCHERS
  */
-watch(gauges.data, async newGauges => {
+watch(gauges, async newGauges => {
   console.log('newGauges', newGauges);
   if (newGauges) await injectRewardTokens(newGauges);
 });
 
-watch(pools.data, async newPools => {
+watch(pools, async newPools => {
   console.log('pools', newPools);
   if (newPools) await injectPoolTokens(newPools);
 });
@@ -142,19 +191,27 @@ watch(pools.data, async newPools => {
 
       <div class="flex mt-4">
         <BalAsset :iconURI="balToken?.logoURI" />
-        <h3 class="text-xl ml-2">Balancer (BAL) earnings</h3>
+        <h3 class="text-xl ml-2 mb-2">Balancer (BAL) earnings</h3>
       </div>
+      <TokenClaimsTable
+        :rewardsData="balRewardsData"
+        :isLoading="poolQueryLoading"
+      />
 
-      <div v-for="token in rewardTokens" :key="token.address">
+      <div v-for="token in rewardTokens" :key="token.address" class="mt-8">
         <div class="flex mt-4">
           <BalAsset :iconURI="token?.logoURI" />
-          <h3 class="text-xl ml-2">
+          <h3 class="text-xl ml-2 mb-2">
             {{ token?.name }} ({{ token?.symbol }}) earnings
           </h3>
         </div>
+        <TokenClaimsTable
+          :rewardsData="rewardDataFor(token)"
+          :isLoading="poolQueryLoading"
+        />
       </div>
 
-      <h2 class="font-body font-bold text-2xl">
+      <h2 class="font-body font-bold text-2xl mt-8">
         {{ $t('pages.claim.titles.incentivesOnOtherNetworks') }}
       </h2>
       <div class="flex mt-4">
