4506a6b850064a6a42ec642975ba13d195f6dc44
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-08 08:53:14 +0000

    Add legacy claims to claim page (#1439)
    * Duplicate nav claim popover and adjust for claim page
    
    * Add locales
    
    * Add LegacyClaims component to claims page

--- src/pages/claim.vue
+++ src/pages/claim.vue
@@ -2,6 +2,7 @@
 import { computed } from 'vue';
 import StatCard from '@/components/cards/StatCard/StatCard.vue';
 import { configService } from '@/services/config/config.service';
+import LegacyClaims from '@/components/contextual/pages/claim/LegacyClaims.vue';
 
 const networks = [
   {
@@ -30,7 +31,7 @@ const networkBtns = computed(() => {
 </script>
 
 <template>
-  <div class="lg:container lg:mx-auto py-12">
+  <div class="lg:container lg:mx-auto py-12 px-2 lg:px-0">
     <div class="grid gap-24 grid-cols-2 grid-rows-1">
       <div class="">
         <h1 class="font-body font-bold text-4xl">
@@ -52,13 +53,13 @@ const networkBtns = computed(() => {
       </div>
     </div>
   </div>
-  <div class="bg-gray-50">
+  <div class="px-2 lg:px-0">
     <div class="lg:container lg:mx-auto py-12">
       <h2 class="font-body font-bold text-2xl">
         {{ configService.network.chainName }} {{ $t('liquidityIncentives') }}
       </h2>
 
-      <h2 class="font-body font-bold text-2xl">
+      <h2 class="font-body font-bold text-2xl mt-8">
         {{ $t('pages.claim.titles.incentivesOnOtherNetworks') }}
       </h2>
       <div class="flex mt-4">
@@ -78,6 +79,11 @@ const networkBtns = computed(() => {
           {{ $t('pages.claim.btns.claimOn') }} {{ network.name }}
         </BalBtn>
       </div>
+
+      <h2 class="font-body font-bold text-2xl mt-8">
+        {{ $t('pages.claim.titles.legacyIncentives') }}
+      </h2>
+      <LegacyClaims />
     </div>
   </div>
 </template>
