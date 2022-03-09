b20cefd98006e818652930915bd31882a8fbdbba
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-09 17:00:31 +0000

    Add claiming states and refetch gauges on success

--- src/components/btns/ClaimBalBtn/ClaimBalBtn.vue
+++ src/components/btns/ClaimBalBtn/ClaimBalBtn.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed, reactive } from 'vue';
 import { balancerMinter } from '@/services/balancer/contracts/contracts/balancer-minter';
 import { Gauge } from '@/services/balancer/gauges/types';
 import { getAddress } from '@ethersproject/address';
 import useTransactions from '@/composables/useTransactions';
 import useNumbers, { FNumFormats } from '@/composables/useNumbers';
+import useEthers from '@/composables/useEthers';
+import useGaugesQuery from '@/composables/queries/useGaugesQuery';
+import useGaugesDecorationQuery from '@/composables/queries/useGaugesDecorationQuery';
 
 /**
  * TYPES
@@ -25,6 +28,16 @@ const props = defineProps<Props>();
  */
 const { addTransaction } = useTransactions();
 const { fNum2 } = useNumbers();
+const { txListener } = useEthers();
+const { data: subgraphGauges } = useGaugesQuery();
+const gaugesQuery = useGaugesDecorationQuery(subgraphGauges);
+
+/**
+ * STATE
+ */
+const state = reactive({
+  confirming: false
+});
 
 /**
  * COMPUTED
@@ -46,15 +59,36 @@ async function claimTx() {
 
 async function claim() {
   const tx = await claimTx();
+  state.confirming = true;
+
   addTransaction({
     id: tx.hash,
     type: 'tx',
     action: 'claim',
     summary: `Claim ${fNum2(props.amount, FNumFormats.token)} BAL`
   });
+
+  await txListener(tx, {
+    onTxConfirmed: async () => {
+      await gaugesQuery.refetch.value();
+      state.confirming = false;
+    },
+    onTxFailed: () => {
+      console.error('Claim failed');
+      state.confirming = false;
+    }
+  });
 }
 </script>
 
 <template>
-  <BalBtn :label="label" color="gradient" size="sm" @click.stop="claim" />
+  <BalBtn
+    :label="label"
+    loadingLabel="Claiming..."
+    color="gradient"
+    size="sm"
+    :loading="state.confirming"
+    :disabled="state.confirming"
+    @click.stop="claim"
+  />
 </template>
--- src/components/btns/ClaimRewardsBtn/ClaimRewardsBtn.vue
+++ src/components/btns/ClaimRewardsBtn/ClaimRewardsBtn.vue
@@ -1,13 +1,20 @@
 <script lang="ts" setup>
+import useGaugesDecorationQuery from '@/composables/queries/useGaugesDecorationQuery';
+import useGaugesQuery from '@/composables/queries/useGaugesQuery';
+import useEthers from '@/composables/useEthers';
+import useNumbers, { FNumFormats } from '@/composables/useNumbers';
+import useTransactions from '@/composables/useTransactions';
 import { LiquidityGauge } from '@/services/balancer/contracts/contracts/liquidity-gauge';
 import { Gauge } from '@/services/balancer/gauges/types';
 import { getAddress } from '@ethersproject/address';
+import { reactive } from 'vue';
 
 /**
  * TYPES
  */
 type Props = {
   gauge: Gauge;
+  value: string;
 };
 
 /**
@@ -16,8 +23,20 @@ type Props = {
 const props = defineProps<Props>();
 
 /**
+ * COMPOSABLES
+ */
+const { addTransaction } = useTransactions();
+const { fNum2 } = useNumbers();
+const { txListener } = useEthers();
+const { data: subgraphGauges } = useGaugesQuery();
+const gaugesQuery = useGaugesDecorationQuery(subgraphGauges);
+
+/**
  * STATE
  */
+const state = reactive({
+  confirming: false
+});
 const gaugeAddress = getAddress(props.gauge.id);
 const liquidityGaugeContract = new LiquidityGauge(gaugeAddress);
 
@@ -25,10 +44,37 @@ const liquidityGaugeContract = new LiquidityGauge(gaugeAddress);
  * METHODS
  */
 async function claim() {
-  await liquidityGaugeContract.claimRewards();
+  const tx = await liquidityGaugeContract.claimRewards();
+  state.confirming = true;
+
+  addTransaction({
+    id: tx.hash,
+    type: 'tx',
+    action: 'claim',
+    summary: `Claim ${fNum2(props.value, FNumFormats.fiat)}`
+  });
+
+  await txListener(tx, {
+    onTxConfirmed: async () => {
+      await gaugesQuery.refetch.value();
+      state.confirming = false;
+    },
+    onTxFailed: () => {
+      console.error('Claim failed');
+      state.confirming = false;
+    }
+  });
 }
 </script>
 
 <template>
-  <BalBtn label="Claim all" color="gradient" size="sm" @click.stop="claim" />
+  <BalBtn
+    label="Claim all"
+    loadingLabel="Claiming..."
+    color="gradient"
+    size="sm"
+    :loading="state.confirming"
+    :disabled="state.confirming"
+    @click.stop="claim"
+  />
 </template>
