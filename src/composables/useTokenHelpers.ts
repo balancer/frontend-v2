349f2395083959c7806fec6ed11f647f33ef9cf1
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-09 15:43:17 +0000

    Add tokens helper composable

--- src/composables/useTokenHelpers.ts
+++ src/composables/useTokenHelpers.ts
@@ -1,14 +1,23 @@
 import { TOKENS } from '@/constants/tokens';
+import { TokenInfo } from '@/types/TokenList';
 import { computed } from 'vue';
 import { networkId } from './useNetwork';
+import useTokens from './useTokens';
 
 export function useTokenHelpers() {
+  /**
+   * COMPOSABLES
+   */
+  const { getToken } = useTokens();
+
   /**
    * COMPUTED
    */
   const balAddress = computed(
     (): string => TOKENS.AddressMap[networkId.value]?.BAL
   );
+
+  const balToken = computed((): TokenInfo => getToken(balAddress.value));
 
   /**
    * METHODS
@@ -20,6 +29,7 @@ export function useTokenHelpers() {
   return {
     // computed
     balAddress,
+    balToken,
     // methods
     isBalAddress
   };
