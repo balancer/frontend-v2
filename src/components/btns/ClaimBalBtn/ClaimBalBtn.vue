68c89616ea52ea02718fe1d6492311bf68e45332
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-11 14:28:28 +0000

    Refactor ClaimBalBtn component

--- src/components/btns/ClaimBalBtn/ClaimBalBtn.vue
+++ src/components/btns/ClaimBalBtn/ClaimBalBtn.vue
@@ -1,14 +1,13 @@
 <script lang="ts" setup>
-import { computed, reactive } from 'vue';
+import { computed } from 'vue';
 import { balancerMinter } from '@/services/balancer/contracts/contracts/balancer-minter';
 import { Gauge } from '@/services/balancer/gauges/types';
 import { getAddress } from '@ethersproject/address';
-import useTransactions from '@/composables/useTransactions';
 import useNumbers, { FNumFormats } from '@/composables/useNumbers';
-import useEthers from '@/composables/useEthers';
 import useGaugesQuery from '@/composables/queries/useGaugesQuery';
 import useGaugesDecorationQuery from '@/composables/queries/useGaugesDecorationQuery';
 import { useI18n } from 'vue-i18n';
+import TxActionBtn from '@/components/btns/TxActionBtn/TxActionBtn.vue';
 
 /**
  * TYPES
@@ -28,18 +27,9 @@ const props = defineProps<Props>();
  * COMPOSABLES
  */
 const { t } = useI18n();
-const { addTransaction } = useTransactions();
 const { fNum2 } = useNumbers();
-const { txListener } = useEthers();
 const { data: subgraphGauges } = useGaugesQuery();
 const gaugesQuery = useGaugesDecorationQuery(subgraphGauges);
-
-/**
- * STATE
- */
-const state = reactive({
-  confirming: false
-});
 
 /**
  * COMPUTED
@@ -58,39 +48,17 @@ async function claimTx() {
     return await balancerMinter.mintMany(gaugeAddresses.value);
   }
 }
-
-async function claim() {
-  const tx = await claimTx();
-  state.confirming = true;
-
-  addTransaction({
-    id: tx.hash,
-    type: 'tx',
-    action: 'claim',
-    summary: `${t('claim')} ${fNum2(props.amount, FNumFormats.token)} BAL`
-  });
-
-  await txListener(tx, {
-    onTxConfirmed: async () => {
-      await gaugesQuery.refetch.value();
-      state.confirming = false;
-    },
-    onTxFailed: () => {
-      console.error('Claim failed');
-      state.confirming = false;
-    }
-  });
-}
 </script>
 
 <template>
-  <BalBtn
+  <TxActionBtn
     :label="label"
-    :loadingLabel="$t('claiming')"
     color="gradient"
     size="sm"
-    :loading="state.confirming"
-    :disabled="state.confirming"
-    @click.stop="claim"
+    :actionFn="claimTx"
+    :onConfirmFn="gaugesQuery.refetch.value"
+    action="claim"
+    :summary="`${t('claim')} ${fNum2(props.amount, FNumFormats.token)} BAL`"
+    :confirmingLabel="$t('claiming')"
   />
 </template>
