1905d4b3ae71b69d2e4b30d9afb8766d0b15fe3e
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-11 14:35:54 +0000

    Refactor ClaimRewardsBtn to use TxActionBtn

--- src/components/btns/ClaimRewardsBtn/ClaimRewardsBtn.vue
+++ src/components/btns/ClaimRewardsBtn/ClaimRewardsBtn.vue
@@ -1,14 +1,12 @@
 <script lang="ts" setup>
 import useGaugesDecorationQuery from '@/composables/queries/useGaugesDecorationQuery';
 import useGaugesQuery from '@/composables/queries/useGaugesQuery';
-import useEthers from '@/composables/useEthers';
 import useNumbers, { FNumFormats } from '@/composables/useNumbers';
-import useTransactions from '@/composables/useTransactions';
 import { LiquidityGauge } from '@/services/balancer/contracts/contracts/liquidity-gauge';
 import { Gauge } from '@/services/balancer/gauges/types';
 import { getAddress } from '@ethersproject/address';
-import { reactive } from 'vue';
 import { useI18n } from 'vue-i18n';
+import TxActionBtn from '../TxActionBtn/TxActionBtn.vue';
 
 /**
  * TYPES
@@ -27,56 +25,33 @@ const props = defineProps<Props>();
  * COMPOSABLES
  */
 const { t } = useI18n();
-const { addTransaction } = useTransactions();
 const { fNum2 } = useNumbers();
-const { txListener } = useEthers();
 const { data: subgraphGauges } = useGaugesQuery();
 const gaugesQuery = useGaugesDecorationQuery(subgraphGauges);
 
 /**
  * STATE
  */
-const state = reactive({
-  confirming: false
-});
 const gaugeAddress = getAddress(props.gauge.id);
 const liquidityGaugeContract = new LiquidityGauge(gaugeAddress);
 
 /**
  * METHODS
  */
-async function claim() {
-  const tx = await liquidityGaugeContract.claimRewards();
-  state.confirming = true;
-
-  addTransaction({
-    id: tx.hash,
-    type: 'tx',
-    action: 'claim',
-    summary: `${t('claim')} ${fNum2(props.fiatValue, FNumFormats.fiat)}`
-  });
-
-  await txListener(tx, {
-    onTxConfirmed: async () => {
-      await gaugesQuery.refetch.value();
-      state.confirming = false;
-    },
-    onTxFailed: () => {
-      console.error('Claim failed');
-      state.confirming = false;
-    }
-  });
+function claimTx() {
+  return liquidityGaugeContract.claimRewards();
 }
 </script>
 
 <template>
-  <BalBtn
+  <TxActionBtn
     :label="$t('claimAll')"
-    :loadingLabel="$t('claiming')"
     color="gradient"
     size="sm"
-    :loading="state.confirming"
-    :disabled="state.confirming"
-    @click.stop="claim"
+    :actionFn="claimTx"
+    :onConfirmFn="gaugesQuery.refetch.value"
+    action="claim"
+    :summary="`${t('claim')} ${fNum2(props.fiatValue, FNumFormats.fiat)}`"
+    :confirmingLabel="$t('claiming')"
   />
 </template>
