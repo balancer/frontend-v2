082c7ac2ad0252177b242cf9d5110cc8d7c1ca80
Tim Robinson <tim@timjrobinson.com>
2022-03-12 14:31:15 +1000

    Add tracking of vote state and errors, and update UI as required

--- src/components/vebal/GaugeVote.vue
+++ src/components/vebal/GaugeVote.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, reactive } from 'vue';
 import { useI18n } from 'vue-i18n';
-
+import useWeb3 from '@/services/web3/useWeb3';
 import useNumbers, { FNumFormats } from '@/composables/useNumbers';
-
-import { DecoratedPoolWithGaugeShares } from '@/services/balancer/subgraph/types';
+import useTransactions from '@/composables/useTransactions';
+import useEthers from '@/composables/useEthers';
+import { dateTimeLabelFor } from '@/composables/useTime';
+import {
+  TransactionReceipt,
+  TransactionResponse
+} from '@ethersproject/abstract-provider';
+import { PoolWithGauge } from '@/services/balancer/subgraph/types';
 import { scale, bnum } from '@/lib/utils';
 import BalForm from '../_global/BalForm/BalForm.vue';
 import BalTextInput from '../_global/BalTextInput/BalTextInput.vue';
 import { gaugeControllerService } from '@/services/contracts/gauge-controller.service';
 import { BigNumber } from '@ethersproject/bignumber';
+import { TransactionActionState } from '@/types/transactions';
+import { WalletError } from '@/types';
 
 /**
  * TYPES
  */
 type Props = {
-  pool: DecoratedPoolWithGaugeShares;
+  pool: PoolWithGauge;
   unallocatedVoteWeight: number;
 };
 
@@ -27,14 +35,23 @@ const props = defineProps<Props>();
 /**
  * COMPOSABLES
  */
+const { explorerLinks } = useWeb3();
 const { fNum2 } = useNumbers();
 const { t } = useI18n();
+const { addTransaction } = useTransactions();
+const { txListener, getTxConfirmedAt } = useEthers();
 
 /**
  * STATE
  */
 
 const voteWeight = ref<string>('');
+const voteState = reactive<TransactionActionState>({
+  init: false,
+  confirming: false,
+  confirmed: false,
+  confirmedAt: ''
+});
 
 /**
  * COMPUTED
@@ -42,20 +59,80 @@ const voteWeight = ref<string>('');
 const voteDisabled = computed(() => false); // Make disabled when not a valid number
 const currentWeight = computed(() => props.pool.gauge.userVotes);
 const voteButtonText = computed(() =>
-  currentWeight.value > 0
+  parseFloat(currentWeight.value) > 0
     ? t('veBAL.liquidityMining.popover.button.edit')
     : t('veBAL.liquidityMining.popover.button.vote')
+);
+
+const explorerLink = computed((): string =>
+  voteState.receipt
+    ? explorerLinks.txLink(voteState.receipt.transactionHash)
+    : ''
+);
+
+const transactionInProgress = computed(
+  (): boolean => voteState.init || voteState.confirming || voteState.confirmed
 );
 
 /**
  * METHODS
  */
-function submitVote() {
+async function submitVote() {
   const totalVoteShares = scale(voteWeight.value, 2).toString();
-  gaugeControllerService.voteForGaugeWeights(
-    props.pool.gauge.address,
-    BigNumber.from(totalVoteShares)
-  );
+  try {
+    voteState.init = true;
+    voteState.error = null;
+    const tx = await gaugeControllerService.voteForGaugeWeights(
+      props.pool.gauge.address,
+      BigNumber.from(totalVoteShares)
+    );
+    voteState.init = false;
+    voteState.confirming = true;
+    handleTransaction(tx);
+  } catch (e) {
+    console.error(e);
+    const error = e as WalletError;
+    voteState.init = false;
+    voteState.confirming = false;
+    voteState.error = {
+      title: 'Vote Failed',
+      description: error.message
+    };
+  }
+}
+
+async function handleTransaction(tx) {
+  addTransaction({
+    id: tx.hash,
+    type: 'tx',
+    action: 'voteForGauge',
+    summary: t('veBAL.liquidityMining.popover.voteForGauge', [
+      fNum2(voteWeight.value, FNumFormats.percent),
+      '50% DAI, 50% WETH'
+    ]),
+    details: {
+      voteWeight: voteWeight.value
+    }
+  });
+
+  txListener(tx, {
+    onTxConfirmed: async (receipt: TransactionReceipt) => {
+      voteState.receipt = receipt;
+
+      const confirmedAt = await getTxConfirmedAt(receipt);
+      voteState.confirmedAt = dateTimeLabelFor(confirmedAt);
+      voteState.confirmed = true;
+      voteState.confirming = false;
+    },
+    onTxFailed: () => {
+      console.error('Vote failed');
+      voteState.error = {
+        title: 'Vote Failed',
+        description: 'Vote failed for an unknown reason'
+      };
+      voteState.confirming = false;
+    }
+  });
 }
 </script>
 
@@ -94,11 +171,25 @@ function submitVote() {
               )
             }}
           </div>
+          <BalAlert
+            v-if="voteState.error"
+            type="error"
+            :title="voteState.error.title"
+            :description="voteState.error.description"
+            block
+            class="mb-4"
+          />
           <BalBtn
             color="gradient"
             class="mt-6"
             block
             :disabled="voteDisabled"
+            :loading="voteState.init || voteState.confirming"
+            :loading-label="
+              voteState.init
+                ? $t('veBAL.liquidityMining.popover.actions.vote.loadingLabel')
+                : $t('veBAL.liquidityMining.popover.actions.vote.confirming')
+            "
             @click.prevent="submitVote"
             >{{ voteButtonText }}</BalBtn
           >
