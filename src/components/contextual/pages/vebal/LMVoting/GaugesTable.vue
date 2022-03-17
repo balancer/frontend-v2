1e13c98aeb39d63521a6da2bf680c7ced93f13c8
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-17 09:50:17 +0000

    Fix chain icon bg on dark mode

--- src/components/contextual/pages/vebal/LMVoting/GaugesTable.vue
+++ src/components/contextual/pages/vebal/LMVoting/GaugesTable.vue
@@ -211,18 +211,18 @@ function redirectToPool(gauge: VotingGaugeWithVotes) {
           <NetworkIcon />
         </div>
       </template>
-      <template #iconColumnHeader>
-        <div class="flex items-center">
-          <CompositionIcon />
-        </div>
-      </template>
       <template #networkColumnCell="{ network }">
         <div v-if="!isLoading" class="px-6 py-4">
           <div
-            class="w-8 h-8 rounded shadow-sm bg-gray-50 flex items-center justify-center"
+            class="w-8 h-8 rounded shadow-sm bg-gray-50 dark:bg-gray-800 flex items-center justify-center"
           >
             <img :src="networkSrc(network)" :alt="network" class="w-6 h-6" />
           </div>
+        </div>
+      </template>
+      <template #iconColumnHeader>
+        <div class="flex items-center">
+          <CompositionIcon />
         </div>
       </template>
       <template #iconColumnCell="gauge">
