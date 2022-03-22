110d309dcf0f9c436ade76029e0afbeac71cbaaa
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-22 20:15:37 +0000

    Up navbar z-index

--- src/components/navs/AppNav/AppNav.vue
+++ src/components/navs/AppNav/AppNav.vue
@@ -69,7 +69,7 @@ onUnmounted(() => {
 
 <style scoped>
 #app-nav {
-  @apply w-full z-20;
+  @apply w-full z-30;
   @apply bg-white dark:bg-gray-900;
   @apply border-b border-transparent;
   transition: all 0.2s ease-in-out;
