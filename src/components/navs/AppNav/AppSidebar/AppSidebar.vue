c19c8672684ca57b1ade676c9cb4cd95c7c059f2
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-22 11:09:12 +0000

    Fix sidebar animations

--- src/components/navs/AppNav/AppSidebar/AppSidebar.vue
+++ src/components/navs/AppNav/AppSidebar/AppSidebar.vue
@@ -1,52 +1,32 @@
 <script lang="ts" setup>
-import AppLogo from '@/components/images/AppLogo.vue';
-import { useSidebar } from '@/composables/useSidebar';
-import { sleep } from '@/lib/utils';
 import { ref } from 'vue';
+import { useSidebar } from '@/composables/useSidebar';
+import SidebarContent from './SidebarContent.vue';
 
+/**
+ * STATE
+ */
+const showSidebar = ref(false);
+
+/**
+ * COMPOSABLES
+ */
 const { setSidebarOpen } = useSidebar();
-
-const show = ref(true);
-
-async function handlClose() {
-  show.value = false;
-  await sleep(300);
-  setSidebarOpen(false);
-}
 </script>
 
 <template>
-  <Transition name="overlay" appear>
-    <div v-if="show" class="sidebar-overlay">
-      <div class="app-sidebar">
-        <div
-          class="h-20 px-4 border-b border-gray-800 flex flex-col justify-center"
-        >
-          <AppLogo forceDark />
+  <Transition name="overlay" @afterEnter="showSidebar = true" appear>
+    <div class="sidebar-overlay" @click="showSidebar = false">
+      <Transition name="sidebar" @afterLeave="setSidebarOpen(false)">
+        <div v-if="showSidebar" class="app-sidebar" @click.stop>
+          <SidebarContent />
         </div>
-
-        <div class="grid grid-col-1 text-lg mt-8">
-          <routerLink to="/" class="side-bar-link">Invest</routerLink>
-          <routerLink to="/trade" class="side-bar-link">Trade</routerLink>
-          <routerLink to="/vebal" class="side-bar-link">Vest+Vote</routerLink>
-          <routerLink to="/claim" class="side-bar-link">Claim</routerLink>
-          <div class="side-bar-link">Support</div>
-          <BalLink
-            href="https://balancer.fi/build"
-            class="side-bar-link flex items-center"
-            external
-            noStyle
-          >
-            Build
-            <BalIcon name="arrow-up-right" class="ml-2 text-gray-500" />
-          </BalLink>
-        </div>
-      </div>
+      </Transition>
       <BalIcon
         name="x"
         size="lg"
         class="fixed top-8 right-8 text-white cursor-pointer"
-        @click="handlClose"
+        @click="showSidebar = false"
       />
     </div>
   </Transition>
@@ -55,24 +35,31 @@ async function handlClose() {
 <style scoped>
 .sidebar-overlay {
   z-index: 999999999;
-  @apply fixed top-0 left-0 h-screen w-full bg-black bg-opacity-80;
+  @apply fixed top-0 left-0 h-screen w-full bg-black bg-opacity-80 cursor-pointer;
 }
 
 .app-sidebar {
-  @apply text-white shadow-xl h-full w-3/4 max-w-sm bg-gray-900;
-}
-
-.side-bar-link {
-  @apply transition duration-300 p-4 hover:bg-gray-850 cursor-pointer;
+  @apply text-white shadow-xl h-full w-3/4 max-w-sm bg-gray-900 cursor-default;
 }
 
 .overlay-enter-active,
 .overlay-leave-active {
-  transition: all 0.15s ease-in-out;
+  transition: all 0.1s ease-in-out;
 }
 
 .overlay-enter-from,
 .overlay-leave-to {
+  opacity: 0;
+}
+
+.sidebar-enter-active,
+.sidebar-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+
+.sidebar-enter-from,
+.sidebar-leave-to {
+  transform: translateX(-100%);
   opacity: 0;
 }
 </style>
---
+++ src/components/navs/AppNav/AppSidebar/SidebarContent.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import AppLogo from '@/components/images/AppLogo.vue';
+</script>
+
+<template>
+  <div>
+    <div
+      class="h-20 px-4 border-b border-gray-800 flex flex-col justify-center"
+    >
+      <AppLogo forceDark />
+    </div>
+
+    <div class="grid grid-col-1 text-lg mt-8">
+      <routerLink to="/" class="side-bar-link">Invest</routerLink>
+      <routerLink to="/trade" class="side-bar-link">Trade</routerLink>
+      <routerLink to="/vebal" class="side-bar-link">Vest+Vote</routerLink>
+      <routerLink to="/claim" class="side-bar-link">Claim</routerLink>
+      <div class="side-bar-link">Support</div>
+      <BalLink
+        href="https://balancer.fi/build"
+        class="side-bar-link flex items-center"
+        external
+        noStyle
+      >
+        Build
+        <BalIcon name="arrow-up-right" class="ml-2 text-gray-500" />
+      </BalLink>
+    </div>
+
+    <div class="grid grid-col-1 text-sm mt-8">
+      <span class="text-gray-500 px-4 font-medium">Ecosystem</span>
+      <BalLink
+        href="https://medium.com/balancer-protocol"
+        class="side-bar-link flex items-center"
+        external
+        noStyle
+      >
+        Blog
+        <BalIcon name="arrow-up-right" class="ml-2 text-gray-500" />
+      </BalLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.side-bar-link {
+  @apply transition duration-300 p-4 py-2 hover:bg-gray-850 cursor-pointer;
+}
+</style>
