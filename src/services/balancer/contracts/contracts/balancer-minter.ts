600d5e7a4c4d35b6e8591d9098a721e1d5ac627b
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-24 21:32:51 +0000

    Hotfix - console error (#1546)
    * Fix console error
    
    Contract instance is being instantiated even when the contract address doesn't exist. It's actually not needed at all so removing.
    
    * 1.45.4

--- src/services/balancer/contracts/contracts/balancer-minter.ts
+++ src/services/balancer/contracts/contracts/balancer-minter.ts
@@ -1,21 +1,15 @@
 import BalancerMinterAbi from '@/lib/abi/BalancerMinter.json';
 import { configService } from '@/services/config/config.service';
-import { rpcProviderService } from '@/services/rpc-provider/rpc-provider.service';
 import { web3Service } from '@/services/web3/web3.service';
-import { Contract } from '@ethersproject/contracts';
 
 export class BalancerMinter {
-  instance: Contract;
-
   constructor(
     private readonly abi = BalancerMinterAbi,
-    private readonly provider = rpcProviderService.jsonProvider,
     private readonly config = configService,
     private readonly web3 = web3Service,
     public readonly address = config.network.addresses.balancerMinter
   ) {
     if (!this.address) console.error('BalancerMinter address not set');
-    this.instance = new Contract(this.address, this.abi, this.provider);
   }
 
   /**
