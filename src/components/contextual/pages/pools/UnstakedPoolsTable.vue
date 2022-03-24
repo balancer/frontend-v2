4ed9e4606d8553e6ae73c24e895290cbbe181587
Tim Robinson <tim@timjrobinson.com>
2022-03-24 17:34:51 +1000

    Add handleModalClose function that sets showStakeModal to false

--- src/components/contextual/pages/pools/UnstakedPoolsTable.vue
+++ src/components/contextual/pages/pools/UnstakedPoolsTable.vue
@@ -107,6 +107,10 @@ function calculateFiatValueOfShares(
     .times((stakedBalance || '0').toString())
     .toString();
 }
+
+function handleModalClose() {
+  showStakeModal.value = false;
+}
 </script>
 
 <template>
