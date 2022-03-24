b71eb960f94fbbea98af51b7c49b381bf874d711
1saf <me@ansaf.net>
2022-03-25 03:13:11 +1100

    update: remove junk from staked pools table

--- src/components/contextual/pages/pools/StakedPoolsTable.vue
+++ src/components/contextual/pages/pools/StakedPoolsTable.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from 'vue';
+import { ref } from 'vue';
 
-import useUserPoolsQuery from '@/composables/queries/useUserPoolsQuery';
 import useStaking from '@/composables/staking/useStaking';
 
-import {
-  DecoratedPool,
-  DecoratedPoolWithShares,
-  FullPool
-} from '@/services/balancer/subgraph/types';
-import { bnum } from '@/lib/utils';
+import { FullPool } from '@/services/balancer/subgraph/types';
 
 import PoolsTable from '@/components/tables/PoolsTable/PoolsTable.vue';
 import StakePreviewModal from '../../stake/StakePreviewModal.vue';
 import AnimatePresence from '@/components/animate/AnimatePresence.vue';
-import useGraphQuery, { subgraphs } from '@/composables/queries/useGraphQuery';
-import { UserGuageSharesResponse } from './types';
-import useWeb3 from '@/services/web3/useWeb3';
 
 /** STATE */
 const showStakeModal = ref(false);
@@ -28,39 +19,6 @@ const {
   isLoading: isLoadingStakingData,
   setPoolAddress
 } = useStaking();
-const { account } = useWeb3();
-
-/** COMPUTED */
-
-// a fast query to let us determine if we should show
-// the staked pools table and while loading, the height
-// of the skeleton
-const {
-  data: gaugeShares,
-  isIdle: isGaugeSharesIdle,
-  isLoading: isLoadingGaugeShares
-} = useGraphQuery<UserGuageSharesResponse>(
-  subgraphs.gauge,
-  ['staking', 'data', { account }],
-  () => ({
-    gaugeShares: {
-      __args: {
-        where: { user: account.value.toLowerCase(), balance_gt: '0' }
-      },
-      balance: 1,
-      gauge: {
-        poolId: 1
-      }
-    }
-  }),
-  reactive({
-    refetchOnWindowFocus: false
-  })
-);
-
-const numStakedPools = computed(
-  () => (gaugeShares.value?.gaugeShares || []).length
-);
 
 /** METHODS */
 function handleStake(pool: FullPool) {
@@ -75,11 +33,7 @@ function handleModalClose() {
 </script>
 
 <template>
-  <AnimatePresence
-    :isVisible="
-      !isLoadingStakingData && (numStakedPools > 0 || stakedPools.length > 0)
-    "
-  >
+  <AnimatePresence :isVisible="!isLoadingStakingData && stakedPools.length > 0">
     <div class="mt-8">
       <BalStack vertical spacing="sm">
         <h5>{{ $t('staking.stakedPools') }}</h5>
@@ -101,21 +55,9 @@ function handleModalClose() {
       action="stake"
     />
   </AnimatePresence>
-  <AnimatePresence
-    :isVisible="
-      (isLoadingStakingData && numStakedPools > 0) ||
-        isLoadingGaugeShares ||
-        isGaugeSharesIdle
-    "
-  >
+  <AnimatePresence :isVisible="isLoadingStakingData">
     <div class="mt-8">
-      <BalLoadingBlock class="staked-pools-table-loading" />
+      <BalLoadingBlock class="h-32" />
     </div>
   </AnimatePresence>
 </template>
-
-<style scoped>
-.staked-pools-table-loading {
-  height: calc(4rem + (4.375rem * v-bind(numStakedPools)));
-}
-</style>
