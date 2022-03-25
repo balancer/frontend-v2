9ce6337a1ace6a6ba292c1ccdca395b497b2a50d
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-25 14:36:54 +0000

    Add useLock composable

---
+++ src/composables/useLock.ts
@@ -0,0 +1,76 @@
+import { bnum } from '@/lib/utils';
+import { FullPool } from '@/services/balancer/subgraph/types';
+import useWeb3 from '@/services/web3/useWeb3';
+import { TokenInfo } from '@/types/TokenList';
+import BigNumber from 'bignumber.js';
+import { computed } from 'vue';
+import usePoolQuery from './queries/usePoolQuery';
+import useVeBalLockInfoQuery from './queries/useVeBalLockInfoQuery';
+import useTokens from './useTokens';
+import useVeBal from './useVeBAL';
+
+export function useLock() {
+  /**
+   * COMPOSABLES
+   */
+  const { lockablePoolId } = useVeBal();
+  const { tokens } = useTokens();
+  const { isWalletReady } = useWeb3();
+
+  /**
+   * QUERIES
+   */
+  const lockPoolQuery = usePoolQuery(
+    lockablePoolId.value as string,
+    isWalletReady
+  );
+  const lockQuery = useVeBalLockInfoQuery();
+
+  /**
+   * COMPUTED
+   */
+  const isLoadingLockPool = computed(
+    (): boolean => lockPoolQuery.isLoading.value || lockPoolQuery.isIdle.value
+  );
+
+  const isLoadingLockInfo = computed(
+    (): boolean => lockQuery.isLoading.value || lockQuery.isIdle.value
+  );
+
+  const isLoadingLock = computed(
+    (): boolean => isLoadingLockPool.value || isLoadingLockInfo.value
+  );
+
+  const lockPool = computed<FullPool | undefined>(
+    () => lockPoolQuery.data.value
+  );
+
+  const lockPoolToken = computed((): TokenInfo | null =>
+    lockPool.value != null ? tokens.value[lockPool.value.address] : null
+  );
+
+  const lock = computed(() => lockQuery.data.value);
+
+  const poolShares = computed(
+    (): BigNumber =>
+      lockPool.value
+        ? bnum(lockPool.value.totalLiquidity).div(lockPool.value.totalShares)
+        : bnum(0)
+  );
+
+  const lockFiatValue = computed((): string =>
+    lock.value && lock.value.hasExistingLock
+      ? poolShares.value.times(lock.value.lockedAmount).toString()
+      : '0'
+  );
+
+  return {
+    isLoadingLockPool,
+    isLoadingLockInfo,
+    isLoadingLock,
+    lockPoolToken,
+    lockPool,
+    lock,
+    lockFiatValue
+  };
+}
