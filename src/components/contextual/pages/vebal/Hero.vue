316d75b5b8c0f1b8eee42bc9a255b9925feb4149
pkattera <kattera@gmail.com>
2022-03-27 14:38:01 +0100

    Feature/pk vebal header (#1560)
    * Updated hero section of veBAL page
    
    * Updates to the veBAL hero section including new graphics
    
    * Fixed linting errors and added a new graphic
    
    * Fixed dark mode linting issue
    
    * Tweaks to the veBAL hero
    
    * Linting fix
    
    * Deleted an unused hero image
    
    * Fix image url
    
    Co-authored-by: Gareth Fuller <gareth.a.f@gmail.com>

--- src/components/contextual/pages/vebal/Hero.vue
+++ src/components/contextual/pages/vebal/Hero.vue
@@ -2,22 +2,20 @@
 import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
-import useDarkMode from '@/composables/useDarkMode';
 
 /**
  * COMPOSABLES
  */
 const { t } = useI18n();
 const router = useRouter();
-const { darkMode } = useDarkMode();
 
 /**
  * COMPUTED
  */
 const benefits = computed(() => [
-  t('veBAL.hero.benefits.earn'),
   t('veBAL.hero.benefits.boost'),
-  t('veBAL.hero.benefits.vote')
+  t('veBAL.hero.benefits.vote'),
+  t('veBAL.hero.benefits.earn')
 ]);
 
 /**
@@ -34,35 +32,67 @@ function navigateToGetVeBAL() {
 </script>
 
 <template>
-  <div class="px-2 lg:px-0 w-full lg:w-1/2">
-    <h1 class="mb-8">{{ $t('veBAL.hero.title') }}</h1>
-    <div
-      v-for="(benefit, i) in benefits"
-      :key="i"
-      class="flex items-center mb-2 text-gray-600"
-    >
-      <BalIcon name="check" class="text-green-500 mr-2" />
-      {{ benefit }}
-    </div>
-    <div class="flex mt-8">
-      <BalBtn color="gradient" @click="navigateToGetVeBAL" class="mr-3">
-        {{ $t('veBAL.hero.buttons.getVeBAL') }}
-      </BalBtn>
-      <BalBtn
-        tag="a"
-        href="https://forum.balancer.fi/t/introducing-vebal-tokenomics/2512"
-        target="_blank"
-        rel="noreferrer"
-        :color="darkMode ? 'white' : 'primary'"
-        outline
-      >
-        {{ $t('veBAL.hero.buttons.learnMore') }}
-        <BalIcon
-          name="arrow-up-right"
-          size="sm"
-          class="ml-px group-hover:text-pink-500 transition-colors"
+  <div class="px-2 lg:px-0 w-full bg-gray-850 hero-container">
+    <div class="flex flex-col md:flex-row md:items-center max-w-7xl mx-auto">
+      <div class="py-8 px-4 md:order-2">
+        <img
+          src="@/assets/images/hero-veBAL.png"
+          class="max-w-full aspect-video"
         />
-      </BalBtn>
+      </div>
+      <div class="py-8 lg:py-4 xl:pt-0 px-4 lg:px-8 max-w-md">
+        <p class="text-white font-medium mb-3">
+          {{ $t('veBAL.hero.eyebrow') }}
+        </p>
+        <h1 class="title mb-6 text-white">{{ $t('veBAL.hero.title') }}</h1>
+        <div
+          v-for="(benefit, i) in benefits"
+          :key="i"
+          class="flex items-center mb-2 text-white"
+        >
+          <BalIcon name="check" class="text-green-500 mr-2" />
+          {{ benefit }}
+        </div>
+        <div class="flex mt-6">
+          <BalBtn
+            color="gradient"
+            @click="navigateToGetVeBAL"
+            class="mr-3 hero-btn"
+          >
+            {{ $t('veBAL.hero.buttons.getVeBAL') }}
+          </BalBtn>
+          <BalBtn
+            tag="a"
+            href="https://forum.balancer.fi/t/introducing-vebal-tokenomics/2512"
+            target="_blank"
+            rel="noreferrer"
+            color="white"
+            class="hero-btn"
+            outline
+          >
+            {{ $t('veBAL.hero.buttons.learnMore') }}
+            <BalIcon
+              name="arrow-up-right"
+              size="sm"
+              class="ml-px group-hover:text-pink-500 transition-colors"
+            />
+          </BalBtn>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<style scoped>
+.hero-container {
+  background-image: url('/images/backgrounds/spirograph.svg');
+  background-repeat: no-repeat;
+  background-position-x: right;
+  background-size: cover;
+}
+.title {
+  max-width: 820px;
+}
+.hero-btn {
+  min-width: 140px;
+}
+</style>
