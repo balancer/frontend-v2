1c482d418fbabe25f681104a1bfaf821f41eb588
1saf <me@ansaf.net>
2022-04-06 00:53:53 +1000

    feat: remove staking rewards composable and move logic to a service

---
+++ src/services/staking/staking-rewards.service.ts
@@ -0,0 +1,117 @@
+import LiquidityGaugeAbi from '@/lib/abi/LiquidityGaugeV5.json';
+import { getBalAddress } from '@/lib/utils';
+import { Multicaller } from '@/lib/utils/balancer/contract';
+import { getBptPrice } from '@/lib/utils/balancer/pool';
+import { configService } from '@/services/config/config.service';
+import { rpcProviderService } from '@/services/rpc-provider/rpc-provider.service';
+import { AddressZero } from '@ethersproject/constants';
+import { getUnixTime } from 'date-fns';
+import { formatUnits, getAddress } from 'ethers/lib/utils';
+import { isNil, mapValues } from 'lodash';
+import { GaugeController } from '../balancer/contracts/contracts/gauge-controller';
+import { LiquidityGauge } from '../balancer/contracts/contracts/liquidity-gauge';
+import { BalancerTokenAdmin } from '../balancer/contracts/contracts/token-admin';
+import { SubgraphGauge } from '../balancer/gauges/types';
+import { calculateGaugeApr, getAprRange } from './utils';
+import { AnyPool, DecoratedPool, Pool } from '../balancer/subgraph/types';
+import { TokenPrices } from '../coingecko/api/price.service';
+
+export type PoolAPRs = Record<string, { min: string; max: string }>;
+
+export class StakingRewardsService {
+  private gaugeController = new GaugeController(
+    configService.network.addresses.gaugeController
+  );
+  private tokenAdmin = new BalancerTokenAdmin(
+    configService.network.addresses.tokenAdmin
+  );
+
+  private async getWorkingSupplyForGauges(gaugeAddresses: string[]) {
+    // start with a fresh multicaller
+    const multicaller = LiquidityGauge.getMulticaller();
+
+    for (const gaugeAddress of gaugeAddresses) {
+      multicaller.call(
+        getAddress(gaugeAddress),
+        getAddress(gaugeAddress),
+        'working_supply'
+      );
+    }
+    const result = await multicaller.execute();
+    const supplies = mapValues(result, weight => formatUnits(weight, 18));
+    return supplies;
+  }
+
+  private async getRelativeWeightsForGauges(
+    gaugeAddresses: string[],
+    customTimestamp?: number
+  ) {
+    const timestamp = customTimestamp || getUnixTime(new Date());
+    const result = await this.gaugeController.getRelativeWeights(
+      gaugeAddresses,
+      timestamp
+    );
+    return result;
+  }
+
+  /**
+   * @summary calculates the APR for a gauge
+   */
+  async getGaugeAprForPools({
+    prices,
+    gauges,
+    pools
+  }: {
+    prices: TokenPrices;
+    gauges: SubgraphGauge[];
+    pools: Pool[];
+  }): Promise<PoolAPRs> {
+    const gaugeAddresses = gauges.map(gauge => gauge.id);
+    const inflationRate = await new BalancerTokenAdmin(
+      configService.network.addresses.tokenAdmin
+    ).getInflationRate();
+    const relativeWeights = await this.getRelativeWeightsForGauges(
+      gaugeAddresses
+    );
+
+    const aprs = gauges.map(async gauge => {
+      const poolId = gauge.poolId;
+      const pool = pools.find(pool => pool.id === poolId);
+      const nilApr = [poolId, { min: '0', max: '0' }];
+
+      if (!pool) return nilApr;
+      if (isNil(inflationRate)) return nilApr;
+
+      const bptPrice = getBptPrice(pool);
+      const balAddress = getBalAddress();
+      if (!balAddress) return nilApr;
+
+      const workingSupplies = await this.getWorkingSupplyForGauges(
+        gaugeAddresses
+      );
+      const balPrice = prices[getAddress(balAddress)].usd;
+
+      const apr = calculateGaugeApr({
+        gaugeAddress: getAddress(gauge.id),
+        bptPrice: bptPrice.toString(),
+        balPrice: String(balPrice),
+        // undefined inflation rate is guarded above
+        inflationRate: inflationRate as string,
+        boost: '1',
+        workingSupplies,
+        relativeWeights
+      });
+
+      console.log([poolId, apr.toString()]);
+
+      const range = getAprRange(apr.toString());
+      return [poolId, range];
+    });
+
+    const resolvedAprs = await Promise.all(aprs);
+
+    return Object.fromEntries(resolvedAprs);
+  }
+}
+
+export const stakingRewardsService = new StakingRewardsService();
---
+++ src/services/staking/utils.ts
@@ -0,0 +1,81 @@
+import { bnum } from '@/lib/utils';
+import BigNumber from 'bignumber.js';
+
+const MIN_BOOST = 1;
+const MAX_BOOST = 2.5;
+
+export function calculateWeeklyReward(
+  workingSupply: BigNumber,
+  balPayableToGauge: BigNumber
+) {
+  const workingBalance = 0.4;
+  const shareForOneBpt = bnum(workingBalance).div(
+    workingSupply.plus(workingBalance)
+  );
+  const weeklyReward = shareForOneBpt.times(balPayableToGauge);
+  return weeklyReward;
+}
+
+export function calculateBalPayableToGauge(
+  inflationRate: BigNumber,
+  gaugeRelativeWeight: BigNumber
+) {
+  return bnum(inflationRate)
+    .times(7)
+    .times(86400)
+    .times(gaugeRelativeWeight);
+}
+
+export function calculateGaugeApr({
+  gaugeAddress,
+  inflationRate,
+  balPrice,
+  bptPrice,
+  workingSupplies,
+  relativeWeights,
+  boost = '1'
+}: {
+  gaugeAddress: string;
+  inflationRate: string;
+  balPrice: string;
+  bptPrice: string;
+  boost: string;
+  workingSupplies: Record<string, string>;
+  relativeWeights: Record<string, string>;
+}) {
+  const workingSupply = bnum((workingSupplies || {})[gaugeAddress]) || '0';
+  const relativeWeight = bnum((relativeWeights || {})[gaugeAddress]) || '0';
+  const balPayable = calculateBalPayableToGauge(
+    bnum(inflationRate),
+    relativeWeight
+  );
+  const weeklyReward = calculateWeeklyReward(workingSupply, balPayable);
+  const yearlyReward = weeklyReward
+    .times(boost)
+    .times(52)
+    .times(balPrice);
+
+  console.log('hey', {
+    workingSupplies,
+    relativeWeights,
+    gaugeAddress,
+    workingSupply: workingSupply.toString(),
+    relativeWeight: relativeWeight.toString(),
+    balPayable: balPayable.toString(),
+    weeklyReward: weeklyReward.toString(),
+    yearlyReward: yearlyReward.toString(),
+    inflationRate: inflationRate.toString(),
+    bptPrice: bptPrice.toString()
+  });
+  const apr = yearlyReward.div(bptPrice);
+  return apr;
+}
+
+export function getAprRange(apr: string) {
+  const min = bnum(apr).times(MIN_BOOST);
+  const max = bnum(apr).times(MAX_BOOST);
+  return {
+    min: min.toString(),
+    max: max.toString()
+  };
+}
