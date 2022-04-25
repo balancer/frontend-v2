e1071fc95270441a0545de11e5a8594d2566623d
pkattera <kattera@gmail.com>
2022-04-25 11:36:30 +0100

    Alignment of featured pools CTA

--- src/components/sections/FeaturedPools.vue
+++ src/components/sections/FeaturedPools.vue
@@ -29,10 +29,12 @@
           hFull
           growContent
         >
+        <div>
           <h6>LUSD3CRV-f—ePyvCurveLUSD</h6>
           <p>
             Element principal pool Yearn crvLUSD v2 vault.
           </p>
+        </div>          
           <template v-slot:footer>
             <BalLink :href="EXTERNAL_LINKS.Element.Pools.LUSD" external>
               <span>{{ $t('viewOnElement') }}</span>
@@ -64,6 +66,12 @@
     </div>
   </div>
 </template>
+<style scoped>
+   /deep/ .card-container {
+    justify-content: space-between;
+    height: 100%;
+  }
+</style>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
