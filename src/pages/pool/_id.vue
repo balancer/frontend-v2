a4184b3a73154b7d837e370dc41d7b34eb6e15b3
pkattera <kattera@gmail.com>
2022-04-29 09:17:52 +0100

    padding tweaks

--- src/pages/pool/_id.vue
+++ src/pages/pool/_id.vue
@@ -48,7 +48,7 @@
               <BalIcon
                 name="arrow-up-right"
                 size="sm"
-                class="ml-1 mt-1 text-gray-500 hover:text-blue-500 transition-colors"
+                class="ml-2 mt-2 text-gray-500 hover:text-blue-500 transition-colors"
               />
             </BalLink>
           </div>
