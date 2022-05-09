5013d2ad894749a086233ff0c968b03180cdb7e0
Tim Robinson <timjrobinson@users.noreply.github.com>
2022-05-09 22:19:04 +1000

    Hotfix: Fix Table Sorting (#1847)
    * Return sorted data for unpinnedData variable
    
    * 1.52.9

--- src/components/_global/BalTable/BalTable.vue
+++ src/components/_global/BalTable/BalTable.vue
@@ -181,7 +181,7 @@ onMounted(() => {
  * COMPUTED
  */
 const unpinnedData = computed(() => {
-  if (!props.pin) return props.data;
+  if (!props.pin) return tableData.value;
   return (tableData.value || []).filter(
     data => !props.pin?.pinnedData.includes(data[props.pin.pinOn])
   );
