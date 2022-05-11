94001563b2a1c125d28284f20039f5b363b096c4
Gareth Fuller <gareth.a.f@gmail.com>
2022-05-11 11:55:34 +0100

    Hotfix - aave aprs (#1855)
    * Add generic erc20 multicaller class
    
    * Refactor to handle when onchain pool data not available
    The aave APRs are needed in the pool list where we don't fetch onchain data for every pool. In general external linear pool calculations shouldn't be dependent on our schema.
    
    * Bump version
    
    * Use correct balance for apr calc

---
+++ src/services/multicalls/erc20.multicaller.ts
@@ -0,0 +1,39 @@
+import {
+  InvestmentPool__factory,
+  StablePool__factory,
+  WeightedPool__factory
+} from '@balancer-labs/typechain';
+
+import ERC20_ABI from '@/lib/abi/ERC20.json';
+import IERC4626 from '@/lib/abi/IERC4626.json';
+import LinearPoolAbi from '@/lib/abi/LinearPool.json';
+import StablePhantomPool from '@/lib/abi/StablePhantomPool.json';
+import StaticATokenLMAbi from '@/lib/abi/StaticATokenLM.json';
+import { Multicaller } from '@/lib/utils/balancer/contract';
+
+import { configService } from '../config/config.service';
+import { rpcProviderService } from '../rpc-provider/rpc-provider.service';
+
+const ERC20ABIs = Object.values(
+  Object.fromEntries(
+    [
+      ...WeightedPool__factory.abi,
+      ...StablePool__factory.abi,
+      ...InvestmentPool__factory.abi,
+      ...StablePhantomPool,
+      ...LinearPoolAbi,
+      ...StaticATokenLMAbi,
+      ...ERC20_ABI,
+      ...IERC4626
+    ].map(row => [row.name, row])
+  )
+);
+
+export class ERC20Multicaller extends Multicaller {
+  constructor(
+    private readonly config = configService,
+    private readonly jsonProvider = rpcProviderService.jsonProvider
+  ) {
+    super(config.network.key, jsonProvider, ERC20ABIs);
+  }
+}
