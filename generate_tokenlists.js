4842a2f6b48ddbde9056513040f4933a70b71247
1saf <me@ansaf.net>
2022-05-19 01:20:06 +1000

    update: dynamic and lint fix

--- generate_tokenlists.js
+++ generate_tokenlists.js
@@ -1,17 +1,21 @@
-const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
 
+let tokenlists = {
+  balancer:
+    'https://raw.githubusercontent.com/balancer-labs/assets/master/generated/listed.tokenlist.json'
+};
+
 async function generate() {
   if (!fs.existsSync('./public/tokens/balancer.json')) {
-    const tokenlistResponse = await axios.get(
-      'https://raw.githubusercontent.com/balancer-labs/assets/master/generated/listed.tokenlist.json'
-    );
-    const tokenlist = tokenlistResponse.data;
-    fs.writeFileSync(
-      './public/tokens/balancer.json',
-      JSON.stringify(tokenlist)
-    );
+    for (const tokenlist of Object.keys(tokenlists)) {
+      const tokenlistResponse = await axios.get(tokenlists[tokenlist]);
+      const tokenlistJSON = tokenlistResponse.data;
+      fs.writeFileSync(
+        `./public/tokens/${tokenlist}.json`,
+        JSON.stringify(tokenlistJSON)
+      );
+    }
   }
 }
 
