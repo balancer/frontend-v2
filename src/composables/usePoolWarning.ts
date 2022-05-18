ab2b9f00e32789431b65f706a9a95f33a9d4b6ec
Gareth Fuller <gareth.a.f@gmail.com>
2022-05-18 09:48:04 +0100

    Add new warning for L2 ecosystem affected pools

--- src/composables/usePoolWarning.ts
+++ src/composables/usePoolWarning.ts
@@ -36,7 +36,7 @@ const POOL_ISSUES = {
       '0xc31a37105b94ab4efca1954a14f059af11fcd9bb000000000000000000000455',
       '0xc17636e36398602dd37bb5d1b3a9008c7629005f0002000000000000000004c4'
     ],
-    poolOwnerVulnWarningEcosystem: [
+    poolOwnerVulnWarningEcosystemMigrate: [
       '0xaf5e0b5425de1f5a630a8cb5aa9d97b8141c908d000200000000000000000366',
       '0xb4670d1389c758e4380c4211bcbc85342688b9c50002000000000000000003d8'
     ]
@@ -45,7 +45,7 @@ const POOL_ISSUES = {
     poolOwnerVulnWarningGovernance: [
       '0x5a5884fc31948d59df2aeccca143de900d49e1a300000000000000000000006f'
     ],
-    poolOwnerVulnWarningEcosystem: [
+    poolOwnerVulnWarningEcosystemMigrate: [
       '0x0510ccf9eb3ab03c1508d3b9769e8ee2cfd6fdcf00000000000000000000005d'
     ]
   }
