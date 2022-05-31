f0395cbc0c999f1658a3ba2854670013b5f31fe0
Gareth Fuller <gareth.a.f@gmail.com>
2022-05-31 14:41:24 +0100

    Hide feature alert on L2s

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script lang="ts">
 import BigNumber from 'bignumber.js';
 import { defineComponent, onBeforeMount, ref, watch } from 'vue';
-import { useI18n } from 'vue-i18n';
 import { VueQueryDevTools } from 'vue-query/devtools';
 import { useRoute } from 'vue-router';
 import { useStore } from 'vuex';
@@ -15,15 +14,9 @@ import useWeb3 from '@/services/web3/useWeb3';
 
 import GlobalModalContainer from './components/modals/GlobalModalContainer.vue';
 import AppSidebar from './components/navs/AppNav/AppSidebar/AppSidebar.vue';
-import useAlerts, {
-  Alert,
-  AlertPriority,
-  AlertType
-} from './composables/useAlerts';
 import useBackgroundColor from './composables/useBackgroundColor';
 import useGnosisSafeApp from './composables/useGnosisSafeApp';
 import useNavigationGuards from './composables/useNavigationGuards';
-import { isL2 } from './composables/useNetwork';
 import { useSidebar } from './composables/useSidebar';
 import useExploitWatcher from './composables/watchers/useExploitWatcher';
 import useGlobalQueryWatchers from './composables/watchers/useGlobalQueryWatchers';
@@ -63,51 +56,19 @@ export default defineComponent({
     } = useWeb3();
     const route = useRoute();
     const store = useStore();
-    const { addAlert } = useAlerts();
-    const { t } = useI18n();
     const { newRouteHandler: updateBgColorFor } = useBackgroundColor();
     const { sidebarOpen } = useSidebar();
 
-    // Temporary feature alert for Balancer boosted pools.
-    // commented out until veBAL launch, then change details for veBAL alert
-    // if (isMainnet.value) {
-    //   const featureAlert: Alert = {
-    //     id: 'boosted-pools',
-    //     priority: AlertPriority.LOW,
-    //     label: t('alerts.boostedPools'),
-    //     type: AlertType.FEATURE,
-    //     rememberClose: true,
-    //     actionOnClick: true,
-    //     action: () =>
-    //       router.push({
-    //         name: 'pool',
-    //         params: {
-    //           id:
-    //             '0x7b50775383d3d6f0215a8f290f2c9e2eebbeceb20000000000000000000000fe'
-    //         },
-    //         query: {
-    //           utm_source: 'website',
-    //           utm_medium: 'banner',
-    //           utm_campaign: 'aaveboostedpools'
-    //         }
-    //       })
-    //   };
-    //   addAlert(featureAlert);
-    // }
-
-    // Temporary feature alert due to the decreased APRs on L2s
-    // because of the 1 week gap from veBAL
-    if (isL2.value) {
-      const featureAlert: Alert = {
-        id: 'vebal-gap',
-        priority: AlertPriority.LOW,
-        label: t('alerts.vebalL2'),
-        type: AlertType.FEATURE,
-        rememberClose: false,
-        actionOnClick: false
-      };
-      addAlert(featureAlert);
-    }
+    // ADD FEATURE ALERT HERE
+    // const featureAlert: Alert = {
+    //   id: 'vebal-gap',
+    //   priority: AlertPriority.LOW,
+    //   label: t('alerts.vebalL2'),
+    //   type: AlertType.FEATURE,
+    //   rememberClose: false,
+    //   actionOnClick: false
+    // };
+    // addAlert(featureAlert);
 
     /**
      * CALLBACKS
