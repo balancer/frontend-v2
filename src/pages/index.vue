de756c0b3caf896026503eef66e81e0ba63697e0
sssmi <simeon.kerkola@gmail.com>
2022-06-07 10:28:03 +0300

    Create a portfolio page, move user pools from home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,8 +3,6 @@ import { computed, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
 
-import StakedPoolsTable from '@/components/contextual/pages/pools/StakedPoolsTable.vue';
-import UnstakedPoolsTable from '@/components/contextual/pages/pools/UnstakedPoolsTable.vue';
 import TokenSearchInput from '@/components/inputs/TokenSearchInput.vue';
 import FeaturedProtocols from '@/components/sections/FeaturedProtocols.vue';
 import PoolsTable from '@/components/tables/PoolsTable/PoolsTable.vue';
@@ -13,17 +11,13 @@ import usePools from '@/composables/pools/usePools';
 import useStreamedPoolsQuery from '@/composables/queries/useStreamedPoolsQuery';
 import useAlerts, { AlertPriority, AlertType } from '@/composables/useAlerts';
 import useBreakpoints from '@/composables/useBreakpoints';
-import { isMigratablePool } from '@/composables/usePool';
 import useTokens from '@/composables/useTokens';
-import { MIN_FIAT_VALUE_POOL_MIGRATION } from '@/constants/pools';
-import { bnum } from '@/lib/utils';
-import StakingProvider from '@/providers/local/staking/staking.provider';
 import useWeb3 from '@/services/web3/useWeb3';
 
 // COMPOSABLES
 const router = useRouter();
 const { t } = useI18n();
-const { isWalletReady, appNetworkConfig, isWalletConnecting } = useWeb3();
+const { appNetworkConfig } = useWeb3();
 const isElementSupported = appNetworkConfig.supportsElementPools;
 const {
   selectedTokens,
@@ -31,9 +25,7 @@ const {
   removeSelectedToken
 } = usePoolFilters();
 
-const { userPools, isLoadingPools, isLoadingUserPools, poolsQuery } = usePools(
-  selectedTokens
-);
+const { isLoadingPools, poolsQuery } = usePools(selectedTokens);
 const {
   dataStates,
   result: investmentPools,
@@ -43,17 +35,6 @@ const {
 const { addAlert, removeAlert } = useAlerts();
 const { upToMediumBreakpoint } = useBreakpoints();
 const { priceQueryLoading } = useTokens();
-
-// COMPUTED
-const showMigrationColumn = computed(() =>
-  userPools.value?.some(pool => {
-    return (
-      isMigratablePool(pool) &&
-      // TODO: this is a temporary solution to allow only big holders to migrate due to gas costs.
-      bnum(pool.shares).gt(MIN_FIAT_VALUE_POOL_MIGRATION)
-    );
-  })
-);
 
 // userPools.value[0].shares
 watch(poolsQuery.error, () => {
@@ -72,15 +53,10 @@ watch(poolsQuery.error, () => {
   }
 });
 
-const migratableUserPools = computed(() => {
-  return userPools.value.filter(pool => isMigratablePool(pool));
-});
-
 const isInvestmentPoolsTableLoading = computed(
   () => dataStates['basic'] === 'loading' || priceQueryLoading.value
 );
 
-watch(showMigrationColumn, () => console.log(showMigrationColumn.value));
 /**
  * METHODS
  */
@@ -91,35 +67,6 @@ function navigateToCreatePool() {
 
 <template>
   <div class="lg:container lg:mx-auto pt-10 md:pt-12">
-    <template v-if="isWalletReady || isWalletConnecting">
-      <BalStack vertical>
-        <div class="px-4 lg:px-0">
-          <BalStack horizontal justify="between" align="center">
-            <h3>{{ $t('myInvestments') }}</h3>
-          </BalStack>
-        </div>
-        <BalStack vertical spacing="xl">
-          <StakingProvider>
-            <UnstakedPoolsTable :userPools="userPools" />
-            <StakedPoolsTable :userPools="userPools" />
-          </StakingProvider>
-          <BalStack vertical spacing="sm" v-if="migratableUserPools.length > 0">
-            <h5 class="px-4 lg:px-0">{{ $t('poolsToMigrate') }}</h5>
-            <PoolsTable
-              :key="migratableUserPools"
-              :isLoading="isLoadingUserPools"
-              :data="migratableUserPools"
-              :noPoolsLabel="$t('noInvestments')"
-              showPoolShares
-              :selectedTokens="selectedTokens"
-              :hiddenColumns="['poolVolume', 'poolValue', 'stake']"
-            />
-          </BalStack>
-        </BalStack>
-      </BalStack>
-      <div class="mb-16" />
-    </template>
-
     <BalStack vertical>
       <div class="px-4 lg:px-0">
         <h3 class="mb-3">{{ $t('investmentPools') }}</h3>
---
+++ src/pages/portfolio.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { computed, watch } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+import StakedPoolsTable from '@/components/contextual/pages/pools/StakedPoolsTable.vue';
+import UnstakedPoolsTable from '@/components/contextual/pages/pools/UnstakedPoolsTable.vue';
+import PoolsTable from '@/components/tables/PoolsTable/PoolsTable.vue';
+import usePoolFilters from '@/composables/pools/usePoolFilters';
+import usePools from '@/composables/pools/usePools';
+import useAlerts, { AlertPriority, AlertType } from '@/composables/useAlerts';
+import { isMigratablePool } from '@/composables/usePool';
+import StakingProvider from '@/providers/local/staking/staking.provider';
+import useWeb3 from '@/services/web3/useWeb3';
+
+// COMPOSABLES
+
+const { t } = useI18n();
+const { isWalletReady, isWalletConnecting } = useWeb3();
+
+const { selectedTokens } = usePoolFilters();
+
+const { userPools, isLoadingUserPools, poolsQuery } = usePools(selectedTokens);
+
+const { addAlert, removeAlert } = useAlerts();
+
+// userPools.value[0].shares
+watch(poolsQuery.error, () => {
+  if (poolsQuery.error.value) {
+    addAlert({
+      id: 'pools-fetch-error',
+      label: t('alerts.pools-fetch-error'),
+      type: AlertType.ERROR,
+      persistent: true,
+      action: poolsQuery.refetch.value,
+      actionLabel: t('alerts.retry-label'),
+      priority: AlertPriority.MEDIUM
+    });
+  } else {
+    removeAlert('pools-fetch-error');
+  }
+});
+
+const migratableUserPools = computed(() => {
+  return userPools.value.filter(pool => isMigratablePool(pool));
+});
+</script>
+
+<template>
+  <div class="lg:container lg:mx-auto pt-10 md:pt-12">
+    <template v-if="isWalletReady || isWalletConnecting">
+      <BalStack vertical>
+        <div class="px-4 lg:px-0">
+          <BalStack horizontal justify="between" align="center">
+            <h3>{{ $t('myInvestments') }}</h3>
+          </BalStack>
+        </div>
+        <BalStack vertical spacing="xl">
+          <StakingProvider>
+            <UnstakedPoolsTable :userPools="userPools" />
+            <StakedPoolsTable :userPools="userPools" />
+          </StakingProvider>
+          <BalStack vertical spacing="sm" v-if="migratableUserPools.length > 0">
+            <h5 class="px-4 lg:px-0">{{ $t('poolsToMigrate') }}</h5>
+            <PoolsTable
+              :key="migratableUserPools"
+              :isLoading="isLoadingUserPools"
+              :data="migratableUserPools"
+              :noPoolsLabel="$t('noInvestments')"
+              showPoolShares
+              :selectedTokens="selectedTokens"
+              :hiddenColumns="['poolVolume', 'poolValue', 'stake']"
+            />
+          </BalStack>
+        </BalStack>
+      </BalStack>
+      <div class="mb-16" />
+    </template>
+  </div>
+</template>
