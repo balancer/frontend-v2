eb39da8236771571e9fd229c6486e979cb2c8dc1
sssmi <simeon.kerkola@gmail.com>
2022-06-12 14:09:33 +0300

    Control BalAsset root element type with "button" prop, use typescript

--- src/components/_global/BalAsset/BalAsset.vue
+++ src/components/_global/BalAsset/BalAsset.vue
@@ -1,96 +1,111 @@
 <template>
-  <img
-    v-if="iconSRC && !error"
-    :src="iconSRC"
+  <component
+    class="bal-asset rounded-full inline-block leading-none shadow-sm"
     :style="{
       width: `${size}px`,
-      height: `${size}px`,
-      background: 'white'
+      height: `${size}px`
     }"
-    @error="error = true"
-    class="rounded-full inline-block leading-none shadow-sm"
-  />
-  <Avatar v-else-if="!!address" :address="address" :size="size" />
-  <div
-    v-else
-    class="rounded-full inline-block leading-none shadow-sm overflow-visible"
-    :style="{
-      width: `${size}px`,
-      height: `${size}px`,
-      background: `${noIconColor} !important`
-    }"
-  />
+    :is="rootElement"
+    v-bind="rootElementAttrs"
+  >
+    <img
+      v-if="iconSRC && !error"
+      :src="iconSRC"
+      :style="{
+        background: 'white'
+      }"
+      @error="error = true"
+      class="rounded-full"
+    />
+    <Avatar v-else-if="!!address" :address="address" :size="size" />
+    <div
+      v-else
+      class="overflow-visible"
+      :style="{
+        width: `${size}px`,
+        height: `${size}px`,
+        background: `${noIconColor} !important`
+      }"
+    />
+  </component>
 </template>
 
-<script>
-import { computed, defineComponent, ref, toRefs, watch } from 'vue';
+<script setup lang="ts">
+import { computed, ref, toRefs, watch } from 'vue';
 
 import useDarkMode from '@/composables/useDarkMode';
 import useTailwind from '@/composables/useTailwind';
 import useTokens from '@/composables/useTokens';
 import useUrls from '@/composables/useUrls';
+import { TokenInfo } from '@/types/TokenList';
 
 import Avatar from '../../images/Avatar.vue';
 
-export default defineComponent({
-  name: 'BalAsset',
+type Props = {
+  address?: string;
+  iconURI?: string;
+  size?: number;
+  button?: boolean;
+};
 
-  components: {
-    Avatar
-  },
+const props = withDefaults(defineProps<Props>(), {
+  address: '',
+  size: 24,
+  button: false
+});
 
-  props: {
-    address: {
-      type: String
-    },
-    iconURI: { type: String },
-    size: {
-      type: Number,
-      default: 24
-    }
-  },
-  setup(props) {
-    /**
-     * COMPOSABLES
-     */
-    const { getToken } = useTokens();
-    const { resolve } = useUrls();
-    const { theme } = useTailwind();
-    const { darkMode } = useDarkMode();
+/**
+ * COMPOSABLES
+ */
+const { getToken } = useTokens();
+const { resolve } = useUrls();
+const { theme } = useTailwind();
+const { darkMode } = useDarkMode();
 
-    /**
-     * STATE
-     */
-    const { address } = toRefs(props);
-    const error = ref(false);
+/**
+ * STATE
+ */
+const { address } = toRefs(props);
+const error = ref(false);
 
-    /**
-     * COMPUTED
-     */
-    const iconSRC = computed(() => {
-      if (props.iconURI) return resolve(props.iconURI);
+/**
+ * COMPUTED
+ */
+const token = computed<TokenInfo | undefined>(() => getToken(address.value));
 
-      const token = getToken(address.value);
-      if (!token) return '';
-      return resolve(token.logoURI);
-    });
+const iconSRC = computed(() => {
+  if (props.iconURI) return resolve(props.iconURI);
 
-    const noIconColor = computed(() =>
-      darkMode.value ? theme.colors.gray['700'] : theme.colors.gray['300']
-    );
+  if (!token.value?.logoURI) return '';
+  return resolve(token.value.logoURI);
+});
 
-    /**
-     * WATCHERS
-     */
-    watch(iconSRC, newURL => {
-      if (newURL !== '') error.value = false;
-    });
+const rootElement = computed(() => (props.button ? 'button' : 'div'));
 
-    return {
-      iconSRC,
-      error,
-      noIconColor
-    };
-  }
+const rootElementAttrs = computed(() => ({
+  'aria-label': token.value?.symbol
+}));
+
+const noIconColor = computed(() =>
+  darkMode.value ? theme.colors.gray['700'] : theme.colors.gray['300']
+);
+
+/**
+ * WATCHERS
+ */
+watch(iconSRC, newURL => {
+  if (newURL !== '') error.value = false;
 });
 </script>
+
+<style scoped>
+button.bal-asset {
+  @apply transition-transform border-0 shadow;
+}
+
+button.bal-asset:hover,
+button.bal-asset:focus {
+  @apply scale-110 transform-gpu;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+}
+</style>
