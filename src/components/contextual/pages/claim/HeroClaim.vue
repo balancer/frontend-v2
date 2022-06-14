83c28f2644d3f3567d176c450959139813553954
pkattera <kattera@gmail.com>
2022-06-14 11:50:11 +0100

    Updated claims header

--- src/components/contextual/pages/claim/HeroClaim.vue
+++ src/components/contextual/pages/claim/HeroClaim.vue
@@ -12,11 +12,6 @@ const router = useRouter();
 /**
  * COMPUTED
  */
-const benefits = computed(() => [
-  t('veBAL.hero.benefits.boost'),
-  t('veBAL.hero.benefits.vote'),
-  t('veBAL.hero.benefits.earn')
-]);
 
 /**
  * METHODS
@@ -32,77 +27,142 @@ function navigateToGetVeBAL() {
 </script>
 
 <template>
-  <div class="w-full bg-gray-850 hero-container">
-    <div class="hero-content">
-      <div
-        class="hero-text py-8 lg:py-4 xl:pt-0 px-4 lg:px-8 2xl:px-0 max-w-md"
-      >
-        <h1 class="title mb-6 text-white">{{ $t('claimHero.title') }}</h1>
-        <p class="text-white">
-          {{
-            isL2
-              ? $t('pages.claim.descriptionL2')
-              : $t('pages.claim.description')
-          }}
-        </p>
+  <div class="bg">
+    <div class="w-full hero-container">
+      <div class="hero-content">
+        <div class="hero-text fade-in-slow">
+          <h1 class="title mb-3 text-white">
+            {{ $t('claimHero.title') }}
+          </h1>
+          <p class="text-white pb-0 font-medium">
+            {{ $t('claimHero.description') }}
+          </p>
+        </div>
       </div>
-      <div class="coins"></div>
+      <div class="hero-graphic-container">
+        <div
+          class="grid grid-cols-2 sm:grid-cols-4 gap-8 sm:gap-4 xl:gap-12 items-center w-full fade-in-slow"
+        >
+          <div class="tip group">
+            <img
+              class="tip-icon"
+              src="@/assets/images/icons/claims-header/add-liquidity.svg"
+            />
+            <p class="tip-label font-semibold">
+              <!-- <span class="tip-number">1</span> -->
+              Add liquidity
+              <BalTooltip
+                iconSize="xs"
+                textAlign="left"
+                class="relative top-px"
+                iconClass="text-white"
+                width="60"
+              >
+                {{ $t('claimHero.tips.addLiquidity') }}
+              </BalTooltip>
+            </p>
+          </div>
+          <div class="tip group">
+            <img
+              class="tip-icon"
+              src="@/assets/images/icons/claims-header/stake.svg"
+            />
+            <p class="tip-label font-semibold">
+              Stake to earn
+              <BalTooltip
+                iconSize="xs"
+                textAlign="left"
+                class="relative top-px"
+                iconClass="text-white"
+                width="60"
+              >
+                {{ $t('claimHero.tips.stake') }}
+              </BalTooltip>
+            </p>
+          </div>
+          <div class="tip group">
+            <img
+              class="tip-icon"
+              src="@/assets/images/icons/claims-header/vebal.svg"
+            />
+            <p class="tip-label font-semibold">
+              Get a boost
+              <BalTooltip
+                iconSize="xs"
+                textAlign="left"
+                class="relative top-px"
+                iconClass="text-white"
+                width="60"
+              >
+                {{ $t('claimHero.tips.boost') }}
+              </BalTooltip>
+            </p>
+          </div>
+          <div class="tip group">
+            <img
+              class="tip-icon"
+              src="@/assets/images/icons/claims-header/sparkles.svg"
+            />
+            <p class="tip-label font-semibold">
+              Claim anytime
+              <BalTooltip
+                iconSize="xs"
+                textAlign="left"
+                class="relative top-px"
+                iconClass="text-white"
+                width="60"
+              >
+                {{ $t('claimHero.tips.claim') }}
+              </BalTooltip>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
 <style scoped>
-.hero-container {
+.bg {
+  background: linear-gradient(
+    90deg,
+    rgba(255, 0, 0, 0.5) 0%,
+    rgba(255, 0, 255, 0) 25%
+  );
   background-image: url('/images/backgrounds/claims-header.png');
-  background-repeat: no-repeat;
-  background-position-x: right;
-  background-size: cover;
-  min-height: 440px;
-  @apply flex content-center;
+  @apply bg-no-repeat bg-cover bg-center;
 }
-.hero-content {
-  @apply flex flex-col md:flex-row md:items-center max-w-screen-2xl mx-auto md:gap-4 lg:gap-8 py-4 md:py-8 w-full;
-}
-.hero-text {
-  opacity: 0;
-  animation: fadeIn 1s ease-out 0.1s both;
-}
-.title {
-  max-width: 820px;
-}
-.hero-btn {
-  min-width: 140px;
-}
-.coins {
-  @apply flex lg:gap-8;
-  flex-grow: 1;
-}
-.coin-wrapper {
-  aspect-ratio: 7 / 8;
-}
-.coin {
-  @apply w-full flex flex-col items-center justify-end;
+.hero-container {
+  @apply grid grid-cols-1 lg:grid-cols-3 lg:container lg:mx-auto;
 }
 
-.coin:hover .graphic {
+.hero-graphic-container {
+  @apply w-full px-4 py-8 xl:p-8 flex items-center lg:col-span-2;
+}
+
+.hero-content {
+  @apply px-4 pt-12 lg:p-8 lg:pl-0 flex items-center w-full relative;
+}
+
+.hero-text {
+  @apply opacity-0;
+}
+
+.tip {
+  @apply flex flex-col items-center text-center group;
+}
+.tip-icon {
+  @apply pb-4 transition-transform duration-300;
+}
+.tip-label {
+  @apply text-white leading-5 transition-colors;
+}
+
+.tip:hover .tip-icon {
   transform: translateY(-8px);
 }
-.graphic {
-  @apply transition-transform duration-300;
-}
-.caption {
-  @apply font-semibold text-sm md:text-base text-gray-400 transition-colors text-center group-hover:text-white;
-  animation: fadeInMoveUp 0.5s ease-out 0.15s both;
-}
 
-@media (min-width: 768px) {
-  .hero-text {
-    min-width: 400px;
-  }
-}
-</style>
-
-<style>
-.caption .bal-icon {
-  animation: fadeInMoveUp 0.5s ease-out 0.15s both;
+.tip-number {
+  @apply block pb-1;
 }
 </style>
