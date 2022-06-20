c6e6f4e52730808deec1f6447f7ecf21e9dfd5fd
alter-eggo <anastas3993@gmail.com>
2022-06-20 14:51:47 +0400

    fix lint issues

--- src/components/_global/BalChart/BalChart.vue
+++ src/components/_global/BalChart/BalChart.vue
@@ -4,8 +4,7 @@ import { GridOption } from 'echarts/types/dist/shared';
 import { last } from 'lodash';
 import { Dictionary } from 'lodash';
 import numeral from 'numeral';
-import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
-import { nextTick } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import ECharts from 'vue-echarts';
 
 import useDarkMode from '@/composables/useDarkMode';
