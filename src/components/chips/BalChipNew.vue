9be6542f3949c1ea871583941561e18c3fe3f2e2
Simeon Kerkola <simeon.kerkola@gmail.com>
2022-06-23 06:08:21 +0300

    Feature/add expired state for expired pool gauges (#1979)
    * Add multicaller calls that fetch the is_killed status for each gauge in given array of gauges
    
    * Show "Expired" text if gauge is expired
    
    * Use useExpiredGaugesQuery
    
    * Add New and Expired type chips
    
    * Fix queryKey
    
    * Clean
    
    * Reduce chip border-radius
    
    * Show red Remove button if user has votes in expired gauge
    
    * Get table row class based on expired status of the pool
    
    * Restore locale json formatting
    
    * Add comment
    
    * Add a red border for expired gauge row instead of a red background

---
+++ src/components/chips/BalChipNew.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+type Props = {
+  size?: 'sm' | 'md' | 'lg';
+};
+
+/**
+ * PROPS & EMITS
+ */
+const props = withDefaults(defineProps<Props>(), {
+  size: 'sm'
+});
+</script>
+
+<template>
+  <BalChip v-bind="props" color="orange" class="uppercase" :outline="false">
+    {{ $t('new') }}
+  </BalChip>
+</template>
