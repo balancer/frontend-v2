e69a7a6274a2d9dc24fdefa89d8322d9f672d23b
Tim Robinson <tim@timjrobinson.com>
2022-06-28 19:02:06 +0900

    Lint fix

--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-import { useI18n } from 'vue-i18n';
 
 import StakedPoolsTable from '@/components/contextual/pages/pools/StakedPoolsTable.vue';
 import UnstakedPoolsTable from '@/components/contextual/pages/pools/UnstakedPoolsTable.vue';
