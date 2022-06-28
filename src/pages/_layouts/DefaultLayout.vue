dcb6f295e1bbbb4acc43b98beb86df6e3034af15
pkattera <kattera@gmail.com>
2022-06-28 09:53:39 +0100

    Remove Intercom from the site (#2019)
    * Remove Intercom from the site
    
    * Fix lint issue - removed unused variable

--- src/pages/_layouts/DefaultLayout.vue
+++ src/pages/_layouts/DefaultLayout.vue
@@ -1,11 +1,5 @@
 <script setup lang="ts">
 import AppNav from '@/components/navs/AppNav/AppNav.vue';
-import useBreakpoints from '@/composables/useBreakpoints';
-
-/**
- * COMPOSABLES
- */
-const { isDesktop } = useBreakpoints();
 </script>
 
 <template>
@@ -14,25 +8,11 @@ const { isDesktop } = useBreakpoints();
     <div class="pb-16">
       <router-view :key="$route.path" />
     </div>
-    <BalBtn
-      v-if="isDesktop"
-      id="intercom-activator"
-      circle
-      size="lg"
-      color="blue"
-      class="fixed bottom-0 right-0 m-4 z-100"
-    >
-      <BalIcon name="message-square" size="lg" />
-    </BalBtn>
   </div>
 </template>
 
 <style>
 .VueQueryDevtoolsPanel + button {
   @apply text-black bg-gray-100 p-2 rounded text-sm;
-}
-
-#intercom-activator {
-  z-index: 2147483004;
 }
 </style>
