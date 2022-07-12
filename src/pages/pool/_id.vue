06c3645d5444870bc0aa6b342b886a82809d8e4d
pkattera <kattera@gmail.com>
2022-07-12 14:12:18 +0100

    Updated based on a-e's PR feedback

--- src/pages/pool/_id.vue
+++ src/pages/pool/_id.vue
@@ -13,7 +13,7 @@
             <div
               v-for="([address, tokenMeta], i) in titleTokens"
               :key="i"
-              class="mt-2 mr-2 flex items-center h-10 bg-gray-50 dark:bg-gray-850 rounded-lg"
+              class="mt-2 mr-2 flex items-center px-2 h-10 bg-gray-50 dark:bg-gray-850 rounded-lg"
             >
               <BalAsset :address="address" />
               <span class="ml-2">
