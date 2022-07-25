6684c56aac1dfdf1527b7a3a679e038e774cbe01
sssmi <simeon.kerkola@gmail.com>
2022-07-25 19:00:24 +0300

    Set passed math props as reactive

--- src/components/forms/pool_actions/InvestForm/components/InvestFormTotals.vue
+++ src/components/forms/pool_actions/InvestForm/components/InvestFormTotals.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, toRefs } from 'vue';
+import { computed, reactive, toRefs } from 'vue';
 
 import useNumbers, { FNumFormats } from '@/composables/useNumbers';
 import useWeb3 from '@/services/web3/useWeb3';
@@ -42,7 +42,7 @@ const {
   optimized,
   swapRouteLoading,
   supportsPropotionalOptimization
-} = toRefs(props.math);
+} = toRefs(reactive(props.math));
 
 /**
  * COMPUTED
