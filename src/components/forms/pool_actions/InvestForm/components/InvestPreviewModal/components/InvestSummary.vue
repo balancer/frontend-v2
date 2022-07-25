e74754a0fc66ab46be19a05b3d53fcbcc7983079
sssmi <simeon.kerkola@gmail.com>
2022-07-25 13:10:46 +0300

    Update InvestSummary

--- src/components/forms/pool_actions/InvestForm/components/InvestPreviewModal/components/InvestSummary.vue
+++ src/components/forms/pool_actions/InvestForm/components/InvestPreviewModal/components/InvestSummary.vue
@@ -28,9 +28,6 @@ const { currency } = useUserSettings();
 
 <template>
   <div class="summary-table">
-    <h6 class="p-2">
-      {{ $t('summary') }}
-    </h6>
     <div class="flex flex-col py-2">
       <div class="summary-table-row">
         <div class="summary-table-label">
@@ -82,7 +79,8 @@ const { currency } = useUserSettings();
 
 <style scoped>
 .summary-table {
-  @apply border dark:border-gray-700 divide-y dark:divide-gray-700 rounded-lg mt-4;
+  @apply border dark:border-gray-700 divide-y dark:divide-gray-700 rounded-lg mt-4 bg-gray-50 dark:bg-gray-800;
+  @apply text-sm;
 }
 
 .summary-table-row {
