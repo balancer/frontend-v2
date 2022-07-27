7207a5455b0a6023995e10ab91726da081fc4b29
Gareth Fuller <gareth.a.f@gmail.com>
2022-07-27 11:15:13 +0100

    Fix tests

--- src/components/btns/ClaimProtocolRewardsBtn.spec.ts
+++ src/components/btns/ClaimProtocolRewardsBtn.spec.ts
@@ -1,8 +1,9 @@
 import { AddressZero } from '@ethersproject/constants';
 import { fireEvent, render } from '@testing-library/vue';
 
+import { txResponseMock } from '@/__mocks__/transactions';
 import BalBtn from '@/components/_global/BalBtn/BalBtn.vue';
-import { feeDistributor } from '@/services/balancer/contracts/contracts/fee-distributor';
+import { FeeDistributor } from '@/services/balancer/contracts/contracts/fee-distributor';
 
 import ClaimProtocolRewardsBtn from './ClaimProtocolRewardsBtn.vue';
 import TxActionBtn from './TxActionBtn/TxActionBtn.vue';
@@ -20,7 +21,29 @@ jest.mock('@/services/web3/useWeb3');
 jest.mock('@/services/rpc-provider/rpc-provider.service');
 jest.mock('vue-i18n');
 
+const mockClaimBalance = jest.fn().mockResolvedValue(txResponseMock);
+const mockClaimBalances = jest.fn().mockResolvedValue(txResponseMock);
+
 describe.only('ClaimProtocolRewardsBtn', () => {
+  beforeAll(() => {
+    // @ts-ignore
+    FeeDistributor.mockImplementation(() => {
+      return {
+        claimableTokens: [
+          '0x7B50775383d3D6f0215A8F290f2C9e2eEBBEceb2',
+          '0xba100000625a3754423978a60c9317c58a424e3D'
+        ],
+        claimBalance: mockClaimBalance,
+        claimBalances: mockClaimBalances
+      };
+    });
+  });
+
+  beforeEach(() => {
+    // @ts-ignore
+    FeeDistributor.mockClear();
+  });
+
   it('should render props', () => {
     const { getByText } = render(ClaimProtocolRewardsBtn, {
       props: {
@@ -42,13 +65,13 @@ describe.only('ClaimProtocolRewardsBtn', () => {
 
     await fireEvent.click(btn);
 
-    expect(feeDistributor.claimBalances).toBeCalled();
+    expect(mockClaimBalances).toBeCalled();
   });
 
   it('Calls FeeDistributor.claimBalance on click: Claim', async () => {
     const { getByText } = render(ClaimProtocolRewardsBtn, {
       props: {
-        tokenAddress: feeDistributor.claimableTokens[0],
+        tokenAddress: '0x7B50775383d3D6f0215A8F290f2C9e2eEBBEceb2',
         fiatValue: '1000'
       }
     });
@@ -57,9 +80,10 @@ describe.only('ClaimProtocolRewardsBtn', () => {
 
     await fireEvent.click(btn);
 
-    expect(feeDistributor.claimBalance).toBeCalledWith(
+    // @ts-ignore
+    expect(mockClaimBalance).toBeCalledWith(
       AddressZero,
-      feeDistributor.claimableTokens[0]
+      '0x7B50775383d3D6f0215A8F290f2C9e2eEBBEceb2'
     );
   });
 });
