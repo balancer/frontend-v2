8a0faf4e349c73badd27eea1fb111b587bbfa9cc
sssmi <simeon.kerkola@gmail.com>
2022-08-01 10:12:25 +0300

    Allow v-bind function keyword to be any case

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,11 +5,16 @@ module.exports = {
     'stylelint-selector-bem-pattern',
     'stylelint-config-recommended-vue',
     'stylelint-config-tailwindcss',
-    'stylelint-config-prettier'
+    'stylelint-config-prettier',
   ],
   rules: {
     'string-quotes': 'single',
-    // ignored tailwind's theme, and vue's v-bind functions
-    'function-no-unknown': [true, { ignoreFunctions: ['v-bind', 'theme'] }]
-  }
+    // Allow tailwind's theme, and vue's v-bind functions
+    'function-no-unknown': [true, { ignoreFunctions: ['v-bind', 'theme'] }],
+
+    // Allow v-bind function (a method to use component state properties inside <style> tag)
+    // keyword to be any case.
+    // Use default lower case value for other functions
+    'value-keyword-case': ['lower', { ignoreFunctions: ['v-bind'] }],
+  },
 };
