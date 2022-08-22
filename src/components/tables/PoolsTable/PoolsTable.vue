c698c5207ca096ef815e14fc5d9a9b65446c27f5
pkattera <kattera@gmail.com>
2022-08-22 14:19:20 +0100

    Fixed alignment of 'new' label in pools table.

--- src/components/tables/PoolsTable/PoolsTable.vue
+++ src/components/tables/PoolsTable/PoolsTable.vue
@@ -297,8 +297,8 @@ function iconAddresses(pool: PoolWithShares) {
               :isStablePool="isStableLike(pool.poolType)"
               :selectedTokens="selectedTokens"
             />
-            <BalChipNew v-if="pool?.isNew" class="ml-2" />
           </div>
+          <BalChipNew v-if="pool?.isNew" class="mt-1" />
         </div>
       </template>
       <template #volumeCell="pool">
