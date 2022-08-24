5dc4018cd51d0cec25bd20a08c861eced86ef683
Matthews3301 <salamon.mat@gmail.com>
2022-08-24 12:08:28 +0100

    Limit icon and tooltip

---
+++ src/components/icons/IconLimit.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+/**
+ * TYPES
+ */
+type Props = {
+  amount?: string;
+  tooltip?: string;
+  size?: 'sm' | 'md' | 'lg';
+};
+
+/**
+ * PROPS
+ */
+const props = withDefaults(defineProps<Props>(), {
+  amount: '1',
+  tooltip: `Limit amount`,
+  size: 'md',
+});
+
+/**
+ * COMPUTED
+ */
+const sizeClass = computed(() => {
+  switch (props.size) {
+    case 'sm':
+      return 'text-xxs p-0 w-5 h-5 leading-4';
+    case 'lg':
+      return 'text-md p-2 w-11 h-11';
+    default:
+      return 'text-sm p-1 w-8 h-8';
+  }
+});
+</script>
+
+<template>
+  <BalTooltip textAlign="left">
+    <template #activator>
+      <div
+        :class="sizeClass"
+        class="inline-block font-medium text-center rounded-full border-2 border-red-600"
+      >
+        {{ amount }}
+      </div>
+    </template>
+    <div>
+      <div class="text-xs font-normal">
+        {{ tooltip }}
+      </div>
+    </div>
+  </BalTooltip>
+</template>
+
+<style scoped>
+.text-xxs {
+  font-size: 0.6rem;
+  line-height: 1rem;
+}
+</style>
