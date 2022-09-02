029530a79961378f073d04c9e652eec916d50803
sssmi <simeon.kerkola@gmail.com>
2022-09-02 19:15:06 +0300

    Make transaction (fails)

--- src/components/contextual/pages/vebal/LMVoting/ResubmitVotes/GaugeVoteResubmitModal.vue
+++ src/components/contextual/pages/vebal/LMVoting/ResubmitVotes/GaugeVoteResubmitModal.vue
@@ -24,7 +24,7 @@ import { isPositive } from '@/lib/utils/validations';
 import { VeBalLockInfo } from '@/services/balancer/contracts/contracts/veBAL';
 import { VotingGaugeWithVotes } from '@/services/balancer/gauges/gauge-controller.decorator';
 import { gaugeControllerService } from '@/services/contracts/gauge-controller.service';
-import { WalletError } from '@/types';
+import { Address, WalletError } from '@/types';
 import { TransactionActionState } from '@/types/transactions';
 import useVotingEscrowLocks from '@/composables/useVotingEscrowLocks';
 import useVotingGauges from '@/composables/useVotingGauges';
@@ -53,7 +53,7 @@ const { votingGauges: allVotingGauges } = useVotingGauges();
 /**
  * STATE
  */
-const votes = ref({});
+const votes = ref<Record<Address, string>>({});
 
 /**
  * COMPUTED
@@ -64,9 +64,49 @@ const votingGauges = computed(() =>
   })
 );
 
+const totalVotes = computed<number>(() =>
+  Object.values(votes.value).reduce<number>(
+    (total, value) => total + Number(value),
+    0
+  )
+);
+const disabled = computed<boolean>(() => totalVotes.value > 100);
+
 /**
  * METHODS
  */
+async function submitVote() {
+  const gaugeAddresses = Object.keys(votes.value);
+  const weights = Object.values(votes.value).map(weight =>
+    scale(weight, 2).toString()
+  );
+  console.log({
+    gaugeAddresses,
+    weights,
+  });
+  try {
+    // voteState.init = true;
+    // voteState.error = null;
+    const tx = await gaugeControllerService.voteForManyGaugeWeights(
+      gaugeAddresses,
+      weights.map(weight => BigNumber.from(weight))
+    );
+    console.log({ tx });
+    // voteState.init = false;
+    // voteState.confirming = true;
+    // handleTransaction(tx);
+  } catch (e) {
+    console.error(e);
+    const error = e as WalletError;
+    console.error({ error });
+    // voteState.init = false;
+    // voteState.confirming = false;
+    // voteState.error = {
+    //   title: 'Vote failed',
+    //   description: error.message,
+    // };
+  }
+}
 
 watchEffect(() => {
   votingGauges.value.forEach(gauge => {
@@ -97,7 +137,26 @@ watchEffect(() => {
       ></GaugeItem>
 
       <div class="mt-4">
-        <BalBtn color="gradient" block> Confirm votes </BalBtn>
+        <div
+          :class="{
+            'bg-red-600': disabled,
+            'text-white': disabled,
+          }"
+        >
+          Total vote allocation {{ totalVotes }}%
+        </div>
+        <div v-if="disabled" class="text-red-500">
+          Your votes can’t exceed 100%
+        </div>
+        <BalBtn
+          class="mt-4"
+          :disabled="disabled"
+          color="gradient"
+          block
+          @click="submitVote"
+        >
+          Confirm votes
+        </BalBtn>
       </div>
     </div>
   </BalModal>
