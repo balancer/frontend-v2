00af2c894e4654c358af3fcfea8612dd2d656178
Matthews3301 <salamon.mat@gmail.com>
2022-09-06 00:16:53 +0100

    Error: underlying network changed fix

--- src/services/web3/web3.plugin.ts
+++ src/services/web3/web3.plugin.ts
@@ -115,7 +115,7 @@ export default {
     );
     const signer = computed(() => pluginState.connector?.provider?.getSigner());
     const userProvider = computed(() => {
-      return new Web3Provider(pluginState.connector.provider as any);
+      return new Web3Provider(pluginState.connector.provider as any, 'any');
     });
 
     async function getWalletConnector(
