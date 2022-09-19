ef9e6b791027f405d58fbd2f55b45876edb106ae
Gareth Fuller <gareth.a.f@gmail.com>
2022-09-19 14:19:11 +0100

    Add mising actualSupply overwrite

--- src/services/balancer/contracts/contracts/vault.ts
+++ src/services/balancer/contracts/contracts/vault.ts
@@ -11,6 +11,8 @@ import {
   isTradingHaltable,
   isWeightedLike,
   isDeep,
+  isComposableStableLike,
+  isComposableStable,
 } from '@/composables/usePool';
 import VaultAbi from '@/lib/abi/VaultAbi.json';
 import { isSameAddress } from '@/lib/utils';
@@ -81,10 +83,16 @@ export default class Vault {
     } else if (isStableLike(type)) {
       poolMulticaller.call('amp', poolAddress, 'getAmplificationParameter');
 
-      if (isDeep(pool)) {
+      if (isComposableStableLike(type)) {
         // Overwrite totalSupply with virtualSupply for StablePhantom pools
         poolMulticaller.call('totalSupply', poolAddress, 'getVirtualSupply');
+        if (isComposableStable(type)) {
+          // Overwrite totalSupply with actualSupply for ComposableStable pools
+          poolMulticaller.call('totalSupply', poolAddress, 'getActualSupply');
+        }
+      }
 
+      if (isDeep(pool)) {
         Object.keys(tokens).forEach((token, i) => {
           poolMulticaller.call(`linearPools.${token}.id`, token, 'getPoolId');
           poolMulticaller.call(
