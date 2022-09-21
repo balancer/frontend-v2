35c2f8b90ebe4dddc18dae6fd46ecb06134a0d69
Tim Robinson <tim@timjrobinson.com>
2022-09-22 00:03:34 +1000

    Fix type issue

--- src/__mocks__/pool.ts
+++ src/__mocks__/pool.ts
@@ -2,6 +2,7 @@ import { Pool } from '@/services/pool/types';
 import { PoolType } from '@balancer-labs/sdk';
 
 export const PoolMock: Pool = {
+  chainId: 1,
   onchain: {
     tokens: {
       '0x3Ec8798B81485A254928B70CDA1cf0A2BB0B74D7': {
