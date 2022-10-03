192ef8d12cd38583fdfbb7a23efa410720bb47a0
alter-eggo <anastas3993@gmail.com>
2022-10-03 19:42:18 +0400

    change pool snapshot query

--- src/services/balancer/subgraph/entities/poolSnapshots/index.ts
+++ src/services/balancer/subgraph/entities/poolSnapshots/index.ts
@@ -5,8 +5,6 @@ import { QueryBuilder } from '@/types/subgraph';
 import Service from '../../balancer-subgraph.service';
 import poolQueryBuilder from './query';
 
-const DAY = 60 * 60 * 24;
-
 export default class PoolShares {
   service: Service;
   query: QueryBuilder;
@@ -16,56 +14,22 @@ export default class PoolShares {
     this.query = query;
   }
 
-  public async get(
-    poolId: string,
-    days: number,
-    args = {},
-    attrs = {}
-  ): Promise<PoolSnapshots> {
-    const currentTimestamp = Math.ceil(Date.now() / 1000);
-    const dayTimestamp = currentTimestamp - (currentTimestamp % DAY);
-    const timestamps: number[] = [];
-    for (let i = 0; i < days; i++) {
-      timestamps.push(dayTimestamp - i * DAY);
-    }
-    attrs = { ...attrs, __aliasFor: 'poolSnapshot' };
-    const query = Object.fromEntries(
-      timestamps.map(timestamp => {
-        const timestampArgs = { ...args, id: `${poolId}-${timestamp}` };
-        const timestampFragment = this.query(timestampArgs, attrs).poolSnapshot;
-        return [`_${timestamp}`, timestampFragment];
-      })
-    );
-
+  public async get(args = {}, attrs = {}): Promise<PoolSnapshots> {
+    const query = this.query(args, attrs);
     const snapshots = await this.service.client.get(query);
-    return this.serialize(snapshots);
+    return this.serialize(snapshots.poolSnapshots);
   }
 
-  private serialize(snapshotData: Record<string, PoolSnapshot>): PoolSnapshots {
-    return Object.fromEntries(
-      Object.entries(snapshotData)
-        .map(entry => {
-          const [id, data] = entry;
-          const timestamp = toJsTimestamp(parseInt(id.substr(1)));
-          if (!data) {
-            return [timestamp, null];
-          }
-          const { amounts, totalShares, swapVolume, swapFees, liquidity } =
-            data;
-
-          return [
-            timestamp,
-            {
-              timestamp,
-              amounts,
-              totalShares,
-              swapVolume,
-              swapFees,
-              liquidity,
-            },
-          ];
-        })
-        .filter(entry => !!entry[1])
-    );
+  private serialize(poolSnapshots: PoolSnapshot[]): PoolSnapshots {
+    return poolSnapshots
+      .reverse()
+      .reduce((acc: Record<string, PoolSnapshot>, snapshot: PoolSnapshot) => {
+        const timestamp = toJsTimestamp(snapshot.timestamp);
+        acc[timestamp] = {
+          ...snapshot,
+          timestamp,
+        };
+        return acc;
+      }, {});
   }
 }
