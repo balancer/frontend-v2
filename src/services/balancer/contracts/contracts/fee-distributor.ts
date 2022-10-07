900fe207e63d0ded4ff174ca546fc0d640b6c59d
Matthews3301 <salamon.mat@gmail.com>
2022-10-07 17:39:29 +0100

    Fixed injected prices for bb-a-usd

--- src/services/balancer/contracts/contracts/fee-distributor.ts
+++ src/services/balancer/contracts/contracts/fee-distributor.ts
@@ -14,7 +14,7 @@ import { web3Service } from '@/services/web3/web3.service';
 export class FeeDistributor {
   public claimableTokens: string[] = [
     '0x7B50775383d3D6f0215A8F290f2C9e2eEBBEceb2', // bb-a-USD v1
-    '0xa13a9247ea42d743238089903570127dda72fe44', // bb-a-USD v2
+    '0xA13a9247ea42D743238089903570127DdA72fE44', // bb-a-USD v2
     '0xba100000625a3754423978a60c9317c58a424e3D', // BAL
   ];
 
