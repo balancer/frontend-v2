afe5485a367399f3cc835f48ab14ed7bfca5ffb0
Matthews3301 <salamon.mat@gmail.com>
2022-10-07 16:06:32 +0100

    Comments

--- src/services/balancer/contracts/contracts/fee-distributor.ts
+++ src/services/balancer/contracts/contracts/fee-distributor.ts
@@ -13,8 +13,8 @@ import { web3Service } from '@/services/web3/web3.service';
 
 export class FeeDistributor {
   public claimableTokens: string[] = [
-    '0x7B50775383d3D6f0215A8F290f2C9e2eEBBEceb2', // bb-a-USD
-    '0xa13a9247ea42d743238089903570127dda72fe44', // bb-a-USD
+    '0x7B50775383d3D6f0215A8F290f2C9e2eEBBEceb2', // bb-a-USD v1
+    '0xa13a9247ea42d743238089903570127dda72fe44', // bb-a-USD v2
     '0xba100000625a3754423978a60c9317c58a424e3D', // BAL
   ];
 
