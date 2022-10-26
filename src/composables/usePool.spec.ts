e1c176b510e86644b5e8de3e8dbaa21c9ba0885c
Gareth Fuller <gareth.a.f@gmail.com>
2022-10-26 09:37:50 +0100

    Should only extract LinearPool mainTokens

--- src/composables/usePool.spec.ts
+++ src/composables/usePool.spec.ts
@@ -19,14 +19,11 @@ test('tokenTreeNodes', () => {
   ]);
 });
 
-test('tokenTreeNodes', () => {
+test('tokenTreeLeafs', () => {
   const leafs = tokenTreeLeafs(BoostedPoolMock.tokens);
   expect(leafs).toEqual([
     '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
-    '0x3ed3b47dd13ec9a98b44e6204a523e766b225811', // aUSDT
     '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
-    '0xbcca60bb61934080951369a648fb03df4f96263c', // aUSDC
-    '0x028171bca77440897b824ca71d1c56cac55b68a3', // aDAI
     '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
   ]);
 });
