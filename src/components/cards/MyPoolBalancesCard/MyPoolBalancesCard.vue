a7da44ad3be29c9a875aafaac9af10a6feaacf55
alter-eggo <anastas3993@gmail.com>
2022-10-28 12:34:59 +0400

    remove pre-minted bpt when fetching historical prices

--- src/components/cards/MyPoolBalancesCard/MyPoolBalancesCard.vue
+++ src/components/cards/MyPoolBalancesCard/MyPoolBalancesCard.vue
@@ -2,7 +2,7 @@
 import { computed, ref, toRef } from 'vue';
 
 import useNumbers, { FNumFormats } from '@/composables/useNumbers';
-import { usePool } from '@/composables/usePool';
+import { tokensListExclBpt, usePool } from '@/composables/usePool';
 // Composables
 import useTokens from '@/composables/useTokens';
 import { bnum } from '@/lib/utils';
@@ -63,7 +63,7 @@ const propTokenAmounts = computed((): string[] => {
   if (isDeepPool.value) {
     // Return linear pool's main token balance using the price rate.
     // mainTokenBalance = linearPoolBPT * priceRate
-    return props.pool.tokensList.map((address, i) => {
+    return tokensListExclBpt(props.pool).map((address, i) => {
       if (!props.pool?.onchain?.linearPools) return '0';
 
       const priceRate = props.pool.onchain.linearPools[address].priceRate;
@@ -81,7 +81,7 @@ const tokenAddresses = computed((): string[] => {
     // so return mainTokens here so that fiat values are correct.
     return props.pool.mainTokens || [];
   }
-  return props.pool.tokensList;
+  return tokensListExclBpt(props.pool);
 });
 
 const fiatTotal = computed(() => {
