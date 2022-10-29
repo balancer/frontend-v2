1e339ed614a0c73d379cd961c4854323257295ac
sssmi <simeon.kerkola@gmail.com>
2022-10-29 21:22:14 +0300

    Return previous path if its available

--- src/composables/useReturnRoute.ts
+++ src/composables/useReturnRoute.ts
@@ -1,7 +1,8 @@
-import { RouteLocationRaw, useRoute } from 'vue-router';
+import { RouteLocationRaw, useRoute, useRouter } from 'vue-router';
 
 export function useReturnRoute() {
   const route = useRoute();
+  const router = useRouter();
 
   function getReturnRoute(to?: RouteLocationRaw): RouteLocationRaw {
     const queryReturnRoute = route.query?.returnRoute as string;
@@ -19,6 +20,10 @@ export function useReturnRoute() {
       return { name: queryReturnRoute };
     } else if (to) return to;
 
+    const prevPath = router.options.history.state.back;
+    if (prevPath && typeof prevPath === 'string') {
+      return prevPath;
+    }
     return { name: 'home' };
   }
 
