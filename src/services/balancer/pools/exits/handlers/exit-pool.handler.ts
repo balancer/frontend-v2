32301ebed557cd8140643c9d8c6130d6b3a10a0c
Gareth Fuller <gareth.a.f@gmail.com>
2022-10-31 09:14:27 +0000

    Add ExitPoolService (#2341)
    * Add ExitPoolProvider skeleton
    
    * Update missed join references
    
    * Add ExitPoolService

---
+++ src/services/balancer/pools/exits/handlers/exit-pool.handler.ts
@@ -0,0 +1,27 @@
+import { GasPriceService } from '@/services/gas-price/gas-price.service';
+import { Pool } from '@/services/pool/types';
+import { BalancerSDK } from '@balancer-labs/sdk';
+import { TransactionResponse } from '@ethersproject/abstract-provider';
+import { Signer } from '@ethersproject/abstract-signer';
+import { Ref } from 'vue';
+
+export type ExitParams = {
+  signer: Signer;
+  slippageBsp: number;
+};
+
+export type QueryOutput = {
+  priceImpact: number;
+};
+
+export abstract class ExitPoolHandler {
+  constructor(
+    public readonly pool: Ref<Pool>,
+    public readonly sdk: BalancerSDK,
+    public readonly gasPriceService: GasPriceService
+  ) {}
+
+  abstract exit(params: ExitParams): Promise<TransactionResponse>;
+
+  abstract queryExit(): Promise<QueryOutput>;
+}
