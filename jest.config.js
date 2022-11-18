a282537608dddd48a40ba670a706ca349344c9c5
Alberto Gualis <alberto.gualis@rebilly.com>
2022-11-18 09:42:56 +0100

    Add test to cover token lists provider

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: null,
   testRegex: '(\\.|/)(test|spec)\\.[jt]sx?$',
+  //TODO: replace with a smaller version
+  moduleNameMapper: {
+    '/public/data/tokenlists/tokens-5.json':
+      '<rootDir>/src/tests/tokenlists/tokens-5.json',
+  },
 };
