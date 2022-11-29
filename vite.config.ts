4f97ac5bf9053d4be4cd2f760637867cbad02385
Alberto Gualis <agualis@gmail.com>
2022-11-29 09:55:34 +0100

    Setup sentry for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 // import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite';
+import { Plugin, loadEnv } from 'vite';
 import { defineConfig } from 'vitest/config';
 import { version as pkgVersion } from './package.json';
 import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 import postcss from './postcss.config.js';
+import type { ViteSentryPluginOptions } from 'vite-plugin-sentry';
+import viteSentry from 'vite-plugin-sentry';
 
 //TODO: check how this is used
 process.VITE_APP_VERSION = pkgVersion;
@@ -13,32 +16,64 @@ if (process.env.NODE_ENV === 'production') {
   process.VITE_APP_BUILD_EPOCH = new Date().getTime().toString();
 }
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   // balancer-labs-SDK SOR expects this env
   // process.env = PRICE_ERROR_TOLERANCE = null;
+
+  const plugins = [
+    vue(),
+    // Type assertion to avoid TS errors in defineConfig
+    nodePolyfills() as unknown as Plugin,
+    // AutoImport({
+    //   imports: [
+    //     'vue',
+    //     'vue-router',
+    //   ],
+    //   dts: 'src/auto-imports.d.ts',
+    //   eslintrc: {
+    //     enabled: true,
+    //   },
+    // }),
+    Components({
+      dirs: ['src/components/_global/**'],
+      extensions: ['vue'],
+    }),
+  ];
+
+  const envConfig = loadEnv(mode, process.cwd());
+
+  if (mode === 'production' && envConfig.VITE_SENTRY_AUTH_TOKEN) {
+    /**
+     * Configure sentry plugin
+     */
+    const sentryConfig: ViteSentryPluginOptions = {
+      url: 'https://sentry.io',
+      authToken: envConfig.VITE_SENTRY_AUTH_TOKEN,
+      org: 'balancer-labs',
+      project: 'app',
+      release: `frontend-v2@${pkgVersion}`,
+      deploy: {
+        env: 'production',
+      },
+      setCommits: {
+        auto: true,
+      },
+      sourceMaps: {
+        include: ['./dist'],
+        // include: ['./dist/assets'],
+        ignore: ['node_modules', 'vite.config.ts'],
+        // urlPrefix: '~/assets',
+      },
+    };
+    plugins.push(viteSentry(sentryConfig));
+  }
 
   return {
     define: {
       'process.env': {},
     },
-    plugins: [
-      vue(),
-      nodePolyfills(),
-      // AutoImport({
-      //   imports: [
-      //     'vue',
-      //     'vue-router',
-      //   ],
-      //   dts: 'src/auto-imports.d.ts',
-      //   eslintrc: {
-      //     enabled: true,
-      //   },
-      // }),
-      Components({
-        dirs: ['src/components/_global/**'],
-        extensions: ['vue'],
-      }),
-    ],
+    plugins: plugins,
+    sourcemap: true,
     resolve: {
       alias: {
         '@': resolve(__dirname, './src'),
@@ -65,7 +100,7 @@ export default defineConfig(() => {
       globals: true,
       environment: 'happy-dom',
       setupFiles: 'tests/unit/vitest/setup-vitest.ts',
-      // coverage: { reporter: ['text', 'lcov'] }, // lcov reporter is used by IDE coverage extensions
+      coverage: { reporter: ['text', 'lcov'] }, // lcov reporter is used by IDE coverage extensions
       include: [
         'tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
         'src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
