9b69a57d7dc5441ed4d9bd1ec8c311e5d2abefa0
sssmi <simeon.kerkola@gmail.com>
2022-12-05 09:35:42 +0200

    Clean up

--- src/pages/_layouts/index.ts
+++ src/pages/_layouts/index.ts
@@ -2,4 +2,3 @@ export { default as BlankLayout } from './BlankLayout.vue';
 export { default as ContentLayout } from './ContentLayout.vue';
 export { default as DefaultLayout } from './DefaultLayout.vue';
 export { default as FocusedLayout } from './FocusedLayout.vue';
-export { default as PoolTransferLayout } from './PoolTransferLayout.vue';
