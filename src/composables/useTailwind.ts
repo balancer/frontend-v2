a1a1fd41711d7751ff237b674412deac3f4046ff
Alberto Gualis <agualis@gmail.com>
2022-12-05 18:23:00 +0100

    Fix tailwind related typecheck

--- src/composables/useTailwind.ts
+++ src/composables/useTailwind.ts
@@ -1,7 +1,10 @@
 import resolveConfig from 'tailwindcss/resolveConfig';
-
 import tailwindConfig from 'tailwind.config.js';
+import { Config } from 'tailwindcss';
 
 export default function useTailwind() {
-  return resolveConfig(tailwindConfig);
+  // Use any until tailwind decides to fully type resolveConfig:
+  // Check related discussions:
+  // https://github.com/tailwindlabs/tailwindcss/pull/9784#issuecomment-1312065258
+  return resolveConfig(tailwindConfig as Config) as any;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   safelist: [
