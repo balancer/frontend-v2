03260570baf14de349d2d7285e2dd144697d44e0
Alberto Gualis <agualis@gmail.com>
2022-12-09 08:33:14 +0100

    Improve sourcemap setup

--- src/plugins/sentry.ts
+++ src/plugins/sentry.ts
@@ -1,24 +1,25 @@
 import { captureException, init, setTag } from '@sentry/browser';
 import { Integrations } from '@sentry/tracing';
 import { App } from 'vue';
-
-import { version } from '../../package.json';
+// https://www.npmjs.com/package/vite-plugin-sentry
+import 'virtual:vite-plugin-sentry/sentry-config';
 import { isProductionMode } from './modes';
 
 // Using Sentry's vanila JS package (@sentry/browser) here instead of
 // the official vue package (@sentry/vue) because it doesn't support vue 3 yet.
 // https://github.com/getsentry/sentry-javascript/issues/2925
 
-const release = `frontend-v2@${version}`;
-
 function shouldInitSentry() {
-  if (!import.meta.env.VITE_SENTRY_AUTH_TOKEN) {
-    console.warn(
-      'Sentry disabled because VITE_SENTRY_AUTH_TOKEN env was not provided'
-    );
-    return false;
+  if (isProductionMode()) {
+    if (!import.meta.env.VITE_SENTRY_AUTH_TOKEN) {
+      console.warn(
+        'Sentry disabled because VITE_SENTRY_AUTH_TOKEN env was not provided'
+      );
+      return false;
+    }
+    return true;
   }
-  return isProductionMode();
+  return false;
 }
 
 export default function initSentry(app: App) {
@@ -36,12 +37,16 @@ export default function initSentry(app: App) {
       }
     };
 
+    const dist = import.meta.env.VITE_PLUGIN_SENTRY_CONFIG.dist;
+    const release = import.meta.env.VITE_PLUGIN_SENTRY_CONFIG.release;
+
     init({
       dsn: 'https://d292b6ec7b6e4aa2801d972e06cb232c@o574636.ingest.sentry.io/5725878',
       integrations: [new Integrations.BrowserTracing()],
       tracesSampleRate: 1.0,
       environment: import.meta.env.MODE,
       release,
+      dist,
     });
   }
 }
