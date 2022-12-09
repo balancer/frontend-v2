b8a9b52801bb81848f16f4e9650936e05ce796d6
Simeon Kerkola <simeon.kerkola@gmail.com>
2022-12-09 12:09:29 +0200

    Feature/vebal resubmit votes (#2536)
    * Init
    
    * Add empty string fallback

--- src/components/contextual/pages/vebal/LMVoting/LMVoting.vue
+++ src/components/contextual/pages/vebal/LMVoting/LMVoting.vue
@@ -3,15 +3,17 @@ import { computed, ref } from 'vue';
 
 import useExpiredGaugesQuery from '@/composables/queries/useExpiredGaugesQuery';
 import useVeBalLockInfoQuery from '@/composables/queries/useVeBalLockInfoQuery';
+import useVotingEscrowLocks from '@/composables/useVotingEscrowLocks';
 import useNumbers, { FNumFormats } from '@/composables/useNumbers';
-import { orderedPoolTokens, poolURLFor } from '@/composables/usePool';
+import { poolURLFor } from '@/composables/usePool';
 import useVotingGauges from '@/composables/useVotingGauges';
 import { bnum, scale } from '@/lib/utils';
 import { VotingGaugeWithVotes } from '@/services/balancer/gauges/gauge-controller.decorator';
 
 import GaugesTable from './GaugesTable.vue';
 import GaugeVoteModal from './GaugeVoteModal.vue';
-import { Pool } from '@/services/pool/types';
+import ResubmitVotesAlert from './ResubmitVotes/ResubmitVotesAlert.vue';
+import { orderedTokenURIs } from '@/composables/useVotingGauges';
 
 /**
  * DATA
@@ -32,10 +34,11 @@ const {
 const { fNum2 } = useNumbers();
 const veBalLockInfoQuery = useVeBalLockInfoQuery();
 
+const { shouldResubmitVotes } = useVotingEscrowLocks();
+
 const votingGaugeAddresses = computed<string[]>(
   () => votingGauges.value?.map(gauge => gauge.address) || []
 );
-
 const { data: expiredGauges } = useExpiredGaugesQuery(votingGaugeAddresses);
 
 /**
@@ -76,16 +79,6 @@ function setActiveGaugeVote(votingGauge: VotingGaugeWithVotes) {
   activeVotingGauge.value = votingGauge;
 }
 
-function orderedTokenURIs(gauge: VotingGaugeWithVotes): string[] {
-  const sortedTokens = orderedPoolTokens(
-    gauge.pool as unknown as Pool,
-    gauge.pool.tokens
-  );
-  return sortedTokens.map(
-    token => gauge.tokenLogoURIs[token?.address || ''] || ''
-  );
-}
-
 function handleModalClose() {
   activeVotingGauge.value = null;
   refetchVotingGauges.value();
@@ -97,87 +90,98 @@ function handleVoteSuccess() {
 </script>
 
 <template>
-  <div
-    class="flex flex-col lg:flex-row gap-4 lg:justify-between lg:items-end mb-2"
-  >
-    <div class="px-4 xl:px-0 max-w-3xl">
-      <h3 class="mb-2">
-        {{ $t('veBAL.liquidityMining.title') }}
-      </h3>
-      <p class="">
-        {{ $t('veBAL.liquidityMining.description') }}
-      </p>
-    </div>
-    <div class="flex gap-2 xs:gap-3 px-4 xl:px-0">
-      <BalCard shadow="none" class="md:w-48 min-w-max">
-        <div class="flex items-center">
-          <p class="inline mr-1 text-sm text-secondary">My unallocated votes</p>
-          <BalTooltip
-            :text="$t('veBAL.liquidityMining.myUnallocatedVotesTooltip')"
-            iconClass="text-gray-400 dark:text-gray-600"
-            iconSize="sm"
-            width="72"
-            class="mt-1"
-          />
-        </div>
-        <p
-          class="inline mr-1 text-lg font-semibold"
-          :class="{ 'text-red-500': hasExpiredLock }"
-        >
-          <span v-if="hasLock">
-            {{ unallocatedVotesFormatted }}
-          </span>
-          <span v-else class="mr-1">—</span>
+  <div class="flex flex-col">
+    <div
+      class="flex flex-col lg:flex-row gap-4 lg:justify-between lg:items-end mb-7"
+    >
+      <div class="px-4 xl:px-0 max-w-3xl">
+        <h3 class="mb-2">
+          {{ $t('veBAL.liquidityMining.title') }}
+        </h3>
+        <p class="">
+          {{ $t('veBAL.liquidityMining.description') }}
         </p>
-        <BalTooltip
-          v-if="hasExpiredLock"
-          :text="$t('veBAL.liquidityMining.votingPowerExpiredTooltip')"
-          iconSize="sm"
-          :iconName="'alert-triangle'"
-          :iconClass="'text-red-500 hover:text-red-700 dark:hover:text-red-400 transition-colors'"
-          width="72"
-          class="relative top-0.5"
-        />
-      </BalCard>
-      <BalCard shadow="none" class="md:w-48 min-w-max">
-        <div class="flex items-center">
+      </div>
+      <div class="flex gap-2 xs:gap-3 px-4 xl:px-0">
+        <BalCard shadow="none" class="md:w-48 min-w-max">
+          <div class="flex items-center">
+            <p class="inline mr-1 text-sm text-secondary">
+              My unallocated votes
+            </p>
+            <BalTooltip
+              :text="$t('veBAL.liquidityMining.myUnallocatedVotesTooltip')"
+              iconClass="text-gray-400 dark:text-gray-600"
+              iconSize="sm"
+              width="72"
+              class="mt-1"
+            />
+          </div>
           <p
-            :class="{ 'text-orange-500 font-medium': votingPeriodLastHour }"
-            class="inline mr-1 text-sm text-secondary"
+            class="inline mr-1 text-lg font-semibold"
+            :class="{ 'text-red-500': hasExpiredLock }"
           >
-            Voting period ends
+            <span v-if="hasLock">
+              {{ unallocatedVotesFormatted }}
+            </span>
+            <span v-else class="mr-1">—</span>
           </p>
           <BalTooltip
-            :text="$t('veBAL.liquidityMining.votingPeriodTooltip')"
+            v-if="hasExpiredLock"
+            :text="$t('veBAL.liquidityMining.votingPowerExpiredTooltip')"
             iconSize="sm"
-            iconClass="text-gray-400 dark:text-gray-600"
+            :iconName="'alert-triangle'"
+            :iconClass="'text-red-500 hover:text-red-700 dark:hover:text-red-400 transition-colors'"
             width="72"
-            class="mt-1"
+            class="relative top-0.5"
           />
-        </div>
-        <p class="text-lg font-semibold tabular-nums">
-          <span
-            v-if="votingPeriodEnd.length"
-            :class="{ 'text-orange-500': votingPeriodLastHour }"
-          >
-            {{
-              $t('veBAL.liquidityMining.votingPeriodCountdown', votingPeriodEnd)
-            }}
-          </span>
-        </p>
-      </BalCard>
+        </BalCard>
+        <BalCard shadow="none" class="md:w-48 min-w-max">
+          <div class="flex items-center">
+            <p
+              :class="{ 'text-orange-500 font-medium': votingPeriodLastHour }"
+              class="inline mr-1 text-sm text-secondary"
+            >
+              Voting period ends
+            </p>
+            <BalTooltip
+              :text="$t('veBAL.liquidityMining.votingPeriodTooltip')"
+              iconSize="sm"
+              iconClass="text-gray-400 dark:text-gray-600"
+              width="72"
+              class="mt-1"
+            />
+          </div>
+          <p class="text-lg font-semibold tabular-nums">
+            <span
+              v-if="votingPeriodEnd.length"
+              :class="{ 'text-orange-500': votingPeriodLastHour }"
+            >
+              {{
+                $t(
+                  'veBAL.liquidityMining.votingPeriodCountdown',
+                  votingPeriodEnd
+                )
+              }}
+            </span>
+          </p>
+        </BalCard>
+      </div>
     </div>
+    <ResubmitVotesAlert
+      v-if="shouldResubmitVotes"
+      class="mx-4 xl:mx-0 mb-7"
+    ></ResubmitVotesAlert>
+    <GaugesTable
+      :key="gaugesTableKey"
+      :expiredGauges="expiredGauges"
+      :isLoading="isLoading"
+      :data="votingGauges"
+      :noPoolsLabel="$t('noInvestments')"
+      showPoolShares
+      class="mb-8"
+      @clicked-vote="setActiveGaugeVote"
+    />
   </div>
-  <GaugesTable
-    :key="gaugesTableKey"
-    :expiredGauges="expiredGauges"
-    :isLoading="isLoading"
-    :data="votingGauges"
-    :noPoolsLabel="$t('noInvestments')"
-    showPoolShares
-    class="mb-8"
-    @clicked-vote="setActiveGaugeVote"
-  />
   <teleport to="#modal">
     <GaugeVoteModal
       v-if="!!activeVotingGauge"
