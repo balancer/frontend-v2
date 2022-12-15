81bfaf7840884629b00257ca8813d3f98233f8a3
Gareth Fuller <gareth.a.f@gmail.com>
2022-12-15 15:59:50 +0000

    Simplify useApp

--- src/composables/useApp.ts
+++ src/composables/useApp.ts
@@ -1,26 +1,7 @@
-import { computed, ComputedRef } from 'vue';
-import { useStore } from 'vuex';
 import { version } from '../../package.json';
-import useTokens from './useTokens';
-
-export interface AppProviderResponse {
-  version: string;
-  appLoading: ComputedRef<boolean>;
-}
 
 export default function useApp() {
-  return getAppProvision();
-}
-
-function getAppProvision() {
-  const store = useStore();
-  const { loading: loadingTokens } = useTokens();
-  const appLoading = computed(
-    () => store.state.app.loading || loadingTokens.value
-  );
   return {
     version,
-    // computed
-    appLoading,
   };
 }
