ad3c12110630556f4349217960cb19b5439824b6
Alberto Gualis <agualis@gmail.com>
2022-12-16 13:47:20 +0100

    Fix test

--- src/services/pool/pool.repository.spec.ts
+++ src/services/pool/pool.repository.spec.ts
@@ -5,7 +5,7 @@ import { computed } from 'vue';
 describe('Building a pool repository', () => {
   it('Logs when balancer api provider is skipped', () => {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    const log = vi.spyOn(console, 'log').mockImplementationOnce(() => {});
+    const log = jest.spyOn(console, 'log').mockImplementationOnce(() => {});
     new PoolRepository(computed(() => ({} as TokenInfoMap)));
     expect(log).toBeCalledWith(
       'Skipping balancer api provider in your current network (Goerli)'
