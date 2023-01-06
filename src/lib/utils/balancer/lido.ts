9e464bf22c622de2fd671b0ccedf58e4d5987a9c
Anastasios <anastas3993@gmail.com>
2023-01-06 15:17:38 +0400

    fix: token input errors (#2535)
    * add debounce in token input
    
    * fix wsteth input error
    
    * fix queryBatch error
    
    * fix wsteth amount adjust
    
    * add goerli rate provider
    
    * add calc by rate wstEth by stEth
    
    * fixes after review
    
    * Fix exactIn setting on pair toggle or max click (#2575)
    
    * Fix exactIn setting on pair toggle or max click
    
    * Remove log
    
    * fix: Error syntax
    
    * fix: Re-remove sor pkg from package.json
    
    Co-authored-by: Gareth Fuller <gareth.a.f@gmail.com>

--- src/lib/utils/balancer/lido.ts
+++ src/lib/utils/balancer/lido.ts
@@ -1,5 +1,6 @@
 import { BigNumberish, Contract } from 'ethers';
-
+import { BigNumber } from '@ethersproject/bignumber';
+import { WeiPerEther as ONE } from '@ethersproject/constants';
 import { configService } from '@/services/config/config.service';
 import { rpcProviderService } from '@/services/rpc-provider/rpc-provider.service';
 
@@ -7,6 +8,14 @@ import { includesAddress } from '..';
 
 const { stETH: stEthAddress, wstETH: wstEthAddress } =
   configService.network.addresses;
+
+const MAINNET_RATE_PROVIDER_ADDRESS =
+  '0x72d07d7dca67b8a406ad1ec34ce969c90bfee768';
+
+type ConversionParams = {
+  amount: BigNumber;
+  isWrap: boolean; // e.g. is stETH to wstETH
+};
 
 export function isStETH(tokenInAddress: string, tokenOutAddress: string) {
   if (!tokenInAddress || !tokenOutAddress || !stEthAddress) return false;
@@ -47,15 +56,24 @@ export function getWstETHByStETH(stETHAmount: BigNumberish) {
 }
 
 /**
- * @notice Get amount of stETH for a given amount of wstETH
+ * Convert stETH amount to wstETH or vice versa. Only relevant on mainnet when wrapping or unwrapping.
+ *
+ * @param {BigNumber} amount - The amount to convert, could be stETH or wstETH value.
+ * @param {boolean} isWrap - True if wrapping stETH to wstETH, false if unwrapping wstETH to stETH.
+ * @returns Converted value for wrap or unwrap, if input is stETH, returns wstETH value and vice versa.
  */
-export function getStETHByWstETH(wstETHAmount: BigNumberish) {
-  const wstETH = new Contract(
-    wstEthAddress,
-    [
-      'function getStETHByWstETH(uint256 wstETHAmount) external view returns (uint256)',
-    ],
-    rpcProviderService.jsonProvider
-  );
-  return wstETH.getStETHByWstETH(wstETHAmount);
+export async function convertStEthWrap({ amount, isWrap }: ConversionParams) {
+  try {
+    const rateProvider = new Contract(
+      MAINNET_RATE_PROVIDER_ADDRESS,
+      ['function getRate() external view returns (uint256)'],
+      rpcProviderService.jsonProvider
+    );
+
+    const rate = await rateProvider.getRate();
+
+    return isWrap ? amount.mul(rate).div(ONE) : amount.mul(ONE).div(rate);
+  } catch (error) {
+    throw new Error('Failed to convert stETH', { cause: error });
+  }
 }
