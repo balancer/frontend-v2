883ea768c976e6e3c58b8cea2e6d8ac9ee9d0197
Alberto Gualis <agualis@gmail.com>
2023-01-12 14:56:19 +0100

    Remove console.log

--- src/providers/inject.ts
+++ src/providers/inject.ts
@@ -6,7 +6,6 @@ export function safeInject<T>(symbol: InjectionKey<T>) {
     const injectionError =
       'Inject error: make sure that your parent is providing' +
       symbol.toString();
-    console.log('PACHAMAMA: ', injectionError);
     throw Error(injectionError);
   }
   return response;
