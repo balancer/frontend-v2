9f7805e437f32155b39dc87ac13b896ecf9b6eaa
Alberto Gualis <agualis@gmail.com>
2023-01-13 14:05:12 +0100

    Add comment

--- src/tests/noop.ts
+++ src/tests/noop.ts
@@ -1,2 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/*
+ * noop is an explicit "do nothing" function that avoids empty function eslint error
+ **/
 export const noop = (...args: any): any => {};
