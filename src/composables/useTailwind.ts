acb209451cd451d49a6d766645e01a5dbf50e579
Alberto Gualis <agualis@gmail.com>
2023-01-13 13:37:52 +0100

    Add tailwind import comment 2

--- src/composables/useTailwind.ts
+++ src/composables/useTailwind.ts
@@ -1,5 +1,7 @@
 import resolveConfig from 'tailwindcss/resolveConfig';
 
+// Note that we are explicitly importing from 'tailwind.config.js' which is a vite alias (see vite.config.ts)
+// because we need to use ES modules but tailwind + tailwind intellisense require commonJS import style to work properly.
 import tailwindConfig from 'tailwind.config.js';
 
 export default function useTailwind() {
