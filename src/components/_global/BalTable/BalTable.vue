61b8fbdd026e214cbe5e50e318ae4a08891cd7ce
Anastasios <anastas3993@gmail.com>
2023-01-17 13:29:26 +0400

    feat: Add filtering to veBal table (#2638)
    * add filtering by token name on vebal page
    
    * add network selection filter
    
    * add expired and user stake filters
    
    * add debounce to hide expired pools checkbox
    
    * add table description to tooltip
    
    * add token pills backlight
    
    * add debounced network filter choice
    
    * show expired pool if user voted
    
    * fix styles
    
    * small fixes
    
    * fixes after review
    
    * Changed the order of the filters and tweaked padding.
    
    * fixes after review 2
    
    * apply autofocus when necessary
    
    * add active filters num
    
    Co-authored-by: pkattera <kattera@gmail.com>

--- src/components/_global/BalTable/BalTable.vue
+++ src/components/_global/BalTable/BalTable.vue
@@ -321,7 +321,7 @@ watch(
         <!-- begin data rows -->
         <BalTableRow
           v-for="(dataItem, index) in unpinnedData"
-          :key="`tableRow-${index}`"
+          :key="`tableRow-${dataItem.id ?? index}`"
           :class="
             props.getTableRowClass
               ? props.getTableRowClass(dataItem, index)
