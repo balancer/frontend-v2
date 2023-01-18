8c9c8f7c1301e173d322a9008a702c3cac225e8f
Gareth Fuller <gareth.a.f@gmail.com>
2023-01-18 14:12:38 +0000

    refactor: make summaries consistent (#2663)
    * feat: Add BalDataList component
    
    * refactor: Use BalDataList for InvestSummary
    
    * refactor: Use BalDataList for WithdrawSummary
    
    * refactor: Create StakeSummary and use BalDataList

--- src/components/forms/pool_actions/InvestForm/components/InvestPreviewModal/components/InvestSummary.vue
+++ src/components/forms/pool_actions/InvestForm/components/InvestPreviewModal/components/InvestSummary.vue
@@ -32,71 +32,40 @@ const { currency } = useUserSettings();
 </script>
 
 <template>
-  <div class="summary-table">
-    <div class="flex flex-col py-2">
-      <div class="summary-table-row">
-        <div class="summary-table-label">
-          {{ $t('investment.preview.summary.total') }}
-        </div>
-        <div class="summary-table-number">
-          {{ fNum2(fiatTotal, FNumFormats.fiat) }}
+  <BalDataList :title="$t('summary')">
+    <BalDataListRow :label="$t('investment.preview.summary.total')">
+      <template #value>
+        {{ fNum2(fiatTotal, FNumFormats.fiat) }}
+        <BalTooltip
+          :text="$t('tooltips.addLiquidity.total', [currency.toUpperCase()])"
+          iconSize="sm"
+          class="ml-2"
+        />
+      </template>
+    </BalDataListRow>
+    <BalDataListRow
+      :label="$t('priceImpact')"
+      :class="{
+        'bg-red-50 dark:bg-red-500 text-red-500 dark:text-white':
+          highPriceImpact,
+      }"
+    >
+      <template #value>
+        <BalLoadingBlock v-if="isLoadingPriceImpact" class="w-10 h-6" />
+        <template v-else>
+          {{ fNum2(priceImpact, FNumFormats.percent) }}
           <BalTooltip
-            :text="$t('tooltips.addLiquidity.total', [currency.toUpperCase()])"
+            :text="$t('tooltips.addLiquidity.priceImpact')"
             iconSize="sm"
+            :iconName="highPriceImpact ? 'alert-triangle' : 'info'"
+            :iconClass="
+              highPriceImpact ? 'text-red-500 dark:text-white' : 'text-gray-300'
+            "
+            width="72"
             class="ml-2"
           />
-        </div>
-      </div>
-      <div
-        :class="[
-          'summary-table-row',
-          {
-            'bg-red-50 dark:bg-red-500 text-red-500 dark:text-white':
-              highPriceImpact,
-          },
-        ]"
-      >
-        <div class="summary-table-label">
-          {{ $t('priceImpact') }}
-        </div>
-        <div class="summary-table-number">
-          <BalLoadingBlock v-if="isLoadingPriceImpact" class="w-10 h-6" />
-          <template v-else>
-            {{ fNum2(priceImpact, FNumFormats.percent) }}
-            <BalTooltip
-              :text="$t('tooltips.addLiquidity.priceImpact')"
-              iconSize="sm"
-              :iconName="highPriceImpact ? 'alert-triangle' : 'info'"
-              :iconClass="
-                highPriceImpact
-                  ? 'text-red-500 dark:text-white'
-                  : 'text-gray-300'
-              "
-              width="72"
-              class="ml-2"
-            />
-          </template>
-        </div>
-      </div>
-    </div>
-  </div>
+        </template>
+      </template>
+    </BalDataListRow>
+  </BalDataList>
 </template>
-
-<style scoped>
-.summary-table {
-  @apply border dark:border-gray-700 divide-y dark:divide-gray-700 rounded-lg mt-4 bg-gray-50 dark:bg-gray-800;
-  @apply text-sm;
-}
-
-.summary-table-row {
-  @apply grid grid-cols-2 px-2 py-1;
-}
-
-.summary-table-label {
-  @apply flex items-center;
-}
-
-.summary-table-number {
-  @apply flex items-center justify-end;
-}
-</style>
