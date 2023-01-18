02fac5a14743a66a083b42412c6e52fdadc50f65
Anastasios <anastas3993@gmail.com>
2023-01-18 14:54:39 +0400

    chore: add optimisations (#2669)

--- src/components/_global/BalTable/BalTable.vue
+++ src/components/_global/BalTable/BalTable.vue
@@ -301,7 +301,7 @@ watch(
         <PinHeader v-if="pinnedData.length" />
         <BalTableRow
           v-for="(dataItem, index) in pinnedData"
-          :key="`tableRow-${index}`"
+          :key="`tableRow-${dataItem.id ?? index}`"
           :class="getTableRowClass(dataItem, index)"
           :data="dataItem"
           :columns="filteredColumns"
