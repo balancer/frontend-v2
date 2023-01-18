afbcc8eb7223c7f37349009804d1707adda2fb12
Alberto Gualis <agualis@gmail.com>
2023-01-18 09:52:49 +0100

    Use createHtmlPlugin to inject FHANTOM env in index.html

--- src/tests/noop.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-/*
- * noop is an explicit "do nothing" function that avoids empty function eslint error
- **/
-export const noop = (...args: any): any => {};
