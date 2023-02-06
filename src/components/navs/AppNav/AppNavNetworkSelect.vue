d64d5b1bf73a1e1d7e9e8b695588544bf98dda19
sssmi <simeon.kerkola@gmail.com>
2023-02-06 15:51:52 +0200

    Bring the brach back from the dead 🪦

--- src/components/navs/AppNav/AppNavNetworkSelect.vue
+++ src/components/navs/AppNav/AppNavNetworkSelect.vue
@@ -147,7 +147,11 @@ function isActive(network: NetworkOption): boolean {
 <template>
   <BalPopover noPad>
     <template #activator>
-      <BalBtn color="white" :size="upToLargeBreakpoint ? 'md' : 'sm'">
+      <BalBtn
+        :aria-label="$t('networkSelection')"
+        color="white"
+        :size="upToLargeBreakpoint ? 'md' : 'sm'"
+      >
         <template v-if="activeNetwork">
           <img
             :src="buildNetworkIconURL(activeNetwork.id)"
@@ -161,7 +165,7 @@ function isActive(network: NetworkOption): boolean {
         </template>
       </BalBtn>
     </template>
-    <div class="flex overflow-hidden flex-col w-48 rounded-lg">
+    <div role="menu" class="flex overflow-hidden flex-col w-48 rounded-lg">
       <div
         class="py-2 px-3 text-sm font-medium text-gray-500 whitespace-nowrap bg-gray-50 dark:bg-gray-800 border-b dark:border-gray-900"
       >
