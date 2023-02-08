42ff27f1b5583ab2d60af40c45bc6ad69df56e67
Gareth Fuller <gareth.a.f@gmail.com>
2023-02-08 14:53:13 +0000

    chore: Remove optional chaining

--- src/composables/queries/usePoolGaugesQuery.ts
+++ src/composables/queries/usePoolGaugesQuery.ts
@@ -43,7 +43,7 @@ export default function usePoolGaugesQuery(
   const subgraphQuery = computed(() => ({
     pool: {
       __args: {
-        id: poolAddress?.value?.toLowerCase(),
+        id: poolAddress.value?.toLowerCase(),
       },
       preferentialGauge: {
         id: true,
