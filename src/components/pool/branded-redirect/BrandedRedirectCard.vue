573bdb7bf9ba4675ae3da59a94b4834d3c4687db
alter-eggo <anastas3993@gmail.com>
2023-02-08 17:45:35 +0400

    add branded redirect card

---
+++ src/components/pool/branded-redirect/BrandedRedirectCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { BRANDED_REDIRECTS } from './constants';
+
+const props = defineProps<{
+  poolId: string;
+}>();
+
+const redirectData = computed(() => {
+  return BRANDED_REDIRECTS[props.poolId];
+});
+
+function openRedirectLink() {
+  window.open(redirectData.value.link, '_blank');
+}
+</script>
+
+<template>
+  <BalStack vertical>
+    <BalCard shadow="2xl" noPad class="rounded-xl">
+      <div>logo</div>
+      <div class="px-6 pb-4">
+        <div class="mb-1.5 text-lg font-semibold">
+          {{ $t(redirectData.title) }}
+        </div>
+        <div class="mb-3">
+          {{ $t(redirectData.description) }}
+        </div>
+        <BalBtn
+          color="blue"
+          :label="$t(redirectData.btnText)"
+          block
+          @click="openRedirectLink"
+        />
+      </div>
+    </BalCard>
+  </BalStack>
+</template>
---
+++ src/components/pool/branded-redirect/constants.ts
@@ -0,0 +1,30 @@
+import { networkId } from '@/composables/useNetwork';
+import { Network } from '@balancer-labs/sdk';
+
+interface BrandedRedirectData {
+  title: string;
+  description: string;
+  btnText: string;
+  link: string;
+}
+
+type BrandedRedirects = Record<string, BrandedRedirectData>;
+
+const POLYGON_BRANDED_REDIRECTS: BrandedRedirects = {
+  '0x726e324c29a1e49309672b244bdc4ff62a270407000200000000000000000702': {
+    title: 'brandedRedirect.xave.title',
+    description: 'brandedRedirect.xave.description',
+    btnText: 'brandedRedirect.xave.btnText',
+    link: 'https://xave.co/',
+  },
+};
+
+const BRANDED_REDIRECTS_BY_NETWORK = {
+  [Network.POLYGON]: POLYGON_BRANDED_REDIRECTS,
+};
+
+export const BRANDED_REDIRECTS: BrandedRedirects = BRANDED_REDIRECTS_BY_NETWORK[
+  networkId.value
+]
+  ? BRANDED_REDIRECTS_BY_NETWORK[networkId.value]
+  : {};
