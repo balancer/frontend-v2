f1335fc28f3946d21842d2d0cbb062e1108ea641
Alberto Gualis <alberto@balancer.finance>
2023-02-10 09:53:32 +0100

    Migrate new queries to tanstack vue-query

--- src/composables/queries/useStakedSharesQuery.ts
+++ src/composables/queries/useStakedSharesQuery.ts
@@ -1,6 +1,5 @@
-import { UseQueryOptions } from 'react-query/types';
-import { computed, reactive, Ref } from 'vue';
-import { useQuery } from 'vue-query';
+import { Ref } from 'vue';
+import { useQuery, UseQueryOptions } from '@tanstack/vue-query';
 
 import QUERY_KEYS from '@/constants/queryKeys';
 import useWeb3 from '@/services/web3/useWeb3';
@@ -16,6 +15,7 @@ import { formatUnits } from '@ethersproject/units';
 type QueryResponse = {
   [poolId: string]: string;
 };
+type QueryOptions = UseQueryOptions<QueryResponse>;
 
 /**
  * Fetches staked shares for all user positions using onchain calls.
@@ -25,7 +25,7 @@ type QueryResponse = {
  */
 export default function useStakedSharesQuery(
   userGaugeShares: Ref<GaugeShare[] | undefined>,
-  options: UseQueryOptions<QueryResponse> = {}
+  options: QueryOptions = {}
 ) {
   /**
    * COMPOSABLES
@@ -94,5 +94,9 @@ export default function useStakedSharesQuery(
     ...options,
   });
 
-  return useQuery<QueryResponse>(queryKey, queryFn, queryOptions);
+  return useQuery<QueryResponse>(
+    queryKey,
+    queryFn,
+    queryOptions as QueryOptions
+  );
 }
