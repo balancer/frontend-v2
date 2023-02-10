35899250784fee8e0c39466ba6d11f7c1efe764d
Alberto Gualis <alberto@balancer.finance>
2023-02-10 14:59:43 +0100

    Fix tests

--- src/composables/__mocks__/useUserPoolPercentage.ts
+++ src/composables/__mocks__/useUserPoolPercentage.ts
@@ -4,7 +4,7 @@ import BigNumber from 'bignumber.js';
 type Result = ReturnType<typeof originalUseUserPoolPercentage>;
 export function useUserPoolPercentage(): Result {
   return {
-    userPoolPercentage: computed(() => new BigNumber(0)),
+    userPoolPercentage: computed(() => new BigNumber(1.5)),
     userPoolPercentageLabel: computed(() => '0%'),
   };
 }
