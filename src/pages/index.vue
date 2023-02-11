6c1f5b27b23e1a797def074104770ae669411014
Alberto Gualis <alberto@balancer.finance>
2023-02-11 12:57:02 +0100

    Avoid pools table in index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 import HomePageHero from '@/components/heros/HomePageHero.vue';
 import TokenSearchInput from '@/components/inputs/TokenSearchInput.vue';
 import FeaturedProtocols from '@/components/sections/FeaturedProtocols.vue';
-import PoolsTable from '@/components/tables/PoolsTable/PoolsTable.vue';
 import usePoolFilters from '@/composables/pools/usePoolFilters';
 import useBreakpoints from '@/composables/useBreakpoints';
 import useNetwork from '@/composables/useNetwork';
 import useWeb3 from '@/services/web3/useWeb3';
-import usePools from '@/composables/pools/usePools';
+// import usePools from '@/composables/pools/usePools';
 
 // COMPOSABLES
 const router = useRouter();
@@ -19,16 +17,16 @@ const isElementSupported = appNetworkConfig.supportsElementPools;
 const { selectedTokens, addSelectedToken, removeSelectedToken } =
   usePoolFilters();
 
-const poolsSortField = ref('totalLiquidity');
+// const poolsSortField = ref('totalLiquidity');
 
-const { pools, isLoading, poolsIsFetchingNextPage, loadMorePools } = usePools(
-  selectedTokens,
-  poolsSortField
-);
+// const { pools, isLoading, poolsIsFetchingNextPage, loadMorePools } = usePools(
+//   selectedTokens,
+//   poolsSortField
+// );
 const { upToMediumBreakpoint } = useBreakpoints();
 const { networkSlug, networkConfig } = useNetwork();
 
-const isPaginated = computed(() => pools.value.length >= 10);
+// const isPaginated = computed(() => pools.value.length >= 10);
 
 /**
  * METHODS
@@ -37,9 +35,9 @@ function navigateToCreatePool() {
   router.push({ name: 'create-pool', params: { networkSlug } });
 }
 
-function onColumnSort(columnId: string) {
-  poolsSortField.value = columnId;
-}
+// function onColumnSort(columnId: string) {
+//   poolsSortField.value = columnId;
+// }
 </script>
 
 <template>
@@ -87,7 +85,7 @@ function onColumnSort(columnId: string) {
             </BalBtn>
           </div>
         </div>
-        <PoolsTable
+        <!-- <PoolsTable
           :data="pools"
           :noPoolsLabel="$t('noPoolsFound')"
           :isLoading="isLoading"
@@ -99,7 +97,7 @@ function onColumnSort(columnId: string) {
           skeletonClass="pools-table-loading-height"
           @on-column-sort="onColumnSort"
           @load-more="loadMorePools"
-        />
+        /> -->
         <div v-if="isElementSupported" class="p-4 xl:p-0 mt-16">
           <FeaturedProtocols />
         </div>
