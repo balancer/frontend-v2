853217055aca40cf2723c51c86bbe108b99e71a3
Matthews3301 <salamon.mat@gmail.com>
2023-02-13 20:48:54 +0000

    Incorrect class name

--- src/services/balancer/pools/weighted-pool.service.ts
+++ src/services/balancer/pools/weighted-pool.service.ts
@@ -126,7 +126,7 @@ export default class WeightedPoolService {
 
     const Multicaller = getOldMulticaller();
     const multicaller = new Multicaller(configService.network.key, provider, [
-      ...WeightedPoolV3__factory.abi,
+      ...WeightedPool__factory.abi,
       ...Vault__factory.abi,
     ]);
 
