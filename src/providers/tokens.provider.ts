c2f9dce3bec6aeb02a116c85d2c20a118e86b607
sssmi <simeon.kerkola@gmail.com>
2023-02-13 15:33:34 +0200

    Clean

--- src/providers/tokens.provider.ts
+++ src/providers/tokens.provider.ts
@@ -347,6 +347,7 @@ export const tokensProvider = (
     if (!amount || bnum(amount).eq(0)) return false;
     if (!contractAddress) return false;
     if (isSameAddress(tokenAddress, nativeAsset.address)) return false;
+
     const allowance = bnum(
       (allowances.value[contractAddress] || {})[getAddress(tokenAddress)]
     );
