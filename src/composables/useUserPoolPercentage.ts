dbb16ec7543d780d0b1b0230c02cbb4faf5fc259
Alberto Gualis <alberto@balancer.finance>
2023-02-21 15:36:12 +0100

    Hotfix/my shares calculation (#2893)
    * Only add totalLockedshares when pool is veBal
    
    * Bump version

--- src/composables/useUserPoolPercentage.ts
+++ src/composables/useUserPoolPercentage.ts
@@ -11,16 +11,21 @@ export function useUserPoolPercentage(pool: Ref<Pool>) {
   const { balanceFor } = useTokens();
   const { stakedShares } = usePoolStaking();
 
+  const isVeBal = computed(() => isVeBalPool(pool.value.id));
+
   const { totalLockedShares } = useLock({
     // Avoid lock queries when pool is not veBAL:
-    enabled: isVeBalPool(pool.value.id),
+    enabled: isVeBal.value,
   });
   const { fNum } = useNumbers();
 
   const userPoolPercentage = computed(() => {
-    const bptBalance = bnum(balanceFor(pool.value.address))
-      .plus(stakedShares.value)
-      .plus(totalLockedShares.value);
+    let bptBalance = bnum(balanceFor(pool.value.address)).plus(
+      stakedShares.value
+    );
+    if (isVeBal.value && totalLockedShares.value) {
+      bptBalance = bptBalance.plus(totalLockedShares.value);
+    }
     return bptBalance.div(bnum(pool.value.totalShares)).multipliedBy(100);
   });
 
