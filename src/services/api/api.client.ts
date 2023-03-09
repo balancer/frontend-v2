14129a62b923bd6ac52d9f93a92e475605199130
Gareth Fuller <gareth.a.f@gmail.com>
2023-03-09 12:21:02 +0000

    fix: Use env var

--- src/services/api/api.client.ts
+++ src/services/api/api.client.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from 'graphql-request';
 import { getSdk } from './graphql/generated/api-types';
 import { configService } from '../config/config.service';
 
-const graphQLClient = new GraphQLClient(configService.network.api, {
+const graphQLClient = new GraphQLClient(configService.env.API_URL, {
   headers: { ChainId: configService.network.chainId.toString() },
 });
 
