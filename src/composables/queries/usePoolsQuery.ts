f862a1eb4a1d6d74f4c745d69a3fddf7b47af471
Gareth Fuller <gareth.a.f@gmail.com>
2023-03-13 20:48:00 +0000

    chore: Fix pools list sorting with load more for subgraph mode

--- src/composables/queries/usePoolsQuery.ts
+++ src/composables/queries/usePoolsQuery.ts
@@ -21,6 +21,7 @@ import { balancerSubgraphService } from '@/services/balancer/subgraph/balancer-s
 import { balancerAPIService } from '@/services/balancer/api/balancer-api.service';
 import { poolsStoreService } from '@/services/pool/pools-store.service';
 import { isBalancerApiDefined } from '@/lib/utils/balancer/api';
+import { bnum } from '@/lib/utils';
 
 type PoolsQueryResponse = {
   pools: Pool[];
@@ -178,6 +179,26 @@ export default function usePoolsQuery(
     return fetchArgs;
   }
 
+  function customSort(pools: Pool[]): Pool[] {
+    if (poolsSortField?.value === 'totalLiquidity') return pools;
+
+    if (poolsSortField?.value === 'apr') {
+      return pools.sort((a, b) => {
+        const aprA = a?.apr?.max ?? 0;
+        const aprB = b?.apr?.max ?? 0;
+        return aprB - aprA;
+      });
+    } else if (poolsSortField?.value === 'volume') {
+      return pools.sort((a, b) => {
+        const volumeA = bnum(a?.totalSwapVolume ?? 0);
+        const volumeB = bnum(b?.totalSwapVolume ?? 0);
+        return volumeB.minus(volumeA).toNumber();
+      });
+    }
+
+    return pools;
+  }
+
   /**
    *  When filterTokens changes, re-initialize the repositories as their queries
    *  need to change to filter for those tokens
@@ -208,7 +229,8 @@ export default function usePoolsQuery(
     const fetchOptions = getFetchOptions(pageParam);
     let skip = 0;
     try {
-      const pools: Pool[] = await poolsRepository.fetch(fetchOptions);
+      let pools: Pool[] = await poolsRepository.fetch(fetchOptions);
+      if (!isBalancerApiDefined) pools = customSort(pools);
 
       poolsStoreService.addPools(pools);
 
