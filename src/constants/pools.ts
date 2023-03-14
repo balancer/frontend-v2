937c79e633b1bd97e7a2b7ca996bf85483fad9bf
zekraken <zekraken00@gmail.com>
2023-03-14 11:48:11 -0400

    add to isdeep and stable whitelist

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -571,6 +571,7 @@ const POOLS_POLYGON: Pools = {
       '0x3db543faf7a92052de7860c5c9debabee59ed5bd000000000000000000000a62', // 4usd
       '0x65fe9314be50890fb01457be076fafd05ff32b9a000000000000000000000a96', // wsteth/eth
       '0xb3d658d5b95bf04e2932370dd1ff976fe18dd66a000000000000000000000ace', // bb-t-USD (tetu managed boosted pool)
+      '0x9a020bdc2faff5bd24c6acc2020d01ff9f2c627a000000000000000000000ae2', // overnight davos usd
     ],
   },
   Investment: {
