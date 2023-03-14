5fca114be66bd06ad5b9ac18215e94510ec18985
Gareth Fuller <gareth.a.f@gmail.com>
2023-03-14 08:56:34 +0000

    chore: add bb-t-USD to isDeep list

--- src/composables/usePool.ts
+++ src/composables/usePool.ts
@@ -116,6 +116,7 @@ export function isDeep(pool: Pool): boolean {
     '0xb973ca96a3f0d61045f53255e319aedb6ed49240000200000000000000000011', // agave gno/usdc (gnosis)
     '0xf48f01dcb2cbb3ee1f6aab0e742c2d3941039d56000200000000000000000012', // agave gno/weth (gnosis)
     '0x3f7a7fd7f214be45ec26820fd01ac3be4fc75aa70002000000000000000004c5', // stg/bbeusd
+    '0xb3d658d5b95bf04e2932370dd1ff976fe18dd66a000000000000000000000ace', // bb-t-USD (tetu managed boosted pool)
   ];
 
   return treatAsDeep.includes(pool.id);
