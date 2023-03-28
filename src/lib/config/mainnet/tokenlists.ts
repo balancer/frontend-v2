879cc119304bb312519c20cffa0e82fdf733fac3
alter-eggo <anastas3993@gmail.com>
2023-03-28 14:14:15 +0400

    fix: tokens urls

--- src/lib/config/mainnet/tokenlists.ts
+++ src/lib/config/mainnet/tokenlists.ts
@@ -3,9 +3,9 @@ import { TokenListURLMap } from '@/types/TokenList';
 const tokenlists: TokenListURLMap = {
   Balancer: {
     Default:
-      'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/balancer.tokenlist.json',
-    Vetted:
       'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/listed-old.tokenlist.json',
+    Vetted:
+      'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/balancer.tokenlist.json',
   },
   External: [
     'ipns://tokens.uniswap.org',
