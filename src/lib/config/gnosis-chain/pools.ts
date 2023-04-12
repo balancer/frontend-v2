b06c40327b2bceea8e82252f0051a81f30619a6d
Gareth Fuller <gareth.a.f@gmail.com>
2023-04-12 21:26:21 +0100

    chore: Add weighted v4 factory key (#3175)
    * chore: Add weighted v4 factory key
    
    * chore: Bump version

--- src/lib/config/gnosis-chain/pools.ts
+++ src/lib/config/gnosis-chain/pools.ts
@@ -40,6 +40,7 @@ const pools: Pools = {
   Factories: {
     '0xc128468b7ce63ea702c1f104d55a2566b13d3abd': 'composableStablePool', // ComposableStable V3
     '0xc128a9954e6c874ea3d62ce62b468ba073093f25': 'weightedPool', // WeightedPool V3
+    '0x6cad2ea22bfa7f4c14aae92e47f510cd5c509bc7': 'weightedPool', // weighted pool v4
   },
   Stakable: {
     VotingGaugePools: [
