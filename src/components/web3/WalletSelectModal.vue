79402b3a0d29a7e4d79c05ec33ea5c14ec21a047
Anastasios <anastas3993@gmail.com>
2023-04-13 21:32:32 +0400

    feat: add rules checkbox on wallet select (#3182)
    * feat: add bal rules checkbox
    
    * feat: change balradio modifier
    
    * feat: add ls key

--- src/components/web3/WalletSelectModal.vue
+++ src/components/web3/WalletSelectModal.vue
@@ -4,11 +4,14 @@ import { ref } from 'vue';
 import WalletButton from '@/components/web3/WalletButton.vue';
 import { EXTERNAL_LINKS } from '@/constants/links';
 import { SupportedWallets } from '@/providers/wallet.provider';
+import LS_KEYS from '@/constants/local-storage.keys';
 
 interface Props {
   isVisible?: boolean;
   onShowThirdParty: () => void;
 }
+
+type AcceptedLocalStorageItemType = '0' | '1' | null;
 
 const props = withDefaults(defineProps<Props>(), {
   isVisible: false,
@@ -17,6 +20,19 @@ const props = withDefaults(defineProps<Props>(), {
 const emit = defineEmits(['close']);
 
 const wallets = ref(SupportedWallets.filter(id => id !== 'safe'));
+
+const acceptedlocalStorageItem = localStorage.getItem(
+  LS_KEYS.App.TermsAccepted
+) as AcceptedLocalStorageItemType;
+
+const accepted = ref<'0' | '1'>(acceptedlocalStorageItem || '0');
+
+const isBalRulesAccepted = computed(() => accepted.value === '1');
+
+function onBalRulesAccepted() {
+  accepted.value = isBalRulesAccepted.value ? '0' : '1';
+  localStorage.setItem(LS_KEYS.App.TermsAccepted, accepted.value);
+}
 </script>
 
 <template>
@@ -25,47 +41,74 @@ const wallets = ref(SupportedWallets.filter(id => id !== 'safe'));
     title="Connect to a wallet"
     @close="emit('close')"
   >
-    <p class="pb-3 text-sm">
-      {{ $t('byConnectingWallet') }}
-      <router-link :to="{ name: 'terms-of-use' }" target="_blank">
-        <span className="link">{{ $t('policies.termsOfUse') }}</span
-        >,
-      </router-link>
-      <router-link :to="{ name: 'risks' }" target="_blank">
-        <span className="link">{{ $t('policies.risks') }}</span
-        >,
-      </router-link>
-      <router-link :to="{ name: 'cookies-policy' }" target="_blank">
-        <span className="link">{{
-          $t('policies.cookiesPolicy')
-        }}</span> </router-link
-      >,
-      <span>{{ $t('useOf') }}&nbsp;</span>
-      <button @click="onShowThirdParty">
-        <BalLink>
-          <span>{{ $t('policies.thirdPartyServices') }}</span>
-        </BalLink>
-      </button>
-      {{ $t('and') }}
-      <router-link :to="{ name: 'privacy-policy' }" target="_blank">
-        <span className="link">{{ $t('policies.privacyPolicy') }}</span
-        >.
-      </router-link>
-    </p>
-    <WalletButton v-for="wallet in wallets" :key="wallet" :wallet="wallet" />
-    <div
-      class="p-4 bg-gradient-to-b from-gray-50 dark:from-gray-900 to-gray-100 dark:to-gray-850 rounded-lg"
+    <BalRadio
+      :checked="isBalRulesAccepted"
+      value="bal-rules"
+      name="bal-rules"
+      size="lg"
+      @update:model-value="onBalRulesAccepted"
     >
-      <h6>{{ $t('newToEthereum') }}</h6>
-      <p class="text-sm">
-        {{ $t('setUpEthereumWallet') }}
-        <BalLink :href="EXTERNAL_LINKS.Ethereum.Wallets" external>
-          {{ $t('learnMore') }}
-          <span class="align-middle"
-            ><BalIcon name="arrow-up-right" size="sm"
-          /></span>
-        </BalLink>
-      </p>
+      <template #label>
+        <p class="pb-3 pl-1 -mt-1 mb-2 text-base">
+          {{ $t('byConnectingWallet') }}
+          <router-link
+            :to="{ name: 'terms-of-use' }"
+            target="_blank"
+            @click.stop=""
+          >
+            <span className="link">{{ $t('policies.termsOfUse') }} </span>,
+          </router-link>
+          <router-link :to="{ name: 'risks' }" target="_blank" @click.stop="">
+            <span className="link">{{ $t('policies.risks') }} </span>,
+          </router-link>
+          <router-link
+            :to="{ name: 'cookies-policy' }"
+            target="_blank"
+            @click.stop=""
+          >
+            <span className="link">
+              {{ $t('policies.cookiesPolicy') }}
+            </span> </router-link
+          >,
+          <span>{{ $t('useOf') }}&nbsp;</span>
+          <button @click.stop="onShowThirdParty">
+            <BalLink>
+              <span>{{ $t('policies.thirdPartyServices') }}</span>
+            </BalLink>
+          </button>
+          {{ $t('and') }}
+          <router-link
+            :to="{ name: 'privacy-policy' }"
+            target="_blank"
+            @click.stop=""
+          >
+            <span className="link">{{ $t('policies.privacyPolicy') }} </span>.
+          </router-link>
+        </p>
+      </template>
+    </BalRadio>
+
+    <div
+      :class="[
+        !isBalRulesAccepted && 'grayscale pointer-events-none opacity-20',
+        'transition-opacity duration-200',
+      ]"
+    >
+      <WalletButton v-for="wallet in wallets" :key="wallet" :wallet="wallet" />
+      <div
+        class="p-4 bg-gradient-to-b from-gray-50 dark:from-gray-900 to-gray-100 dark:to-gray-850 rounded-lg"
+      >
+        <h6>{{ $t('newToEthereum') }}</h6>
+        <p class="text-sm">
+          {{ $t('setUpEthereumWallet') }}
+          <BalLink :href="EXTERNAL_LINKS.Ethereum.Wallets" external>
+            {{ $t('learnMore') }}
+            <span class="align-middle">
+              <BalIcon name="arrow-up-right" size="sm" />
+            </span>
+          </BalLink>
+        </p>
+      </div>
     </div>
   </BalModal>
 </template>
