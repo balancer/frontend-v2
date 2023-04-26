a6a36e394ecabdbbb760706bc8379a0d8699bac7
Alberto Gualis <alberto@balancer.finance>
2023-04-26 15:28:47 +0200

    Add new exact-in join handler (#3237)

---
+++ src/services/balancer/pools/joins/handlers/exact-in-join.handler.spec.ts
@@ -0,0 +1,70 @@
+import { getBalancerSDK } from '@/dependencies/balancer-sdk';
+import {
+  defaultExactInJoin,
+  initBalancerSdkWithDefaultMocks,
+} from '@/dependencies/balancer-sdk.mocks';
+import { GasPriceService } from '@/services/gas-price/gas-price.service';
+import { Pool } from '@/services/pool/types';
+import { aTokenInfo } from '@/types/TokenList.builders';
+import { aWeightedPool } from '@/__mocks__/weighted-pool';
+import { groAddress, wethAddress } from '@tests/unit/builders/address';
+import {
+  anAmountIn,
+  buildJoinParams,
+} from '@tests/unit/builders/join-exit.builders';
+import {
+  defaultGasLimit,
+  defaultTransactionResponse,
+} from '@tests/unit/builders/signer';
+import { DeepMockProxy, mockDeep } from 'vitest-mock-extended';
+import { ExactInJoinHandler } from './exact-in-join.handler';
+import { JoinParams } from './join-pool.handler';
+
+initBalancerSdkWithDefaultMocks();
+
+const gasPriceServiceMock: DeepMockProxy<GasPriceService> =
+  mockDeep<GasPriceService>();
+
+async function mountExactInJoinHandler(pool: Pool) {
+  return new ExactInJoinHandler(
+    ref(pool),
+    getBalancerSDK(),
+    gasPriceServiceMock
+  );
+}
+
+const joinParams: JoinParams = buildJoinParams({
+  amountsIn: [
+    anAmountIn({ address: groAddress, value: '20' }),
+    anAmountIn({ address: wethAddress, value: '20' }),
+  ],
+  tokensIn: {
+    [groAddress]: aTokenInfo({ address: groAddress }),
+    [wethAddress]: aTokenInfo({ address: wethAddress }),
+  },
+});
+
+test('Successfully queries an Exact In join', async () => {
+  const handler = await mountExactInJoinHandler(aWeightedPool());
+
+  const queryOutput = await handler.queryJoin(joinParams);
+
+  expect(queryOutput).toEqual({
+    bptOut: '0.00000000000000003', // formatFixed defaultExpectedBptOut
+    priceImpact: 1, // Number(formatFixed(evmPriceImpact, 18)) where evmPriceImpact is defaultPriceImpact
+  });
+});
+
+test('Successfully executes an exact in join transaction', async () => {
+  const handler = await mountExactInJoinHandler(aWeightedPool());
+  const joinResult = await handler.join(joinParams);
+
+  expect(joinResult).toEqual(defaultTransactionResponse);
+
+  expect(joinParams.signer.sendTransaction).toHaveBeenCalledOnceWith({
+    data: defaultExactInJoin.data,
+    to: defaultExactInJoin.to,
+    value: defaultExactInJoin.value,
+    gasLimit: defaultGasLimit,
+  });
+});
