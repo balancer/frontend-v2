68ab2ed64f5ce8f8f4b968338ef9f58419691d68
Gareth Fuller <gareth.a.f@gmail.com>
2023-05-04 16:22:19 +0100

    chore: Add kacy-weth to allowlist

--- src/lib/config/polygon/pools.ts
+++ src/lib/config/polygon/pools.ts
@@ -103,6 +103,7 @@ const pools: Pools = {
       '0x3efb91c4f9b103ee45885695c67794591916f34e000200000000000000000b43', // bb-am-usd/2brl
       '0x7f4f4942f2a14b6ab7b08b10ada1aacede4ee8d4000200000000000000000b44', // stmatci-bb-wmatic/bb-am-usd
       '0x8fd39252d683fdb60bddd4df4b53c9380b496d59000200000000000000000b45', // wsteth-bb-a-weth/bb-am-usd
+      '0xfaf3bc722d34146be83a2aac40b43148a51a9126000200000000000000000b4c', // 50kacy/50weth
     ],
   },
   Factories: {
