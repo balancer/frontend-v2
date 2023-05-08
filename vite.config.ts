0e30d2880e02cd5f1fce26487ce35e150a2235fe
Alberto Gualis <agualis@gmail.com>
2023-05-08 18:26:25 +0200

    Set max chunk size to 750kB

--- vite.config.ts
+++ vite.config.ts
@@ -139,7 +139,7 @@ export default defineConfig(({ mode }) => {
           rollupPolyfillNode(),
         ],
       },
-      chunkSizeWarningLimit: 700,
+      chunkSizeWarningLimit: 750,
       commonjsOptions: {
         // Allows to import tailwind.config.js from useTailwind.ts
         // Check: https://github.com/tailwindlabs/tailwindcss.com/issues/765
