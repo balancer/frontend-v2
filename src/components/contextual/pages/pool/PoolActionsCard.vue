d82414c79d3b4f1bc403ca37282300e2990a5013
Gareth Fuller <gareth.a.f@gmail.com>
2023-05-19 16:51:02 +0100

    chore: Revert to anchor link

--- src/components/contextual/pages/pool/PoolActionsCard.vue
+++ src/components/contextual/pages/pool/PoolActionsCard.vue
@@ -92,11 +92,12 @@ const joinDisabled = computed(
       <div class="pt-4 text-xs text-secondary">
         {{ $t('poolTransfer.myPoolBalancesCard.risksDisclaimer') }}
 
-        <router-link to="/risks"
+        <a href="#risks-section"
           ><span class="font-medium link">{{
             $t('poolTransfer.myPoolBalancesCard.poolsRisks')
-          }}</span></router-link
-        >.
+          }}</span></a
+        >
+        .
       </div>
     </div>
   </div>
--- src/components/contextual/pages/pool/risks/PoolRisks.vue
+++ src/components/contextual/pages/pool/risks/PoolRisks.vue
@@ -16,7 +16,7 @@ defineProps<Props>();
 </script>
 
 <template>
-  <div id="risks">
+  <div id="risks-section">
     <h3 class="px-4 lg:px-0 mb-5" v-text="$t('poolRisks.title')" />
 
     <p v-if="poolSpecificRisks(pool)" class="px-4 lg:px-0 mb-5">
