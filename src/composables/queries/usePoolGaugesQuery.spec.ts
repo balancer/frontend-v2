5b181230016441a4db23bf2c60ca6af5d3c8f37c
Alberto Gualis <alberto@balancer.finance>
2023-05-24 10:24:05 +0200

    Refactor gauge mocks (#3423)
    * Refactor gauge mocks

---
+++ src/composables/queries/usePoolGaugesQuery.spec.ts
@@ -0,0 +1,21 @@
+import { aWeightedPool } from '@/__mocks__/weighted-pool';
+import usePoolGaugesQuery from '@/composables/queries/usePoolGaugesQuery';
+import { defaultPreferentialGaugeAddress } from '@/services/balancer/gauges/__mocks__/gauge-mocks';
+import {
+  mountComposableWithDefaultTokensProvider as mountComposable,
+  waitForQueryData,
+} from '@tests/mount-helpers';
+
+test('Returns pool gauges', async () => {
+  const pool = aWeightedPool();
+
+  const { result } = mountComposable(() =>
+    usePoolGaugesQuery(ref(pool.address))
+  );
+
+  const data = await waitForQueryData(result);
+
+  expect(data?.pool.preferentialGauge.id).toEqual(
+    defaultPreferentialGaugeAddress
+  );
+});
