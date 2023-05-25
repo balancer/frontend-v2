b2dc95585a43246febe4bbd8846d0f9a0f015732
Tim Robinson <tim@timjrobinson.com>
2023-05-25 17:26:03 +1000

    Make script work with ts files. Add ability to set description

--- src/lib/scripts/allowlist-pool.ts
+++ src/lib/scripts/allowlist-pool.ts
@@ -13,27 +13,32 @@ const path = require('path');
 
 cli
   .option('--network <network>', 'Choose a network')
-  .option('--poolType <poolType>', 'Choose a network')
-  .option('--poolId <poolId>', 'Choose a network');
+  .option('--poolType <poolType>', 'Pool Type')
+  .option('--poolId <poolId>', 'Pool ID')
+  .option('--poolDescription <poolDescription>', 'Pool Description');
 
 const {
-  options: { network, poolType, poolId },
+  options: { network, poolType, poolId, poolDescription },
 } = cli.parse();
 
-const _poolId = poolId.replace(/"/g, '');
+const _poolId = poolId.replace(/[^0-9a-fA-Fx]+/g, '');
+const description = poolDescription ? ` // ${poolDescription}` : '';
 
-console.log('Injecting:', network, poolType, _poolId);
+console.log('Injecting:', network, poolType, _poolId, poolDescription);
 
 const allowlistPath = path.resolve(
   __dirname,
-  `../config/${network}/allowlists/${poolType}.json`
+  `../config/${network}/allowlists/${poolType}.ts`
 );
-const allowlist = require(allowlistPath);
+const allowlistText = fs.readFileSync(allowlistPath, 'utf-8');
+let allowlist = allowlistText.split('\n');
 
-allowlist.push(_poolId);
+const closingLines = allowlist.splice(-2, 2);
+allowlist.push(`  '${_poolId}',${description}`);
+allowlist = allowlist.concat(closingLines);
 
 try {
-  fs.writeFileSync(allowlistPath, JSON.stringify(allowlist, null, 2));
+  fs.writeFileSync(allowlistPath, allowlist.join('\n'));
 } catch (error) {
   console.error('Failed to write allowlist file.', { cause: error });
 }
