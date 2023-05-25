783339f37460442015af61b98c1dd2df3e188e5d
Tim Robinson <timjrobinson@users.noreply.github.com>
2023-05-25 15:12:01 +1000

    Whitelist PAXG pool (#3442)
    * Whitelist PAXG 4pool
    
    * 1.102.13

--- src/lib/config/polygon/pools.ts
+++ src/lib/config/polygon/pools.ts
@@ -108,6 +108,7 @@ const pools: Pools = {
       '0xfaf3bc722d34146be83a2aac40b43148a51a9126000200000000000000000b4c', // 50kacy/50weth
       '0xbadcdaf6212b0bacf45cc32447cb615f85fe6adf000100000000000000000b87', // 50weth/50wsteth/50nex
       '0xb1cc24df1d3d2c6cb798b1129ddaae9e29d437c0000100000000000000000b8d', // staking defi pool
+      '0x9841438683c2efbfb28c94ec341544b94e4f6dd5000100000000000000000b8f', // WBTC/USDC/PAXG/WETH
     ],
   },
   Factories: {
