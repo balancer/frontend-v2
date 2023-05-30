2c0e87c214277b778275df0832f608595f169375
alter-eggo <anastas3993@gmail.com>
2023-05-30 11:56:30 +0400

    fix: loader lint

--- src/components/contextual/pages/vebal/cross-chain-boost/IconLoaderWrapper.vue
+++ src/components/contextual/pages/vebal/cross-chain-boost/IconLoaderWrapper.vue
@@ -15,7 +15,7 @@ withDefaults(defineProps<Props>(), {
   </div>
 </template>
 
-<style>
+<style scoped>
 .loader {
   position: absolute;
   top: -2px;
@@ -27,23 +27,14 @@ withDefaults(defineProps<Props>(), {
   border-bottom: 2px solid #f97316;
   width: 38px;
   height: 38px;
-  -webkit-animation: spin 2s linear infinite;
   animation: spin 2s linear infinite;
-}
-
-@-webkit-keyframes spin {
-  0% {
-    -webkit-transform: rotate(0deg);
-  }
-  100% {
-    -webkit-transform: rotate(360deg);
-  }
 }
 
 @keyframes spin {
   0% {
     transform: rotate(0deg);
   }
+
   100% {
     transform: rotate(360deg);
   }
