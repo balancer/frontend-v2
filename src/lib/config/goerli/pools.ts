f889aa0e4afd9354aa5f60ccc4402c2ec62d130b
Gareth Fuller <gareth.a.f@gmail.com>
2023-06-14 09:27:49 +0100

    hotifx: Add bb-a-USD v3 as claimable token from FeeDistributor (#3526)
    * chore: Add bbausd v3 pool ID to config
    
    * chore: Add bbausd v3 as claimable token from FeeDistributor
    
    * chore: Fix typecheck

--- src/lib/config/goerli/pools.ts
+++ src/lib/config/goerli/pools.ts
@@ -26,6 +26,7 @@ const pools: Pools = {
     bbAaveUSD: {
       v1: '0x13acd41c585d7ebb4a9460f7c8f50be60dc080cd00000000000000000000005f',
       v2: '0x3d5981bdd8d3e49eb7bbdc1d2b156a3ee019c18e0000000000000000000001a7',
+      v3: '',
     },
     veBAL: '0xf8a0623ab66f985effc1c69d05f1af4badb01b00000200000000000000000060',
   },
