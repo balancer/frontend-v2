4adbe639f371b35c5817469608dbc79427903c9e
Gareth Fuller <gareth.a.f@gmail.com>
2023-07-06 14:10:59 +0100

    chore: Igrnore specifc sentry error

--- src/plugins/sentry.ts
+++ src/plugins/sentry.ts
@@ -42,6 +42,7 @@ export default function initSentry(app: App) {
       tracesSampleRate: 1.0,
       environment: import.meta.env.MODE,
       release: `frontend-v2@${version}`,
+      ignoreErrors: ['error loading dynamically imported module'], // https://docs.sentry.io/platforms/javascript/configuration/filtering/#using-platformidentifier-nameignore-errors-
     });
   }
 }
