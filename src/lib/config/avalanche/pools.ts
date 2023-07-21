5398d12883fc53e473e29ca9d85a400583a21bcc
ZeKraken <79888567+zekraken-bot@users.noreply.github.com>
2023-07-20 21:13:28 -0400

    whitelist initial boosted pools

--- src/lib/config/avalanche/pools.ts
+++ src/lib/config/avalanche/pools.ts
@@ -1,3 +1,4 @@
+import { BoostedProtocol } from '@/composables/useBoostedPool';
 import { Pools } from '@/types/pools';
 
 const pools: Pools = {
@@ -16,7 +17,13 @@ const pools: Pools = {
   BlockList: [''],
   IncludedPoolTypes: ['Weighted', 'Stable', 'MetaStable'],
   Stable: {
-    AllowList: [],
+    AllowList: [
+      '0xa1d14d922a575232066520eda11e27760946c991000000000000000000000012', // Boosted Aave v3 USD
+      '0xa154009870e9b6431305f19b09f9cfd7284d4e7a000000000000000000000013', // sAVAX/Boosted Aave v3 WAVAX
+      '0xece571847897fd61e764d455dc15cf1cd9de8d6f000000000000000000000014', // yyAVAX/Boosted Aave v3 WAVAX
+      '0x054e7b0c73e1ee5aed6864fa511658fc2b54bcaa000000000000000000000015', // ggAVAX/Boosted Aave v3 WAVAX
+      '0x670734d704eb7ef424d75321670f921fbf42e9cf000000000000000000000016', // sAVAX-ankrAVAX
+    ],
   },
   Investment: {
     AllowList: [''],
@@ -27,6 +34,7 @@ const pools: Pools = {
     AllowList: [
       '0xe4a4565ad31a3af8286bc6e6dbb20ba76752557700010000000000000000000b',
       '0xbf7a12046f7f5a9403fe2f75e1e5c531da3cf55300020000000000000000000c',
+      '0x3bde1563903ebb564ca37d5736afbb850929cfd7000200000000000000000017', // ankrAVAX-ankrETH
     ],
   },
   Factories: {},
@@ -34,8 +42,38 @@ const pools: Pools = {
     VotingGaugePools: [],
     AllowList: [],
   },
-  Metadata: {},
-  Deep: [],
+  Metadata: {
+    '0xa1d14d922a575232066520eda11e27760946c991000000000000000000000012': {
+      name: 'Balancer Boosted Aave v3 USD',
+      hasIcon: false,
+      boosted: true,
+      boostedProtocols: [BoostedProtocol.Aave],
+    },
+    '0xa154009870e9b6431305f19b09f9cfd7284d4e7a000000000000000000000013': {
+      name: 'sAVAX/Boosted Aave v3 WAVAX',
+      hasIcon: false,
+      boosted: true,
+      boostedProtocols: [BoostedProtocol.Aave],
+    },
+    '0xece571847897fd61e764d455dc15cf1cd9de8d6f000000000000000000000014': {
+      name: 'yyAVAX/Boosted Aave v3 WAVAX',
+      hasIcon: false,
+      boosted: true,
+      boostedProtocols: [BoostedProtocol.Aave],
+    },
+    '0x054e7b0c73e1ee5aed6864fa511658fc2b54bcaa000000000000000000000015': {
+      name: 'ggAVAX/Boosted Aave v3 WAVAX',
+      hasIcon: false,
+      boosted: true,
+      boostedProtocols: [BoostedProtocol.Aave],
+    },
+  },
+  Deep: [
+    '0xa1d14d922a575232066520eda11e27760946c991000000000000000000000012', // Boosted Aave v3 USD
+    '0xa154009870e9b6431305f19b09f9cfd7284d4e7a000000000000000000000013', // sAVAX/Boosted Aave v3 WAVAX
+    '0xece571847897fd61e764d455dc15cf1cd9de8d6f000000000000000000000014', // yyAVAX/Boosted Aave v3 WAVAX
+    '0x054e7b0c73e1ee5aed6864fa511658fc2b54bcaa000000000000000000000015', // ggAVAX/Boosted Aave v3 WAVAX
+  ],
   BoostedApr: [],
   DisabledJoins: [],
   Deprecated: {},
