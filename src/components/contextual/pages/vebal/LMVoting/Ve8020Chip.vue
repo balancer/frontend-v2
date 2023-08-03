9cb3ed62bc88de8578586249655a814b6dab52fd
Alberto Gualis <agualis@gmail.com>
2023-08-03 17:12:01 +0200

    Change ve8020 chip gradient color

--- src/components/contextual/pages/vebal/LMVoting/Ve8020Chip.vue
+++ src/components/contextual/pages/vebal/LMVoting/Ve8020Chip.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex relative items-center py-1 pr-1.5 pl-2 mr-1 max-h-10 text-white bg-gradient-to-tr from-yellow-500 to-pink-500 rounded"
+    class="flex relative items-center py-1 pr-1.5 pl-2 mr-1 max-h-10 text-white bg-gradient-to-tr rounded from-[#977622] to-[#f0cb6d]"
   >
     <span class="text-xs font-semibold">ve8020</span>
   </div>
