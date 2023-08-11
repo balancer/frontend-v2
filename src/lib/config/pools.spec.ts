9340c0b1d091e986ddb989730988ece67b2b1e8b
Gareth Fuller <gareth.a.f@gmail.com>
2023-08-11 10:39:12 +0100

    fix: Limit swap exits (DM) (#3946)
    * Fix ethereum pool links in auto allowlist PRs
    
    * Add test for detecting duplicates in voting list
    
    * Change test name
    
    * chore: Add migration notices (#3934)
    
    * 1.118.1
    
    * hotfix: fix reward tokens apr breakdown (#3936)
    
    * 1.118.2
    
    * chore: Add farmDE pool to polygon allowlist
    
    * add well and stg to whitelist (#3939)
    
    * 1.118.3
    
    * chore: Add 2BTC pool to mainnet allowlist
    
    * Add reth pool to allowlist
    
    * 1.118.4
    
    * chore: Add 80TBTC-20WBTC pool to mainnet allowlist
    
    * 1.118.5
    
    * 1.118.6
    
    * 1.118.7
    
    * 1.118.8
    
    * 1.118.9
    
    * fix: Limit swap exits to deep pools
    
    * chore: Add comment
    
    ---------
    
    Co-authored-by: Alberto Gualis <agualis@gmail.com>
    Co-authored-by: Tim Robinson <timjrobinson@users.noreply.github.com>
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
    Co-authored-by: Fábio Mendes <43360747+mendesfabio@users.noreply.github.com>
    Co-authored-by: balopco <balopco@users.noreply.github.com>
    Co-authored-by: ZeKraken <79888567+zekraken-bot@users.noreply.github.com>
    Co-authored-by: Tim Robinson <tim@timjrobinson.com>

--- src/lib/config/pools.spec.ts
+++ src/lib/config/pools.spec.ts
@@ -15,29 +15,51 @@ function detectDuplicates(
   const duplicatedPoolAddresses = getDuplicatedAddresses(addresses);
   if (duplicatedPoolAddresses.length > 0)
     throw new Error(
-      `Network ${networkName}: found duplicated pool addresses in ${allowlistType} Allowlist: ${duplicatedPoolAddresses}.
+      `Network ${networkName}: found duplicated pool addresses in ${allowlistType} Allowlist: ${duplicatedPoolAddresses}
       Review /src/lib/config/${networkName}/pools.ts to remove duplicates.
       It could be that 2 similar allowlist PRs were created at the same time.
       `
     );
 }
 
-test('Detect duplicated pool addresses in allowlists', async () => {
+function detectDuplicatesInVotingList(
+  networkName: string,
+  addresses: string[]
+) {
+  const duplicatedPoolAddresses = getDuplicatedAddresses(addresses);
+  if (duplicatedPoolAddresses.length > 0)
+    throw new Error(
+      `Network ${networkName}: found duplicated pool addresses in Stakable.VotingListPools: ${duplicatedPoolAddresses}
+      Review /src/lib/config/${networkName}/pools.ts to remove duplicates.
+      `
+    );
+}
+
+test('Detect duplicated pool addresses in Staking.VotingGaugePools addresses', async () => {
   Object.values(config).forEach(networkConfig => {
     detectDuplicates(
       networkConfig.network,
       'Stable',
-      networkConfig.pools['Stable'].AllowList
+      networkConfig.pools.Stable.AllowList
     );
     detectDuplicates(
       networkConfig.network,
       'Weighted',
-      networkConfig.pools['Weighted'].AllowList
+      networkConfig.pools.Weighted.AllowList
     );
     detectDuplicates(
       networkConfig.network,
       'Investment',
-      networkConfig.pools['Investment'].AllowList
+      networkConfig.pools.Investment.AllowList
+    );
+  });
+});
+
+test('Detect duplicated pool addresses in voting list', async () => {
+  Object.values(config).forEach(networkConfig => {
+    detectDuplicatesInVotingList(
+      networkConfig.network,
+      networkConfig.pools.Stakable.VotingGaugePools
     );
   });
 });
