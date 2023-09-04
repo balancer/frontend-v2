a380b7e32944d24450fb04ef3865193549b0e03e
Tim Robinson <tim@timjrobinson.com>
2023-09-04 22:59:25 +1000

    Switching AVAX native asset to 0xEee to be consistent with backend

--- src/lib/config/avalanche/tokens.ts
+++ src/lib/config/avalanche/tokens.ts
@@ -5,11 +5,11 @@ const tokens: TokenConstants = {
     Symbols: ['WBTC', 'WETH', 'DAI', 'USDC', 'BAL', 'AVAX'],
   },
   InitialSwapTokens: {
-    input: '0x0000000000000000000000000000000000000000',
+    input: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
     output: '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E', // USDC
   },
   Addresses: {
-    nativeAsset: '0x0000000000000000000000000000000000000000',
+    nativeAsset: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
     wNativeAsset: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
     WETH: '0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB',
     BAL: '0xE15bCB9E0EA69e6aB9FA080c4c4A5632896298C3',
