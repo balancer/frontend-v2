80fbd0d45464b3f3f35e887153e00211f4c72d98
Alberto Gualis <agualis@gmail.com>
2023-09-04 11:34:39 +0200

    Remove lsget from useDarkMode

--- src/composables/useDarkMode.ts
+++ src/composables/useDarkMode.ts
@@ -1,28 +1,26 @@
-import { ref } from 'vue';
+// import { lsGet, lsSet } from '@/lib/utils/localstorage';
 
-import { lsGet, lsSet } from '@/lib/utils/localstorage';
+// const DarkMode = 'app.darkMode';
 
-const DarkMode = 'app.darkMode';
-
-const osDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-const lsDarkMode = lsGet(DarkMode, osDarkMode.toString()) === 'true';
+// const osDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+// const lsDarkMode = lsGet(DarkMode, osDarkMode.toString()) === 'true';
 
 // STATE
-const darkMode = ref<boolean>(lsDarkMode);
+const darkMode = { value: false };
 
-// MUTATIONS
+// // MUTATIONS
 function setDarkMode(val: boolean): void {
   darkMode.value = val;
-  lsSet(DarkMode, darkMode.value.toString());
-  if (darkMode.value) {
-    document.documentElement.classList.add('dark');
-  } else {
-    document.documentElement.classList.remove('dark');
-  }
+  // lsSet(DarkMode, darkMode.value.toString());
+  // if (darkMode.value) {
+  //   document.documentElement.classList.add('dark');
+  // } else {
+  //   document.documentElement.classList.remove('dark');
+  // }
 }
 
-// INIT
-setDarkMode(darkMode.value);
+// // INIT
+// setDarkMode(darkMode.value);
 
 export default function useDarkMode() {
   function toggleDarkMode(): void {
