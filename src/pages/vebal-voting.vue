0569bbec26e066e6f939eb6e657caeb7eabe901c
Alberto Gualis <alberto@balancer.finance>
2023-09-08 12:33:11 +0200

    Register vebal-voting route

---
+++ src/pages/vebal-voting.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MultiVoting from '@/components/contextual/pages/vebal/LMVoting/MultiVoting.vue';
+import { provideVoting } from '@/providers/local/voting.provider';
+
+provideVoting();
+</script>
+
+<template>
+  <MultiVoting />
+</template>
+
