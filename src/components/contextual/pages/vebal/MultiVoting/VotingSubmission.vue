7a2ae2e7d0e23e9220b001055a90aa7c4d526f78
Alberto Gualis <alberto@balancer.finance>
2023-09-08 13:38:08 +0200

    Add components to handle lists of voting inputs

---
+++ src/components/contextual/pages/vebal/MultiVoting/VotingSubmission.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+/**
+ * Shows a read-only list of votes once the user has clicked "Next" after filling the Vote Inputs
+ */
+import VotingActions from '@/components/contextual/pages/vebal/LMVoting/VotingActions.vue';
+import VoteAllocation from './VoteAllocation.vue';
+import ConfirmedVote from './ConfirmedVote.vue';
+import { useVoting } from './composables/voting.provider';
+
+const {
+  unlockedSelectedPools,
+  totalAllocatedWeight,
+  expiredGauges,
+  confirmedVotingRequest,
+  shouldBatchVotes,
+} = useVoting();
+</script>
+
+<template>
+  <div>
+    <BalAlert
+      v-if="shouldBatchVotes"
+      type="tip"
+      class="mb-3"
+      title="Your votes have been batched"
+      description="The protocol only supports up to 8 votes per transaction, so additional votes will be confirmed afterwards."
+    />
+
+    <ConfirmedVote
+      v-for="pool in unlockedSelectedPools"
+      :key="pool.gauge.address"
+      :pool="pool"
+    />
+
+    <VoteAllocation
+      message="Total vote allocation"
+      :percentage="totalAllocatedWeight"
+    ></VoteAllocation>
+
+    <VotingActions
+      :request="confirmedVotingRequest"
+      :expiredGauges="expiredGauges"
+    />
+  </div>
+</template>
