01f30bf89d3946878e442807c3e299a569e58719
Alberto Gualis <alberto@balancer.finance>
2023-10-13 17:12:23 +0200

    Add rate provider for 80OVN-20wUSD+ (#4353)

--- src/lib/config/arbitrum/rateProviders.ts
+++ src/lib/config/arbitrum/rateProviders.ts
@@ -10,6 +10,9 @@ const rateProviders: RateProviders = {
   '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0': {
     '0xf7c5c26b574063e7b098ed74fad6779e65e3f836': true,
   },
+  '0xb86fb1047a955c0186c77ff6263819b37b32440d': {
+    '0x2ba447d4b823338435057571bf70907f8224bb47': true,
+  },
 };
 
 export default rateProviders;
