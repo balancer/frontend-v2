80e7a652889385437b6a2f81b3b607be4db39439
ZeKraken <79888567+zekraken-bot@users.noreply.github.com>
2023-11-07 09:39:48 -0500

    add reth rate provider to OP

--- src/lib/config/optimism/rateProviders.ts
+++ src/lib/config/optimism/rateProviders.ts
@@ -4,6 +4,9 @@ const rateProviders: RateProviders = {
   '*': {
     '0x0000000000000000000000000000000000000000': true,
   },
+  '0x9Bcef72be871e61ED4fBbc7630889beE758eb81D': {
+    '0x658843BB859B7b85cEAb5cF77167e3F0a78dFE7f': true,
+  },
 };
 
 export default rateProviders;
