48c0cd0708218ef002dc378cdee72bf90a7d0c4f
Alberto Gualis <agualis@gmail.com>
2023-12-01 12:28:23 +0100

    Alias GqlPoolType to prepare for type breaking change

--- src/services/api/graphql/mappers.ts
+++ src/services/api/graphql/mappers.ts
@@ -1,6 +1,9 @@
 import { Network } from '@/lib/config/types';
 import { PoolType } from '@/services/pool/types';
-import { GqlChain, GqlPoolMinimalType } from './generated/api-types';
+import {
+  GqlChain,
+  GqlPoolMinimalType as GqlPoolType,
+} from './generated/api-types';
 
 export function mapApiChain(
   apiChain: GqlChain | 'SEPOLIA' | 'GOERLI'
@@ -34,29 +37,27 @@ export function mapApiChain(
   }
 }
 
-export function mapApiPoolType(
-  apiPoolType: GqlPoolMinimalType
-): PoolType | null {
+export function mapApiPoolType(apiPoolType: GqlPoolType): PoolType | null {
   switch (apiPoolType) {
-    case GqlPoolMinimalType.Element:
+    case GqlPoolType.Element:
       return PoolType.Element;
-    case GqlPoolMinimalType.Gyro3:
+    case GqlPoolType.Gyro3:
       return PoolType.Gyro3;
-    case GqlPoolMinimalType.Gyroe:
+    case GqlPoolType.Gyroe:
       return PoolType.GyroE;
-    case GqlPoolMinimalType.Investment:
+    case GqlPoolType.Investment:
       return PoolType.Investment;
-    case GqlPoolMinimalType.Linear:
+    case GqlPoolType.Linear:
       return PoolType.Linear;
-    case GqlPoolMinimalType.LiquidityBootstrapping:
+    case GqlPoolType.LiquidityBootstrapping:
       return PoolType.LiquidityBootstrapping;
-    case GqlPoolMinimalType.MetaStable:
+    case GqlPoolType.MetaStable:
       return PoolType.MetaStable;
-    case GqlPoolMinimalType.PhantomStable:
+    case GqlPoolType.PhantomStable:
       return PoolType.StablePhantom;
-    case GqlPoolMinimalType.Stable:
+    case GqlPoolType.Stable:
       return PoolType.Stable;
-    case GqlPoolMinimalType.Weighted:
+    case GqlPoolType.Weighted:
       return PoolType.Weighted;
     default:
       return null;
