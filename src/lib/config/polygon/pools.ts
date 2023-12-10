accbc3f01efe090ed0e349d940fa4346add79825
balopco <balopco@users.noreply.github.com>
2023-12-10 07:23:36 +0000

    chore: Add 16oRETRO-84STARV3/StarSeeds_Protocol pool to polygon allowlist

--- src/lib/config/polygon/pools.ts
+++ src/lib/config/polygon/pools.ts
@@ -349,6 +349,7 @@ const pools: Pools = {
       '0x30aeac2ed5dcb59ed02c1218f88924ef1309d754000200000000000000000d17', // 80mooCompoundPolygonUSDC-20ICHI_Vault_LP
       '0x4e8c97baa6777ae00ed3fe1acb4dd54fb8fe19c5000200000000000000000d18', // 80WBTC-20ICHI_Vault_LP
       '0xda1503426f6cd9ebbab2964106f42b35c745d453000100000000000000000d19', // 30stMATIC-10SP-60STARV2/StarSeeds_Protocol
+      '0x3c4c55db89b1411d978a4dfb0fe563aaa91e9cac000200000000000000000d28', // 16oRETRO-84STARV3/StarSeeds_Protocol
     ],
   },
   Factories: {
