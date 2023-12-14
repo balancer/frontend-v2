0cbb2137b8ffd23362271d0ae42a1e4a289e52ef
Alberto Gualis <agualis@gmail.com>
2023-12-14 14:12:06 +0100

    Add red alert

---
+++ src/pages/RedAlert.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+/**
+ * COMPOSABLES
+ */
+</script>
+
+<template>
+  <div v-if="true" class="px-4 xl:px-0">
+    <BalAlert
+      type="error"
+      title="Warning - December 14, 2023"
+      class="mb-4"
+      block
+      >A potential vulnerability has been detected with Ledger Connect. We
+      advise all users to exercise heightened caution and avoid using the
+      Balancer frontend until further information is available.</BalAlert
+    >
+  </div>
+</template>
