b4a03d0acadb664309a37a670c068796010343e5
ZeKraken <79888567+zekraken-bot@users.noreply.github.com>
2024-01-19 21:58:31 -0500

    Update rateProviders.ts

--- src/lib/config/zkevm/rateProviders.ts
+++ src/lib/config/zkevm/rateProviders.ts
@@ -5,6 +5,9 @@ const rateProviders: RateProviders = {
   '*': {
     '0x0000000000000000000000000000000000000000': true,
   },
+  '0x8C7D118B5c47a5BCBD47cc51789558B98dAD17c5': {
+    '0x4186BFC76E2E237523CBC30FD220FE055156b41F': true,
+  },
 };
 
 export default convertKeysToLowerCase(rateProviders);
