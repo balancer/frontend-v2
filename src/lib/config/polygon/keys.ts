c95834cc81fb6532cf0938b4273b36fbcef3db2c
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-12 15:32:01 +0000

    chore: Update polygon alchemy key (#5409)

--- src/lib/config/polygon/keys.ts
+++ src/lib/config/polygon/keys.ts
@@ -2,7 +2,7 @@ import { Keys } from '../types';
 
 const keys: Keys = {
   infura: 'daaa68ec242643719749dd1caba2fc66',
-  alchemy: 'ODJ9G5Ipv-Hb2zTWMNbUFIqv9WtqBOc2',
+  alchemy: 'UrVAqAyukeLVl8hhc29tLtegSoWL9Iuy',
 };
 
 export default keys;
