530912d2633d0442ed6aabf9e30f2921683e24df
Gareth Fuller <gareth.a.f@gmail.com>
2024-03-12 15:21:23 +0000

    chore: Update polygon alchemy key

--- src/lib/config/avalanche/keys.ts
+++ src/lib/config/avalanche/keys.ts
@@ -2,7 +2,7 @@ import { Keys } from '../types';
 
 const keys: Keys = {
   infura: 'daaa68ec242643719749dd1caba2fc66',
-  alchemy: 'ODJ9G5Ipv-Hb2zTWMNbUFIqv9WtqBOc2',
+  alchemy: 'UrVAqAyukeLVl8hhc29tLtegSoWL9Iuy',
   balancerApi: 'da2-7a3ukmnw7bexndpx5x522uafui',
 };
 
