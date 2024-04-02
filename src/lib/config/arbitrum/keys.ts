1bbc68fb52184151fc95730b8c13ccb411400364
Gareth Fuller <gareth.a.f@gmail.com>
2024-04-02 19:59:28 +0100

    chore: Key rotation

--- src/lib/config/arbitrum/keys.ts
+++ src/lib/config/arbitrum/keys.ts
@@ -2,7 +2,7 @@ import { Keys } from '../types';
 
 const keys: Keys = {
   infura: 'daaa68ec242643719749dd1caba2fc66',
-  alchemy: 'VBeQgTCRqqPtuuEPsFzRdwKXzDyN6aFh',
+  alchemy: '7zQj9eAtOhGz-sm7NGBqmLNaqvhLN0Kz',
 };
 
 export default keys;
