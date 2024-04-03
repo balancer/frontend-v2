89225ba7a1554a495d72d0616899d43dc3bd7454
Gareth Fuller <gareth.a.f@gmail.com>
2024-04-03 10:02:06 +0100

    Chore/remove keys (dm) (#5503)
    * chore: Add 33tBTC-33WETH-33USDbC pool to base allowlist (#5468)
    
    Co-authored-by: balopco <balopco@users.noreply.github.com>
    
    * 1.143.1
    
    * chore: Add 50OLAS-50USDC pool to base allowlist
    
    * 1.143.2
    
    * chore: Add MATIC-USDC-BTC-ETH/S-Token-Superpool pool to polygon allowlist
    
    * chore: Add 9WMATIC-18WBTC-9USDC-9ETH-S-9MATIC-S-9USD-S-18BTC-S-19STARV3/StarSeeds_Protocol pool to polygon allowlist
    
    * chore: Add 9WMATIC-9USDC-18ETH-S-9MATIC-S-9USD-S-18WETH-9BTC-S-19STARV3/StarSeeds_Protocol pool to polygon allowlist
    
    * chore: Add 33GHST-33stMATIC-33LDO pool to polygon allowlist
    
    * 1.143.3
    
    * 1.143.4
    
    * 1.143.5
    
    * 1.143.6
    
    * Update pools.ts (#5478)
    
    * 1.143.7
    
    * chore: Add 50USDC-50USDT pool to arbitrum allowlist
    
    * chore: Add points meta (#5480)
    
    * chore: Add Kelp points
    
    * chore: Add all gyro pools
    
    * 1.143.8
    
    * chore: Add 45ezETH-55WBTC pool to arbitrum allowlist
    
    * fix pools composition
    
    * chore: Add 99goldCOMP-1WETH pool to ethereum allowlist
    
    * chore: Add 30DFX-30GHST-40GROSH pool to polygon allowlist
    
    * chore: Add MER/USD pool to arbitrum allowlist
    
    * chore: Add 994POOL-BPT-1MER pool to arbitrum allowlist
    
    * 1.143.9
    
    * 1.143.10
    
    * 1.143.11
    
    * 1.143.12
    
    * 1.143.13
    
    * Update pools.ts
    
    * 1.143.14
    
    * chore: Add 16WBTC-16ETH-S-16MATIC-S-16USD-S-16BTC-S-20STARV3/StarSeeds_Protocol pool to polygon allowlist
    
    * 1.143.15
    
    * 1.143.16
    
    * chore: Add 20WMATIC-15ETH-S-15MATIC-S-15USD-S-15BTC-S-20STARV3/StarSeeds_Protocol pool to polygon allowlist
    
    * 1.143.17
    
    * 1.143.18
    
    * chore: Add sfrxETH/cbETH pool to base allowlist (#5491)
    
    Co-authored-by: balopco <balopco@users.noreply.github.com>
    
    * 1.143.19
    
    * chore: Remove trade redirect (#5492)
    
    * 1.143.20
    
    * chore: Add eigen layer points (#5494)
    
    * 1.143.21
    
    * chore: Add 20WMATIC-20WBTC-20USDC-20WETH-20NEX pool to polygon allowlist
    
    * 1.143.22
    
    * chore: Key rotation (#5496)
    
    * 1.143.23
    
    * chore: Add 70WETH-30ARB pool to arbitrum allowlist
    
    * Update pools.ts
    
    * Update pools.ts
    
    * Update pools.ts
    
    * 1.143.24
    
    * chore: Add ShezUSD/sDAI pool to ethereum allowlist (#5500)
    
    Co-authored-by: balopco <balopco@users.noreply.github.com>
    
    * 1.143.25
    
    * 1.143.26
    
    * chore: Remove Alchemy keys
    
    * chore: Remove from mainnet
    
    * chore: Remove more
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: balopco <balopco@users.noreply.github.com>
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
    Co-authored-by: Tritium <11885129+Tritium-VLK@users.noreply.github.com>
    Co-authored-by: Alberto Gualis <alberto@balancer.finance>
    Co-authored-by: Zen-Maxi <85650601+Zen-Maxi@users.noreply.github.com>
    Co-authored-by: mendesfabio <ms.fabiomendes@gmail.com>
    Co-authored-by: ZeKraken <79888567+zekraken-bot@users.noreply.github.com>

--- src/lib/config/mainnet/keys.ts
+++ src/lib/config/mainnet/keys.ts
@@ -2,7 +2,7 @@ import { Keys } from '../types';
 
 const keys: Keys = {
   infura: 'daaa68ec242643719749dd1caba2fc66',
-  alchemy: 'KjFKFD1iyGgsEesE0xOovzJKkVd_Jn19',
+  alchemy: '',
   graph: '7c7b4c36244b0b86171a3931eaf9bb23',
   balancerApi: 'da2-7a3ukmnw7bexndpx5x522uafui',
 };
