80efe344a4205cdd279ca7f7ae0bbf35ac5ce61d
ZeKraken <79888567+zekraken-bot@users.noreply.github.com>
2024-06-24 21:29:28 -0400

    add pools to stakable

--- src/lib/config/fraxtal/pools.ts
+++ src/lib/config/fraxtal/pools.ts
@@ -37,7 +37,12 @@ const pools: Pools = {
     '0x9da18982a33fd0c7051b19f0d7c76f2d5e7e017c': 'weightedPool',
   },
   Stakable: {
-    VotingGaugePools: [],
+    VotingGaugePools: [
+      '0xa0b92b33beafce388ce0092afdcd0ca77323eb12000000000000000000000006',
+      '0xa0af0b88796c1aa67e93db89fead2ab7aa3d6747000000000000000000000007',
+      '0x33251abecb0364df98a27a8d5d7b5ccddc774c42000000000000000000000008',
+      '0x1570315476480fa80cec1fff07a20c1df1adfd53000200000000000000000009',
+    ],
     AllowList: [],
   },
   Deep: [],
