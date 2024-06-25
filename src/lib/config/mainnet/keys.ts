4d5d857bd670a0e421dc54c7aa3caf091cd2828b
Gareth Fuller <gareth.a.f@gmail.com>
2024-06-25 21:00:18 +0100

    chore: Undo key change

--- src/lib/config/mainnet/keys.ts
+++ src/lib/config/mainnet/keys.ts
@@ -3,7 +3,7 @@ import { Keys } from '../types';
 const keys: Keys = {
   infura: 'daaa68ec242643719749dd1caba2fc66',
   alchemy: '',
-  graph: '91429265f2a0b1852cd6665ce5fa6a3e',
+  graph: 'a84caa9e5c322a2faec24ad89ccb9d28',
   balancerApi: 'da2-7a3ukmnw7bexndpx5x522uafui',
 };
 
