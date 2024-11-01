d73985e5a94a5af671d16c1d0ca35930fb87c008
Anastasios <anastas3993@gmail.com>
2024-11-01 12:10:38 +0300

    fix: add graph api key (#5917)

--- src/lib/config/base/keys.ts
+++ src/lib/config/base/keys.ts
@@ -3,6 +3,7 @@ import { Keys } from '../types';
 const keys: Keys = {
   infura: 'daaa68ec242643719749dd1caba2fc66',
   alchemy: '',
+  graph: 'a84caa9e5c322a2faec24ad89ccb9d28',
 };
 
 export default keys;
