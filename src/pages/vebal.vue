c1df77b40279b4f220fa11fc15668177da742e14
uiuxxx <uiuxxx@users.noreply.github.com>
2026-02-03 16:01:10 +0000

    Remove veBAL voting table and link all zen-replicated actions to balancer.fi

--- src/pages/vebal.vue
+++ src/pages/vebal.vue
@@ -2,7 +2,6 @@
 /**
  * veBAL page
  */
-import LMVoting from '@/components/contextual/pages/vebal/LMVoting/LMVoting.vue';
 import MyVeBAL from '@/components/contextual/pages/vebal/MyVeBAL/MyVeBAL.vue';
 import CrossChainBoostCards from '@/components/contextual/pages/vebal/cross-chain-boost/CrossChainBoostCards.vue';
 import { isVeBalSupported } from '@/composables/useVeBAL';
@@ -11,12 +10,10 @@ import { provideUserStaking } from '@/providers/local/user-staking.provider';
 import { providerUserPools } from '@/providers/local/user-pools.provider';
 import { providePoolStaking } from '@/providers/local/pool-staking.provider';
 import MyVebalInfo from '@/components/contextual/pages/vebal/MyVebalInfo.vue';
-import { provideVoting } from '@/components/contextual/pages/vebal/providers/voting.provider';
 
 const userStaking = provideUserStaking();
 providerUserPools(userStaking);
 providePoolStaking();
-provideVoting();
 </script>
 
 <template>
@@ -34,20 +31,6 @@ provideVoting();
           <CrossChainBoostCards />
         </div>
       </div>
-    </div>
-    <div
-      v-if="isVeBalSupported"
-      class="xl:container lg:px-4 xl:px-0 pt-16 xl:pt-20 xl:mx-auto"
-    >
-      <div class="xl:px-0 mb-16">
-        <LMVoting />
-      </div>
-    </div>
-    <div v-else class="text-center">
-      <div class="text-lg font-semibold">
-        {{ $t('veBAL.notSupported.title') }}
-      </div>
-      <div>{{ $t('veBAL.notSupported.description') }}</div>
     </div>
   </div>
 </template>
